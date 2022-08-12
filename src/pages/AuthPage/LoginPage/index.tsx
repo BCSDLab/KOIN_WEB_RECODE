3c3cba8c71145f4ee5015ba318de05554f854342
murpin <qw04011@gmail.com>
2022-08-12 17:04:56 +0900

    style: 로그인 화면 copyright, 로고 이미지 추가

--- src/pages/AuthPage/LoginPage/index.tsx
+++ src/pages/AuthPage/LoginPage/index.tsx
@@ -1,6 +1,20 @@
+import LoginForm from 'components/Auth/LoginForm/LoginForm';
+import styles from './LoginPage.module.scss';
+
 function LoginPage() {
   return (
-    <div>Login</div>
+    <div className={styles.template}>
+      <div className={styles.template__content}>
+        <LoginForm />
+        <span className={styles.template__copyright}>
+          COPYRIGHT ⓒ&nbsp;
+          {
+            new Date().getFullYear()
+          }
+          &nbsp;BY BCSDLab ALL RIGHTS RESERVED.
+        </span>
+      </div>
+    </div>
   );
 }
 
--- src/pages/AuthPage/index.tsx
+++ src/pages/AuthPage/index.tsx
@@ -1,8 +1,12 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
+import styles from './AuthPage.module.scss';
 
 function AuthPage() {
   return (
-    <div>
+    <div className={styles.template}>
+      <Link to="/">
+        <img src="https://static.koreatech.in/assets/img/logo_primary.png" alt="logo" className={styles.template__logo} />
+      </Link>
       <Outlet />
     </div>
   );
