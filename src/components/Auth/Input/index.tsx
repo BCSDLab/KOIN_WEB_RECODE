b8e4681873c29095764812662ef4626459293196
daepan <qw04011@gmail.com>
2022-08-13 20:56:01 +0900

    feat: 로그인페이지 input, LoginForm 컴포넌트 생성

---
+++ src/components/Auth/Input/index.tsx
@@ -0,0 +1,9 @@
+import styles from './input.module.scss';
+
+function input() {
+  return (
+    <input className={styles.styledinput} />
+  );
+}
+
+export default input;
--- src/components/Auth/LoginForm/LoginForm.tsx
+++ src/components/Auth/LoginForm/LoginForm.tsx
@@ -1,8 +1,28 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { Link } from 'react-router-dom';
+import styles from './LoginForm.module.scss';
+import Input from '../Input';
+
 function LoginForm() {
   return (
-    <div>
-      Login
-    </div>
+    <>
+      <form className={styles.loginform}>
+        <Input />
+        <Input />
+        <button type="submit" className={styles.loginform__button}>
+          로그인
+        </button>
+      </form>
+      <div className={styles.autologin}>
+        <input className={styles.autologin__checkbox} type="checkbox" id="autoLoginCheckBox" />
+        <label className={styles.autologin__text} htmlFor="autoLoginCheckBox">자동 로그인</label>
+      </div>
+      <div className={styles.help}>
+        <a className={styles.help__finduser} href="https://portal.koreatech.ac.kr/kut/page/findUser.jsp">아이디 찾기</a>
+        <Link className={styles.help__findpassword} to="/findpw">비밀번호 찾기</Link>
+        <Link className={styles.help__signup} to="/signup">회원가입</Link>
+      </div>
+    </>
   );
 }
 
