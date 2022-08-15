fb03b1bf9b8fc6fdf5a3ff097ae013d4f336ae6a
murpin <qw04011@gmail.com>
2022-08-15 17:58:49 +0900

    feat: Input 컴포넌트 타입 선언 및 추가

--- src/components/Auth/Input/index.tsx
+++ src/components/Auth/Input/index.tsx
@@ -1,9 +1,36 @@
+import React from 'react';
 import styles from './input.module.scss';
 
-function input() {
+type InputProps = {
+  autoComplete : string
+  value: any
+  onChange: any
+  name: any
+  placeholder: any
+};
+
+function input(
+  {
+    autoComplete,
+    value,
+    onChange,
+    name,
+    placeholder,
+  }: InputProps,
+  ref
+  : any,
+) {
   return (
-    <input className={styles.styledinput} />
+    <input
+      className={styles.styledinput}
+      ref={ref}
+      autoComplete={autoComplete}
+      value={value}
+      onChange={onChange}
+      name={name}
+      placeholder={placeholder}
+    />
   );
 }
 
-export default input;
+export default React.forwardRef(input);
