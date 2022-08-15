e1d48daca3a523b7fb599a639c5f90d6d857b282
murpin <qw04011@gmail.com>
2022-08-15 18:00:09 +0900

    feat: 로그인 화면 useImpretiveHandle 적용

--- src/components/Auth/LoginForm/LoginForm.tsx
+++ src/components/Auth/LoginForm/LoginForm.tsx
@@ -1,14 +1,66 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './LoginForm.module.scss';
 import Input from '../Input';
 
-function LoginForm() {
+type LoginFormProps = {
+  loginInfo: any
+  onChange: any
+  onSubmit: any
+  emailRef: any
+  passwordRef: any
+};
+
+function LoginForm({
+  loginInfo,
+  onChange,
+  onSubmit,
+  emailRef,
+  passwordRef,
+}: LoginFormProps) {
+  const emailValidate = (value: any) => value;
+  const passwordValidate = (value: any) => value;
+  const emailChange = (e: any) => {
+    onChange(emailValidate(e));
+  };
+  const passwordChange = (e: any) => {
+    onChange(passwordValidate(e));
+  };
+
+  React.useImperativeHandle(
+    emailRef,
+    () => ({
+      emailValidate,
+    }),
+    [],
+  );
+  React.useImperativeHandle(
+    passwordRef,
+    () => ({
+      emailValidate,
+    }),
+    [],
+  );
   return (
     <>
-      <form className={styles.loginform}>
-        <Input />
-        <Input />
+      <form className={styles.loginform} onSubmit={onSubmit}>
+        <Input
+          ref={emailRef}
+          autoComplete="username"
+          name="userId"
+          value={loginInfo.userId}
+          onChange={emailChange}
+          placeholder="아이디를 입력하세요"
+        />
+        <Input
+          ref={passwordRef}
+          autoComplete="current-password"
+          name="password"
+          value={loginInfo.password}
+          onChange={passwordChange}
+          placeholder="비밀번호를 입력하세요"
+        />
         <button type="submit" className={styles.loginform__button}>
           로그인
         </button>
@@ -26,4 +78,4 @@ function LoginForm() {
   );
 }
 
-export default LoginForm;
+export default React.forwardRef(LoginForm);
--- src/pages/Auth/LoginPage/index.tsx
+++ src/pages/Auth/LoginPage/index.tsx
@@ -1,10 +1,52 @@
+import React from 'react';
 import LoginForm from 'components/Auth/LoginForm/LoginForm';
 import styles from './LoginPage.module.scss';
 
 function LoginPage() {
+  const [loginInfo, setLoginInfo] = React.useState({
+    userId: '',
+    password: '',
+  });
+  const emailLocalPartRegex = React.useMemo(() => /^[a-z_0-9]{1,12}$/, []);
+  const emailRef = React.useRef();
+  const passwordRef = React.useRef();
+
+  const onChange = React.useCallback((e: any) => {
+    setLoginInfo({
+      ...loginInfo,
+      [e.target.name]: e.target.value,
+    });
+  }, [loginInfo]);
+
+  const onSubmit = React.useCallback((e: any) => {
+    e.preventDefault();
+    const { userId, password } = loginInfo;
+    if (!userId || !userId.length) {
+      return;
+    }
+    if (!password || !password.length) {
+      return;
+    }
+    if (userId.indexOf('@koreatech.ac.kr') !== -1) {
+      return;
+    }
+    if (!emailLocalPartRegex.test(userId)) {
+      console.log(emailLocalPartRegex);
+    }
+    /*
+    TODO : 로그인 api 콜
+    */
+  }, [loginInfo, emailLocalPartRegex]);
+
   return (
     <div className={styles.template}>
-      <LoginForm />
+      <LoginForm
+        loginInfo={loginInfo}
+        onChange={onChange}
+        onSubmit={onSubmit}
+        emailRef={emailRef}
+        passwordRef={passwordRef}
+      />
       <span className={styles.template__copyright}>
         COPYRIGHT ⓒ&nbsp;
         {
