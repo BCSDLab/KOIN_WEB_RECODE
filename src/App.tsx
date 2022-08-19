b67791852468c36f0920e7cdac7be4dca9f76e99
daepan <qw04011@gmail.com>
2022-08-19 16:34:39 +0900

    feat: token 유무에 따른 로그인페이지 진입 방지

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import AuthPage from 'pages/Auth';
 import LoginPage from 'pages/Auth/LoginPage';
 import BoardPage from 'pages/BoardPage';
-import { Routes, Route } from 'react-router-dom';
+import {
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 
 function App() {
+  const token = localStorage.getItem('AUTH_TOKEN_KEY');
+
   return (
     <Routes>
       <Route path="/" element={<BoardPage />} />
-      <Route path="login/*" element={<AuthPage />}>
+      <Route path="login/*" element={token ? <Navigate replace to="/" /> : <AuthPage />}>
         <Route index element={<LoginPage />} />
       </Route>
     </Routes>
