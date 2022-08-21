f647162b9f96500a5c16cb881695f8b23b1d09a1
daepan <qw04011@gmail.com>
2022-08-21 19:27:08 +0900

    fix: 쿠키 함수 lint error 수정

--- src/utils/ts/cookie.ts
+++ src/utils/ts/cookie.ts
@@ -1,14 +1,15 @@
-export function setCookie(name: string, val: string) {
+export function setCookie(name: string, val: any) {
   const date = new Date();
   const value = val;
 
   date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
-  document.cookie = name + '=' + value + '; expires=' + date.toUTCString() + '; path=/';
+  document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/`;
 }
 
+// eslint-disable-next-line consistent-return
 export function getCookie(name: string) {
-  const value = ';' + document.cookie;
-  const parts = value.split('; ' + name + '=');
+  const value = `;${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
 
   if (parts.length === 2) {
     return parts.pop()?.split(';').shift();
@@ -19,5 +20,5 @@ export function deleteCookie(name: string) {
   const date = new Date();
   date.setTime(date.getTime() + (-1 * 24 * 60 * 60 * 1000));
 
-  document.cookie = name + '=; expires=' + date.toUTCString() + '; path=/ ';
+  document.cookie = `${name}=; expires=${date.toUTCString()}; path=/ `;
 }
