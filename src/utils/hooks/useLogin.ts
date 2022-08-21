08571c4dfa1a6b8a2ed1d27584e6d1a92e3ba9fc
murpin <qw04011@gmail.com>
2022-08-21 16:40:19 +0900

    feat: useLogin interfacetype 추가

--- src/utils/hooks/useLogin.ts
+++ src/utils/hooks/useLogin.ts
@@ -1,13 +1,22 @@
 import { LoginResponse } from 'api/auth/entity';
 import { useMutation } from 'react-query';
 import { useNavigate } from 'react-router-dom';
+import { setCookie } from 'utils/ts/cookie';
 import * as api from '../../api';
 
-export default function useLogin() {
+interface State {
+  isAutoLoginFlag: boolean
+}
+
+export default function useLogin(state : State) {
   const navigate = useNavigate();
   const postLogin = useMutation(api.auth.login, {
     onSuccess: (data: LoginResponse) => {
-      localStorage.setItem('AUTH_TOKEN_KEY', data.token);
+      if (state.isAutoLoginFlag) {
+        localStorage.setItem('AUTH_TOKEN_KEY', data.token);
+      } else {
+        setCookie('AUTH_TOKEN_KEY', data.token);
+      }
       navigate('/');
     },
   });
