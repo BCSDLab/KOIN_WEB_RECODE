b25fc89428067b88f01ce63819c43536874e8e32
murpin <qw04011@gmail.com>
2022-09-03 00:18:44 +0900

    style: 상점 카테고리 바 UI 작성

--- src/pages/StorePage/index.tsx
+++ src/pages/StorePage/index.tsx
@@ -1,7 +1,23 @@
+import STORECATEGORY from 'static/storeCategory';
+import styles from './StorePage.module.scss';
+
 function StorePage() {
   return (
-    <div className="header">
-      주변상점
+    <div className={styles.list_section}>
+      <div className={styles.header}>
+        주변상점
+      </div>
+      <div className={styles.category}>
+        <div className={styles.category__header}>CATEGORY</div>
+        <div className={styles.category__wrapper}>
+          {STORECATEGORY.map((value) => (
+            <div className={styles.category__menu} key={value.tag}>
+              <img className={styles.category__image} src={value.image} alt="category_img" />
+              {value.title}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
