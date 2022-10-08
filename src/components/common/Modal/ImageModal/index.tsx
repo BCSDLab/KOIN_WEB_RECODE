66f6f7f578c146eb6fee2d80ff2722a4f7d6ef96
murpin <qw04011@gmail.com>
2022-10-09 00:53:12 +0900

    feat: 상점상세페이지 이미지 모달 기능 작성

--- src/components/common/Modal/ImageModal/index.tsx
+++ src/components/common/Modal/ImageModal/index.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import useModal from 'utils/hooks/useModal';
+import cn from 'utils/ts/classnames';
 import styles from './ImageModal.module.scss';
 
 export interface ImageModalProps {
@@ -9,12 +11,36 @@ function ImageModal({
   image,
 }: ImageModalProps) {
   const { hideModal } = useModal();
+  const [selectedImage, setSelectedImage] = React.useState(image[0]);
+  const selectedIndex = image.findIndex((value) => value === selectedImage);
+
   console.log(image, hideModal);
   return (
     <div className={styles.background}>
-      <div className={styles.image}>
-        img
-      </div>
+      {selectedIndex !== 0 && (
+        <button
+          type="button"
+          aria-label="prev"
+          className={cn({
+            [styles['arrow-button']]: true,
+            [styles['arrow-button--prev']]: true,
+          })}
+          onClick={() => setSelectedImage(image[selectedIndex - 1])}
+        />
+      )}
+      {selectedIndex !== image.length - 1 && (
+        <button
+          type="button"
+          aria-label="next"
+          className={cn({
+            [styles['arrow-button']]: true,
+            [styles['arrow-button--next']]: true,
+          })}
+          onClick={() => setSelectedImage(image[selectedIndex + 1])}
+        />
+      )}
+      <button className={styles.close} type="button" aria-label="close" onClick={hideModal} />
+      <img className={styles.image} src={`${selectedImage}`} alt="상점이미지" />
     </div>
   );
 }
