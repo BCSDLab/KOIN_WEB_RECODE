1e18987c9a53cae769516bc6aeb6032d38310ad0
murpin <qw04011@gmail.com>
2022-10-20 01:07:31 +0900

    refactor: 불필요한 import 제거

--- src/utils/hooks/useScrollToTop.ts
+++ src/utils/hooks/useScrollToTop.ts
@@ -1,16 +1,11 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
 
 export default function useScrollToTop() {
-  const { pathname } = useLocation();
-
-  if (!pathname) throw new Error('잘못된 호출입니다');
-
   React.useEffect(() => {
     document.documentElement.scrollTo({
       top: 0,
       left: 0,
       behavior: 'smooth',
     });
-  }, [pathname]);
+  }, []);
 }
