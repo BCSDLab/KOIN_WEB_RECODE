fb92dc3c9e9cf3caa3b6b9b85d1b0a58193108e6
murpin <qw04011@gmail.com>
2022-11-09 02:39:16 +0900

    style: 공지사항 페이지 게시판 헤더 UI 작성

--- src/pages/Notice/NoticePage/index.tsx
+++ src/pages/Notice/NoticePage/index.tsx
@@ -1,3 +1,4 @@
+import cn from 'utils/ts/classnames';
 import styles from './NoticePage.module.scss';
 
 function NoticePage() {
@@ -6,6 +7,45 @@ function NoticePage() {
       <div className={styles.content}>
         <div className={styles.header}>
           <h1 className={styles.header__title}>공지사항</h1>
+        </div>
+        <div className={styles['post-header']}>
+          <div className={styles['post-row']}>
+            <div className={cn({
+              [styles['post-info']]: true,
+              [styles['post-info--number']]: true,
+            })}
+            >
+              번호
+            </div>
+            <div className={cn({
+              [styles['post-info']]: true,
+              [styles['post-info--title']]: true,
+            })}
+            >
+              제목
+            </div>
+            <div className={cn({
+              [styles['post-info']]: true,
+              [styles['post-info--author']]: true,
+            })}
+            >
+              작성자
+            </div>
+            <div className={cn({
+              [styles['post-info']]: true,
+              [styles['post-info--date']]: true,
+            })}
+            >
+              날짜
+            </div>
+            <div className={cn({
+              [styles['post-info']]: true,
+              [styles['post-info--views']]: true,
+            })}
+            >
+              조회수
+            </div>
+          </div>
         </div>
       </div>
     </div>
