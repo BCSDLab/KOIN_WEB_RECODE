c04359997f008845506856dfbcdcedb3dbd198e5
murpin <qw04011@gmail.com>
2022-11-09 14:22:50 +0900

    feat: 상세 페이지 리스트 섹션 추가

--- src/pages/Notice/NoticePage/index.tsx
+++ src/pages/Notice/NoticePage/index.tsx
@@ -8,42 +8,44 @@ function NoticePage() {
         <div className={styles.header}>
           <h1 className={styles.header__title}>공지사항</h1>
         </div>
-        <div className={styles['post-header']}>
-          <div className={styles['post-row']}>
-            <div className={cn({
-              [styles['post-info']]: true,
-              [styles['post-info--number']]: true,
-            })}
-            >
-              번호
-            </div>
-            <div className={cn({
-              [styles['post-info']]: true,
-              [styles['post-info--title']]: true,
-            })}
-            >
-              제목
-            </div>
-            <div className={cn({
-              [styles['post-info']]: true,
-              [styles['post-info--author']]: true,
-            })}
-            >
-              작성자
-            </div>
-            <div className={cn({
-              [styles['post-info']]: true,
-              [styles['post-info--date']]: true,
-            })}
-            >
-              날짜
-            </div>
-            <div className={cn({
-              [styles['post-info']]: true,
-              [styles['post-info--views']]: true,
-            })}
-            >
-              조회수
+        <div className={styles['post-section']}>
+          <div className={styles['post-header']}>
+            <div className={styles['post-row']}>
+              <div className={cn({
+                [styles['post-info']]: true,
+                [styles['post-info--number']]: true,
+              })}
+              >
+                번호
+              </div>
+              <div className={cn({
+                [styles['post-info']]: true,
+                [styles['post-info--title']]: true,
+              })}
+              >
+                제목
+              </div>
+              <div className={cn({
+                [styles['post-info']]: true,
+                [styles['post-info--author']]: true,
+              })}
+              >
+                작성자
+              </div>
+              <div className={cn({
+                [styles['post-info']]: true,
+                [styles['post-info--date']]: true,
+              })}
+              >
+                날짜
+              </div>
+              <div className={cn({
+                [styles['post-info']]: true,
+                [styles['post-info--views']]: true,
+              })}
+              >
+                조회수
+              </div>
             </div>
           </div>
         </div>
