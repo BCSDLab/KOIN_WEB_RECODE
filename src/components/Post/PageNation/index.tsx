496ede0b1ab1988a876db0f9524149b342d56b57
murpin <qw04011@gmail.com>
2022-12-04 19:01:42 +0900

    feat: 페이지네이션 이전으로 다음으로가기 버튼 이벤트 작성

--- src/components/Post/PageNation/index.tsx
+++ src/components/Post/PageNation/index.tsx
@@ -1,4 +1,5 @@
 import cn from 'utils/ts/classnames';
+import showToast from 'utils/ts/showToast';
 import useParamsHandler from 'pages/Notice/hooks/useParamsHandler';
 import styles from './PageNation.module.scss';
 
@@ -8,6 +9,24 @@ interface PageNationProps {
 }
 
 const LIMIT_COUNT = [0, 1, 2, 3, 4];
+
+const movePrevNumber = (moveNumber: number) => {
+  if (moveNumber <= 0) {
+    showToast('error', '첫 페이지입니다.');
+    return '1';
+  }
+
+  return String(moveNumber);
+};
+
+const moveNextNumber = (moveNumber: number, totalPageNum: number) => {
+  if (moveNumber + 1 >= totalPageNum) {
+    showToast('error', '마지막 페이지입니다.');
+    return String(totalPageNum);
+  }
+
+  return String(moveNumber);
+};
 
 const displayCorrectionNum = (totalPageNum: number, nowPageNum: number) => {
   if (totalPageNum <= LIMIT_COUNT.length) return 0;
@@ -23,12 +42,18 @@ const displayCorrectionNum = (totalPageNum: number, nowPageNum: number) => {
 };
 
 function PageNation(props: PageNationProps) {
-  const { setParams } = useParamsHandler();
+  const { params, setParams } = useParamsHandler();
   const { totalPageNum, nowPageNum } = props;
 
   return (
     <div className={styles.container}>
-      <button type="button" className={styles['move-pageSection']}>이전으로</button>
+      <button
+        type="button"
+        className={styles['move-pageSection']}
+        onClick={() => setParams('boardId', movePrevNumber(Number(params.boardId) - 1), false)}
+      >
+        이전으로
+      </button>
       {
         LIMIT_COUNT.length - 1 < totalPageNum ? (
           LIMIT_COUNT.map((limit) => (
@@ -64,7 +89,13 @@ function PageNation(props: PageNationProps) {
           ))
         )
       }
-      <button type="button" className={styles['move-pageSection']}>다음으로</button>
+      <button
+        type="button"
+        className={styles['move-pageSection']}
+        onClick={() => setParams('boardId', moveNextNumber(Number(params.boardId) + 1, Number(totalPageNum)), false)}
+      >
+        다음으로
+      </button>
     </div>
   );
 }
