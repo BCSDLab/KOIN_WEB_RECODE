e1fe33d499e776d452fbb06e67ae574568a0711d
WonBeen Choi <zad1264@naver.com>
2022-12-07 23:39:18 +0900

    feat: 버스 관련 타입, API 정의

---
+++ src/api/bus/entity.ts
@@ -0,0 +1,31 @@
+import { APIResponse } from 'interfaces/APIResponse';
+
+export type BusType = 'shuttle' | 'express' | 'city';
+
+export type Direction = 'from' | 'to';
+
+export type CoursesResponse = Array<{
+  bus_type: 'shuttle' | 'commuting'
+  direction: 'to' | 'from'
+  region: string
+}>;
+
+export interface BusResponse extends APIResponse {
+  bus_type: BusType
+  next_bus: {
+    remain_time: number
+  } | null
+  now_bus: {
+    remain_time: number
+  } | null
+}
+
+export type BusTimetableResponse = Array<BusRouteInfo>;
+
+interface BusRouteInfo {
+  route_name: string;
+  arrival_info: {
+    arrival_time: string;
+    node_name: string;
+  }[]
+}
