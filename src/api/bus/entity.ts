533ef2fb4133d40cd59ab4b26830caea1acd79f8
ChoiWonBeen <zad1264@naver.com>
2022-12-14 13:54:39 +0900

    style: 타입 정의에 세미콜론 추가

--- src/api/bus/entity.ts
+++ src/api/bus/entity.ts
@@ -5,21 +5,21 @@ export type BusType = 'shuttle' | 'express' | 'city';
 export type Direction = 'from' | 'to';
 
 export type CoursesResponse = Array<{
-  bus_type: 'shuttle' | 'commuting'
-  direction: 'to' | 'from'
-  region: string
+  bus_type: 'shuttle' | 'commuting';
+  direction: 'to' | 'from';
+  region: string;
 }>;
 
 export interface BusResponse extends APIResponse {
-  bus_type: BusType
+  bus_type: BusType;
   next_bus: {
-    remain_time: number
-    bus_number?: number
-  } | null
+    remain_time: number;
+    bus_number?: number;
+  } | null;
   now_bus: {
-    remain_time: number
-    bus_number?: number
-  } | null
+    remain_time: number;
+    bus_number?: number;
+  } | null;
 }
 
 export type BusTimetableResponse = Array<BusRouteInfo>;
@@ -27,7 +27,7 @@ export type BusTimetableResponse = Array<BusRouteInfo>;
 interface BusRouteInfo {
   route_name: string;
   arrival_info: {
-    arrival_time: string
-    node_name: string
-  }[]
+    arrival_time: string;
+    node_name: string;
+  }[];
 }
