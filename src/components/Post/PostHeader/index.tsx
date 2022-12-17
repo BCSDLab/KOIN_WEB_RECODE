6c3a395bf471015be2b60fbe2c12b7eb718ab4de
murpin <qw04011@gmail.com>
2022-12-17 18:44:36 +0900

    refactor: PostHeader 반복문을 활용하도록 변경

--- src/components/Post/PostHeader/index.tsx
+++ src/components/Post/PostHeader/index.tsx
@@ -1,46 +1,36 @@
 import cn from 'utils/ts/classnames';
 import styles from './PostHeader.module.scss';
 
+type HeaderRowInfo = {
+  [key: string]: string
+};
+
+const HEADER_ROW: HeaderRowInfo = {
+  number: '번호',
+  title: '제목',
+  author: '작성자',
+  date: '날짜',
+  views: '조회수',
+};
+
 function PostHeader() {
   return (
     <div className={styles.header}>
       <div className={styles.header__container}>
         <div className={styles.header__row}>
-          <div className={cn({
-            [styles.info]: true,
-            [styles['info--number']]: true,
-          })}
-          >
-            번호
-          </div>
-          <div className={cn({
-            [styles.info]: true,
-            [styles['info--title']]: true,
-          })}
-          >
-            제목
-          </div>
-          <div className={cn({
-            [styles.info]: true,
-            [styles['info--author']]: true,
-          })}
-          >
-            작성자
-          </div>
-          <div className={cn({
-            [styles.info]: true,
-            [styles['info--date']]: true,
-          })}
-          >
-            날짜
-          </div>
-          <div className={cn({
-            [styles.info]: true,
-            [styles['info--views']]: true,
-          })}
-          >
-            조회수
-          </div>
+          {
+            Object.keys(HEADER_ROW).map((key) => (
+              <div
+                key={HEADER_ROW[key]}
+                className={cn({
+                  [styles.info]: true,
+                  [styles[`info--${key}`]]: true,
+                })}
+              >
+                {HEADER_ROW[key]}
+              </div>
+            ))
+          }
         </div>
       </div>
     </div>
