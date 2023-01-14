1daee981f762caf93b083b3c00ae78cf60aff770
WonBeen Choi <zad1264@naver.com>
2023-01-15 02:57:59 +0900

    feat: IndexStore 카테고리 추가

--- src/pages/IndexPage/components/IndexStore/index.tsx
+++ src/pages/IndexPage/components/IndexStore/index.tsx
@@ -1,7 +1,19 @@
+import STORE_CATEGORY from 'static/storeCategory';
+import { Link } from 'react-router-dom';
+import styles from './IndexStore.module.scss';
+
 function IndexStore() {
   return (
-    <section>
-      indexstore
+    <section className={styles.template}>
+      <Link to="/store" className={styles.template__title}>주변상점</Link>
+      <div className={styles.category__wrapper}>
+        {STORE_CATEGORY.map((category) => (
+          <Link to={`/store?category=${category.tag}`} key={category.title} className={styles.category__item}>
+            <img src={category.image} alt={category.title} className={styles.category__image} />
+            {category.title}
+          </Link>
+        ))}
+      </div>
     </section>
   );
 }
