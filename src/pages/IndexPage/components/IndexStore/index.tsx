8067329fdd2c7befb889784287a6ecad5b9e328e
WonBeen Choi <zad1264@naver.com>
2023-01-15 20:00:32 +0900

    fix: 모바일 상태에만 글자 4개 제한으로 변경

--- src/pages/IndexPage/components/IndexStore/index.tsx
+++ src/pages/IndexPage/components/IndexStore/index.tsx
@@ -1,8 +1,11 @@
 import STORE_CATEGORY from 'static/storeCategory';
 import { Link } from 'react-router-dom';
+import useMediaQuery from 'utils/hooks/useMediaQuery';
 import styles from './IndexStore.module.scss';
 
 function IndexStore() {
+  const isMobile = useMediaQuery();
+
   return (
     <section className={styles.template}>
       <Link to="/store" className={styles.template__title}>주변상점</Link>
@@ -10,7 +13,7 @@ function IndexStore() {
         {STORE_CATEGORY.map((category) => (
           <Link to={`/store?category=${category.tag}`} key={category.title} className={styles.category__item}>
             <img src={category.image} alt={category.title} className={styles.category__image} />
-            {category.title.slice(0, 4)}
+            {isMobile ? category.title.slice(0, 4) : category.title}
           </Link>
         ))}
       </div>
