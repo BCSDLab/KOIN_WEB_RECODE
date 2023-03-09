da44b02ec596190708622f28f10b0d3cad753033
kimeodml <kimeodml@gmail.com>
2023-03-09 16:25:13 +0900

    feat: 복덕방 페이지 추가, 좌표에 맞게 지도에 표시

---
+++ src/pages/Room/RoomPage/index.tsx
@@ -0,0 +1,42 @@
+import {
+  Container as MapDiv, NaverMap, useNavermaps, Marker,
+} from 'react-naver-maps';
+import useRoomList from './hooks/useRoomList';
+import styles from './RoomPage.module.scss';
+
+function RoomPage() {
+  const navermaps = useNavermaps();
+  const roomList = useRoomList();
+  return (
+    <div className={styles.template}>
+      <h1 className={styles.title}>복덕방</h1>
+      <div className={styles.content}>
+        <MapDiv className={styles.map}>
+          <NaverMap
+            maxZoom={20}
+            minZoom={15}
+            logoControl={false}
+            zoomControl
+            scrollWheel={false}
+            zoomControlOptions={{
+              position: navermaps.Position.TOP_LIFT,
+            }}
+            defaultCenter={{ lat: 36.764617, lng: 127.2831540 }}
+            defaultZoom={16}
+            draggable
+          >
+            {roomList?.lands.map((room) => (
+              <Marker
+                key={room.id}
+                position={new navermaps.LatLng(room.latitude, room.longitude)}
+                title={room.name}
+              />
+            ))}
+          </NaverMap>
+        </MapDiv>
+      </div>
+    </div>
+  );
+}
+
+export default RoomPage;
