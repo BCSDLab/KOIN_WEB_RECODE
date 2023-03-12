fbf5e6e6e7619bccb431442e0f88e2729dbb2623
kimeodml <kimeodml@gmail.com>
2023-03-12 15:29:07 +0900

    feat: 복덕방 리스트 추가

--- src/pages/Room/RoomPage/index.tsx
+++ src/pages/Room/RoomPage/index.tsx
@@ -39,6 +39,23 @@ function RoomPage() {
             ))}
           </NaverMap>
         </MapDiv>
+        <div className={styles.list}>
+          {roomList?.lands.map((room) => (
+            <div className={styles.list__box} key={room.id}>
+              <div className={styles.list__name}>{room.name}</div>
+              <div className={styles.fee}>
+                <div className={styles.fee__info}>
+                  <span className={styles.fee__monthly}>월세</span>
+                  <span>{room.monthly_fee ? room.monthly_fee : '정보없음'}</span>
+                </div>
+                <div className={styles.fee__info}>
+                  <span className={styles.fee__charter}>전세</span>
+                  <span>{room.charter_fee ? `${room.charter_fee}만원` : ' - '}</span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
