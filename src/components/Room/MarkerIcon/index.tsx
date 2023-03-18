f92d5170c8daa1d138b2051f486c4b7aa028d660
kimeodml <kimeodml@gmail.com>
2023-03-18 17:12:19 +0900

    refactor: MarkerIcon type string으로 변경

--- src/components/Room/MarkerIcon/index.tsx
+++ src/components/Room/MarkerIcon/index.tsx
@@ -1,12 +1,12 @@
 import styles from './MarkerIcon.module.scss';
 
 function MarkerIcon() {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.icon} />
-      <div className={styles.pluse} />
+  return `
+    <div class=${styles.wrapper}>
+      <div class=${styles.icon} ></div>
+      <div class=${styles.pluse}></div>
     </div>
-  );
+  `;
 }
 
 export default MarkerIcon;
