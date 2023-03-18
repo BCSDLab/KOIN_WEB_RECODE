6c4b6ff0219809305a3d8916a30c4fa447fce1e0
kimeodml <kimeodml@gmail.com>
2023-03-18 17:06:50 +0900

    feat: RoomList에 Link 추가, 모바일 버전 UI 변경

--- src/components/Room/RoomList/index.tsx
+++ src/components/Room/RoomList/index.tsx
@@ -1,4 +1,5 @@
 import { LandList } from 'api/room/entity';
+import { Link } from 'react-router-dom';
 import styles from './RoomList.module.scss';
 
 interface LandListProps {
@@ -10,19 +11,21 @@ function RoomList(props: LandListProps) {
   return (
     <div className={styles.list}>
       {lands?.map((room) => (
-        <div className={styles.list__box} key={room.id}>
-          <div className={styles.list__name}>{room.name}</div>
-          <div className={styles.fee}>
-            <div className={styles.fee__info}>
-              <span className={styles.fee__monthly}>월세</span>
-              <span>{room.monthly_fee ? room.monthly_fee : '정보없음'}</span>
-            </div>
-            <div className={styles.fee__info}>
-              <span className={styles.fee__charter}>전세</span>
-              <span>{room.charter_fee ? `${room.charter_fee}만원` : ' - '}</span>
+        <Link className={styles.list__link} to={`/room/${room.id}`} key={room.id}>
+          <div className={styles.list__box}>
+            <div className={styles.list__name}>{room.name}</div>
+            <div className={styles.fee}>
+              <div className={styles.fee__info}>
+                <span className={styles.fee__monthly}>월세</span>
+                <span>{room.monthly_fee ? room.monthly_fee : '정보없음'}</span>
+              </div>
+              <div className={styles.fee__info}>
+                <span className={styles.fee__charter}>전세</span>
+                <span>{room.charter_fee ? `${room.charter_fee}만원` : ' - '}</span>
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
