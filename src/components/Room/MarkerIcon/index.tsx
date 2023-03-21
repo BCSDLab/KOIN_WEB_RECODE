fa3d6a7b4ba1c79cf0b144337c898af772429adf
Daeeui Kim <kimeodml@google.com>
2023-03-21 17:00:45 +0900

    feat: 백틱에 따옴표 추가

--- src/components/Room/MarkerIcon/index.tsx
+++ src/components/Room/MarkerIcon/index.tsx
@@ -2,9 +2,9 @@ import styles from './MarkerIcon.module.scss';
 
 function MarkerIcon() {
   return `
-    <div class=${styles.wrapper}>
-      <div class=${styles.icon} ></div>
-      <div class=${styles.pluse}></div>
+    <div class="${styles.wrapper}">
+      <div class="${styles.icon}"></div>
+      <div class="${styles.pluse}"></div>
     </div>
   `;
 }
