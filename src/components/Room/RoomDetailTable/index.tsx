64fab6ca590fe13723a63c64b15526e8badd0bfa
Daeeui Kim <kimeodml@gmail.com>
2023-04-25 00:03:31 +0900

    fix: 테이블 반복문으로 구성

--- src/components/Room/RoomDetailTable/index.tsx
+++ src/components/Room/RoomDetailTable/index.tsx
@@ -1,46 +1,53 @@
 import styles from './RoomDetailTable.module.scss';
 
 interface TableProps {
-  monthlyFee: string
-  roomType: string
-  charterFee: string
-  deposit: string
-  floor: null | number
-  managementFee: string
-  size: number
-  phone: string
+  monthlyFee: string;
+  roomType: string;
+  charterFee: string;
+  deposit: string;
+  floor: null | number;
+  managementFee: string;
+  size: number;
+  phone: string;
+}
+
+interface TableData {
+  title1: string;
+  title2: string;
+  value1: string | number | null;
+  value2: string | number | null;
+  suffix1?: string;
+  suffix2?: string;
 }
 
 function RoomDetailTable({
   monthlyFee, roomType, charterFee, deposit, floor, managementFee, size, phone,
 }: TableProps) {
+  const tableData: TableData[] = [
+    {
+      title1: '월세', value1: monthlyFee, title2: '방 종류', value2: roomType,
+    },
+    {
+      title1: '전세', value1: charterFee, suffix1: '만원', title2: '보증금', value2: deposit, suffix2: '만원',
+    },
+    {
+      title1: '층수', value1: floor, suffix1: '층', title2: '관리비', value2: managementFee,
+    },
+    {
+      title1: '방 크기', value1: size, suffix1: '평', title2: '연락처', value2: phone,
+    },
+  ];
   return (
     <table className={styles.table}>
       <tbody>
-        <tr>
-          <th className={styles.table__title}>월세</th>
-          <td className={styles.table__cell}>{monthlyFee || ' - ' }</td>
-          <th className={styles.table__title}>방 종류</th>
-          <td className={styles.table__cell}>{roomType || ' - '}</td>
-        </tr>
-        <tr className={styles.table__row}>
-          <th className={styles.table__title}>전세</th>
-          <td className={styles.table__cell}>{charterFee ? `${charterFee}만원` : ' - '}</td>
-          <th className={styles.table__title}>보증금</th>
-          <td className={styles.table__cell}>{deposit ? `${deposit}만원` : ' - '}</td>
-        </tr>
-        <tr className={styles.table__row}>
-          <th className={styles.table__title}>층수</th>
-          <td className={styles.table__cell}>{floor ? `${floor}층` : ' - '}</td>
-          <th className={styles.table__title}>관리비</th>
-          <td className={styles.table__cell}>{managementFee || ' - '}</td>
-        </tr>
-        <tr className={styles.table__row}>
-          <th className={styles.table__title}>방 크기</th>
-          <td className={styles.table__cell}>{size ? `${size}평` : ' - '}</td>
-          <th className={styles.table__title}>연락처</th>
-          <td className={styles.table__cell}>{phone || ' - '}</td>
-        </tr>
+        {tableData.map((data) => (
+          <tr className={styles.table__row}>
+            <th className={styles.table__title}>{data.title1}</th>
+            <td className={styles.table__cell}>{data.value1 ? `${data.value1}${data.suffix1 || ''}` : ' - ' }</td>
+            <th className={styles.table__title}>{data.title2}</th>
+            <td className={styles.table__cell}>{data.value2 ? `${data.value2}${data.suffix2 || ''}` : ' - ' }</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
