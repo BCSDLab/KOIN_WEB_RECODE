46795199236c4149345a4846a55b29099d6ce60d
Daeeui Kim <kimeodml@gmail.com>
2023-04-24 22:11:25 +0900

    fix: 초기값 컴포넌트 바깥에 선언

--- src/pages/Room/RoomPage/index.tsx
+++ src/pages/Room/RoomPage/index.tsx
@@ -5,11 +5,12 @@ import styles from './RoomPage.module.scss';
 import useNaverMap from './hooks/useNaverMap';
 import useMarker from './hooks/useMarker';
 
+const LOCATION = { latitude: 36.764617, longitude: 127.2831540 };
+
 function RoomPage() {
   const isMobile = useMediaQuery();
   const roomList = useRoomList();
-  const location = { latitude: 36.764617, longitude: 127.2831540 };
-  const map = useNaverMap(location.latitude, location.longitude);
+  const map = useNaverMap(LOCATION.latitude, LOCATION.longitude);
   useMarker({ map, roomList });
 
   return (
