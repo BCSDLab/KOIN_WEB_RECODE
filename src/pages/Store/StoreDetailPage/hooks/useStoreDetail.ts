5582dcd0c95e0fe7fb659c2f991a16dfee88caae
daepan <qw04011@gmail.com>
2023-07-24 21:19:13 +0900

    refactor: params => id로 변경

--- src/pages/Store/StoreDetailPage/hooks/useStoreDetail.ts
+++ src/pages/Store/StoreDetailPage/hooks/useStoreDetail.ts
@@ -1,12 +1,14 @@
 import { useQuery } from 'react-query';
 import * as api from 'api';
 
-const useStoreDetail = (params: string) => {
+const useStoreDetail = (id: string) => {
   const { data: storeDetail, isError: isStoreDetailError } = useQuery(
-    ['storeDetail', params],
+    ['storeDetail', id],
     ({ queryKey }) => api.store.getStoreDetailInfo(queryKey[1] ?? ''),
   );
-  const storeDescription = storeDetail?.description ? storeDetail?.description.replace(/(?:\/)/g, '\n') : '-';
+  const storeDescription = storeDetail?.description
+    ? storeDetail?.description.replace(/(?:\/)/g, '\n')
+    : '-';
 
   return {
     storeDetail: isStoreDetailError ? null : storeDetail,
