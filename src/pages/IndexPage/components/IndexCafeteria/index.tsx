3e4ec0297a5f9ec86b4c4f306727d5731b956652
(hoooooony) <(cjh41820@gmail.com)>
2024-01-09 01:59:21 +0900

    feat: ui 추가

--- src/pages/IndexPage/components/IndexCafeteria/index.tsx
+++ src/pages/IndexPage/components/IndexCafeteria/index.tsx
@@ -1,9 +1,35 @@
+import { Link } from 'react-router-dom';
+import { CAFETERIA_CATEGORY, CAFETERIA_TIME } from 'static/cafeteria';
 import styles from './IndexCafeteria.module.scss';
 
 function IndexCafeteria() {
   return (
     <section className={styles.template}>
-      indexCafeteria
+      <h2 className={styles.title}>
+        <span>식단</span>
+        <Link to="/cafeteria" className={styles.moreLink}>더 보기</Link>
+      </h2>
+      <div className={styles.cafeteriaCard}>
+        <div className={styles.cafeteriaContainer}>
+          {CAFETERIA_CATEGORY.map((category) => (
+            category.isShowMain && (
+            <div key={category.id} className={styles.cafeteria}>
+              {category.placeName}
+            </div>
+            )
+          ))}
+        </div>
+        <div className={styles.typeContainer}>
+          {CAFETERIA_TIME.map((time) => (
+            <div key={time.id} className={styles.type}>
+              {time.name}
+            </div>
+          ))}
+        </div>
+        <div className={styles.menuContainer}>
+          {/* allMenus를 순회하는 코드 작성 */}
+        </div>
+      </div>
     </section>
   );
 }
