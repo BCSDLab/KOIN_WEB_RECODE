d977be8a08c63dc877a2273bbcf9b0178374684d
daepan <qw04011@gmail.com>
2024-03-15 13:24:32 +0900

    refactor: 콜백 명시 수정

--- src/pages/Auth/ModifyInfoPage/components/UserDeleteModal/index.tsx
+++ src/pages/Auth/ModifyInfoPage/components/UserDeleteModal/index.tsx
@@ -15,7 +15,7 @@ export default function UserDeleteModal({
         <div className={styles.container__header}>알림!</div>
         <div className={styles.container__content}>해당 계정은 복구가 불가능합니다. 정말 삭제하시겠습니까?</div>
         <div className={styles.container__footer}>
-          <button className={styles.button} type="button" onClick={() => deleteUser}>삭제</button>
+          <button className={styles.button} type="button" onClick={() => deleteUser()}>삭제</button>
         </div>
       </div>
     </div>
