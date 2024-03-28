581f62ca3c2f00d334829f5d01f591beb79ea5d3
daepan <qw04011@gmail.com>
2024-03-29 00:07:36 +0900

    docs: 린트 주석 제거

--- src/components/common/LogPage/index.tsx
+++ src/components/common/LogPage/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import * as gtag from 'lib/gtag';
+import uuidv4 from 'utils/ts/uuidGenerater';
+import { UserResponse } from 'api/auth/entity';
 import { useLocation } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { userInfoState } from 'utils/recoil/userInfoState';
+
+const userUniqueIdGenerator = (userInfo: UserResponse | null | undefined) => {
+  const uuid = uuidv4();
+  if (userInfo?.student_number && userInfo.student_number.length > 0) {
+    return `${userInfo.student_number}-${uuid}`;
+  }
+  return uuid;
+};
 
 function LogPage() {
   const location = useLocation();
   const userInfo = useRecoilValue(userInfoState);
 
   React.useEffect(() => {
-    gtag.pageView(location.pathname + location.search, userInfo?.student_number.slice(0, 6));
-  }, [location, userInfo?.student_number]);
+    gtag.pageView(location.pathname + location.search, userUniqueIdGenerator(userInfo));
+  }, [location, userInfo]);
 
   return null;
 }
