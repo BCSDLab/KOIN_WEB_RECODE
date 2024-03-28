b4b159466ed713b0ea943b2dbc381bd4204b01bd
daepan <qw04011@gmail.com>
2024-03-28 23:37:29 +0900

    feat: gtag userID 수집 기능 추가 및 전역 변수를 통한 호출 기능 추가

--- src/components/common/LogPage/index.tsx
+++ src/components/common/LogPage/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import * as gtag from 'lib/gtag';
 import { useLocation } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { userInfoState } from 'utils/recoil/userInfoState';
 
 function LogPage() {
   const location = useLocation();
+  const userInfo = useRecoilValue(userInfoState);
 
   React.useEffect(() => {
-    gtag.pageView(location.pathname + location.search);
-  }, [location]);
+    gtag.pageView(location.pathname + location.search, userInfo?.student_number.slice(0, 6));
+  }, [location, userInfo?.student_number]);
 
   return null;
 }
