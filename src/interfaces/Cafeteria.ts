752b22ee9766cae6c9da87c99df2a31d0831afe7
KimKyungYun <kky38225221@gmail.com>
2024-03-30 22:35:42 +0900

    feat: 식단 타입 추가 및 git ignore에 fsevents추가

--- src/interfaces/Cafeteria.ts
+++ src/interfaces/Cafeteria.ts
@@ -3,12 +3,16 @@ import { CAFETERIA_CATEGORY, CAFETERIA_TIME } from 'static/cafeteria';
 export type CafeteriaTime = typeof CAFETERIA_TIME[number]['type'];
 export type CafeteriaCategory = typeof CAFETERIA_CATEGORY[number]['placeName'];
 export type CafeteriaMenu = {
+  created_at: string;
   id: number;
   date: string;
+  image_url:string | null;
   type: CafeteriaTime;
   place: CafeteriaCategory;
   price_card: number;
   price_cash: number;
   kcal: number;
   menu: string[];
+  sold_out: boolean;
+  updated_at: string;
 };
