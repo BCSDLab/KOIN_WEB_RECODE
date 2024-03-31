738892568881c5808e19cd6d998115ac220ab0a4
dooohun <ehgns0305@naver.com>
2024-03-31 23:44:20 +0900

    fix: 메인페이지에 시간표를 로그인 페이지로 라우팅 되는 에러 수정

--- src/pages/IndexPage/components/IndexTimetable/index.tsx
+++ src/pages/IndexPage/components/IndexTimetable/index.tsx
@@ -48,7 +48,6 @@ export default function IndexTimeTable() {
   // onChange와 deptOptionList가 렌더링될 때마다 선언되서 처음 한번만 해야 하는 onChange를 렌더링할 때마다 한다.
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  const token = useTokenState();
 
   return (
     <div className={styles.template}>
@@ -60,9 +59,6 @@ export default function IndexTimeTable() {
           <CurrentSemesterTimetable />
         </React.Suspense>
       </ErrorBoundary>
-      {!token && (
-        <Link to="/auth" className={styles.needLogin} />
-      )}
     </div>
   );
 }
