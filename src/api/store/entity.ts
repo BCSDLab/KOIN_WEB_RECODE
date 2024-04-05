397555fb4cb7042aaf08a63eafe02dd538195a76
Daeeui Kim <kimeodml@gmail.com>
2024-04-05 17:26:04 +0900

    fix: 타입 수정

--- src/api/store/entity.ts
+++ src/api/store/entity.ts
@@ -45,9 +45,11 @@ interface MultiPriceMenu extends MenuBase {
 
 export type Menu = SinglePriceMenu | MultiPriceMenu;
 
+// 추후 추천, 메인, 세트, 사이드로 변경 예정
+export type MenuCategoryName = '이벤트 메뉴' | '대표 메뉴' | '사이드 메뉴' | '세트 메뉴';
 export interface MenuCategory {
   id: number;
-  name: '이벤트 메뉴' | '대표 메뉴' | '추천 메뉴 ' | '사이드 메뉴' | '세트 메뉴';
+  name: MenuCategoryName;
   menus: Menu[];
 }
 
@@ -57,19 +59,20 @@ export interface StoreDetailMenuResponse extends APIResponse {
   updated_at: string;
 }
 
+export type StoreList = {
+  id: number;
+  name: string;
+  phone: string;
+  delivery: boolean;
+  pay_card: boolean;
+  pay_bank: boolean;
+  open: Open[];
+  category_ids: number[];
+};
+
 export interface StoreListResponse extends APIResponse {
   count: number;
-  shops:
-  {
-    id: number;
-    name: string;
-    phone: string;
-    delivery: boolean;
-    pay_card: boolean;
-    pay_bank: boolean;
-    open: Open[];
-    category_ids: number[];
-  }[]
+  shops: StoreList[];
 }
 
 export interface StoreCategoriesResponse extends APIResponse {
