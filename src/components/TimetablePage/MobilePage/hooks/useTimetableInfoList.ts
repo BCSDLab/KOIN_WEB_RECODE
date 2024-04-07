f34a0a19a53126f1add2fca8599892280e9ff35d
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-04-07 16:43:00 +0900

    add: 시간표 모바일페이지

---
+++ src/components/TimetablePage/MobilePage/hooks/useTimetableInfoList.ts
@@ -0,0 +1,25 @@
+import { useQuery } from 'react-query';
+import { getTimetableInfo } from 'api/timetable';
+import { KoinError } from 'interfaces/APIError';
+import { TimetableLectureInfo } from 'interfaces/Lecture';
+
+export const TIMETABLE_INFO_LIST = 'TIMETABLE_INFO_LIST';
+
+export default function useTimetableInfoList(
+  semester: string,
+  authorization: string,
+) {
+  return useQuery<
+  Awaited<ReturnType<typeof getTimetableInfo>>,
+  KoinError,
+  TimetableLectureInfo[] | undefined,
+  [string, string]
+  >(
+    [TIMETABLE_INFO_LIST, semester],
+    ({ queryKey: [, _semester] }) => getTimetableInfo(authorization, _semester),
+    {
+      enabled: !!authorization && !!semester,
+      select: (data: Awaited<ReturnType<typeof getTimetableInfo>>) => data.timetable,
+    },
+  );
+}
