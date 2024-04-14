1f721876fa856bd26f306cfbcb968e303d6a859c
Daeeui Kim <kimeodml@gmail.com>
2024-04-14 12:03:20 +0900

    fix: 메뉴 카테고리 수정

--- src/api/store/entity.ts
+++ src/api/store/entity.ts
@@ -45,8 +45,7 @@ interface MultiPriceMenu extends MenuBase {
 
 export type Menu = SinglePriceMenu | MultiPriceMenu;
 
-// 추후 추천, 메인, 세트, 사이드로 변경 예정
-export type MenuCategoryName = '이벤트 메뉴' | '대표 메뉴' | '사이드 메뉴' | '세트 메뉴';
+export type MenuCategoryName = '추천 메뉴' | '메인 메뉴' | '사이드 메뉴' | '세트 메뉴';
 export interface MenuCategory {
   id: number;
   name: MenuCategoryName;
