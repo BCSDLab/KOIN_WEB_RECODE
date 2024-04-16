7f916e43e511e4268a4eca0989617bc4f1dee66a
daepan <qw04011@gmail.com>
2024-04-16 20:10:56 +0900

    refactor: 변경된 상점 페이지에 대한 ImageErrorBoundary 적용

--- src/pages/Store/StoreDetailPage/index.tsx
+++ src/pages/Store/StoreDetailPage/index.tsx
@@ -6,6 +6,8 @@ import useMediaQuery from 'utils/hooks/useMediaQuery';
 import { cn } from '@bcsdlab/utils';
 import { Portal } from 'components/common/Modal/PortalProvider';
 import UpdateInfo from 'components/common/UpdateInfo/UpdateInfo';
+import ImageErrorBoundary from 'components/common/ImageErrorBoundary';
+import ErrorDisplay from 'components/common/ErrorDisplay';
 import useLogger from 'utils/hooks/useLogger';
 import useModalPortal from 'utils/hooks/useModalPortal';
 import useScrollToTop from 'utils/hooks/useScrollToTop';
@@ -155,7 +157,7 @@ function StoreDetailPage() {
                     type="button"
                     onClick={() => onClickImage(storeDetail!.image_urls, index)}
                   >
-                    <img className={styles.image__poster} src={`${img}`} alt="상점이미지" />
+                    <ImageErrorBoundary className={styles.image__poster} src={`${img}`} alt="상점이미지" fallback={<ErrorDisplay />} />
                   </button>
                 </div>
               ))) : (
