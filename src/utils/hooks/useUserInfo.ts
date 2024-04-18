1d2a2f97eb7455a50b67c4957cd331c92ae81e76
dbshaejin <dbshaejin@gmail.com>
2024-04-18 12:49:04 +0900

    feat: 로그인 관련 tanstack Query 수정

--- src/utils/hooks/useUserInfo.ts
+++ src/utils/hooks/useUserInfo.ts
@@ -1,4 +1,3 @@
-// import { useQuery } from 'react-query';
 import { queryOptions, useQuery } from '@tanstack/react-query';
 import * as api from 'api';
 import { useSetRecoilState } from 'recoil';
@@ -6,24 +5,7 @@ import { userInfoState } from 'utils/recoil/userInfoState';
 
 const useUserInfo = (token: string) => {
   const setUserInfo = useSetRecoilState(userInfoState);
-  // const { data: userInfo, isError: isUserDataError } = useQuery(
-  //   ['userInfo', token],
-  //   () => api.auth.getUser(token),
-  //   {
-  //     onSuccess: (data) => {
-  //       // fetching 성공 시 Recoil 상태 업데이트
-  //       setUserInfo(data);
-  //     },
-  //     onError: () => {
-  //       // 에러 발생 시 Recoil 상태를 null로 설정
-  //       setUserInfo(null);
-  //     },
-  //   },
-  // );
 
-  // return {
-  //   userInfo: isUserDataError ? null : userInfo,
-  // };
   const { data: userInfo, error } = useQuery(
     queryOptions({
       queryKey: ['userInfo', token],
