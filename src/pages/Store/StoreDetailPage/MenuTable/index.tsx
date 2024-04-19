a2a38f3421e75df7ce9d76b30492b487cb37e34b
Daeeui Kim <kimeodml@gmail.com>
2024-04-19 14:30:16 +0900

    fix: 버튼 스크롤 생성

--- src/pages/Store/StoreDetailPage/MenuTable/index.tsx
+++ src/pages/Store/StoreDetailPage/MenuTable/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
 import { MenuCategory } from 'api/store/entity';
 import { cn } from '@bcsdlab/utils';
 import MENU_CATEGORY from 'static/menu';
@@ -12,23 +13,53 @@ interface MenuTableProps {
 
 function MenuTable({ storeMenuCategories, onClickImage }: MenuTableProps) {
   const isMobile = useMediaQuery();
-  const [categoryType, setCateogoryType] = useState<string>(storeMenuCategories[0].name);
+  const [categoryType, setCategoryType] = useState<string>(storeMenuCategories[0].name);
+  const headerOffset = isMobile ? 120 : 60;
+
+  const handleScroll = () => {
+    storeMenuCategories.forEach((menu) => {
+      const element = document.getElementById(menu.name);
+      if (element) {
+        const categoryTop = element.getBoundingClientRect().top;
+        const categoryBottom = element.getBoundingClientRect().bottom;
+        if (categoryTop <= 0 && categoryBottom >= 0) {
+          setCategoryType(menu.name);
+        }
+      }
+    });
+  };
+
+  useEffect(() => {
+    storeMenuCategories.forEach((menu) => {
+      const element = document.getElementById(menu.name);
+      element!.addEventListener('wheel', handleScroll);
+    });
+
+    return () => {
+      storeMenuCategories.forEach((menu) => {
+        const element = document.getElementById(menu.name);
+        if (element) {
+          element.removeEventListener('wheel', handleScroll);
+        }
+      });
+    };
+  }, [storeMenuCategories]);
 
   const scrollToTarget = (name: string) => {
-    const headerOffset = isMobile ? 120 : 60;
     const element = document.getElementById(name);
     if (element) {
       const elementPosistion = element.getBoundingClientRect().top;
-      const offsetPosition = elementPosistion + window.scrollY - headerOffset;
+      const categoryPosition = elementPosistion + window.scrollY - headerOffset;
+
       window.scrollTo({
-        top: offsetPosition,
+        top: categoryPosition,
         behavior: 'smooth',
       });
     }
   };
 
   return (
-    <>
+    <div className={styles.table}>
       <ul className={styles.categories}>
         {MENU_CATEGORY.map((menuCategories) => (
           <li key={menuCategories.id}>
@@ -40,7 +71,7 @@ function MenuTable({ storeMenuCategories, onClickImage }: MenuTableProps) {
               })}
               type="button"
               onClick={() => {
-                setCateogoryType(menuCategories.name);
+                setCategoryType(menuCategories.name);
                 scrollToTarget(menuCategories.name);
               }}
             >
@@ -49,25 +80,54 @@ function MenuTable({ storeMenuCategories, onClickImage }: MenuTableProps) {
           </li>
         ))}
       </ul>
-      <div className={styles.table}>
-        {storeMenuCategories.map((menuCategories) => (
-          <div
-            id={`${menuCategories.name}`}
-            className={styles.menu}
-            key={menuCategories.id}
-            // ref={(element) => { elementsRef.current[index] = element; }}
-          >
-            {MENU_CATEGORY.map((category) => (
-              category.name === menuCategories.name && (
-                <div className={styles.menu__title} key={category.id}>
-                  <img src={category.img} alt={category.name} />
-                  {menuCategories.name}
+      {storeMenuCategories.map((menuCategories) => (
+        <div
+          id={`${menuCategories.name}`}
+          className={styles.menu}
+          key={menuCategories.name}
+        >
+          {MENU_CATEGORY.map((category) => (
+            category.name === menuCategories.name && (
+            <div className={styles.menu__title} key={category.id}>
+              <img src={category.img} alt={category.name} />
+              {menuCategories.name}
+            </div>
+            )
+          ))}
+          {menuCategories.menus.map((menu) => (
+            menu.option_prices === null ? (
+              <div className={styles['menu-info']} key={menu.id}>
+                {menu.image_urls.length > 0 ? (
+                  menu.image_urls.map((img, idx) => (
+                    <div key={`${img}`} className={styles.image}>
+                      <button
+                        className={styles.image__button}
+                        type="button"
+                        onClick={() => onClickImage(menu.image_urls, idx)}
+                      >
+                        <img src={`${img}`} alt={`${menu.name}`} />
+                      </button>
+                    </div>
+                  ))) : (
+                    <div className={styles['empty-image']}>
+                      <div>
+                        <img width="54px" height="50px" src="https://static.koreatech.in/assets/img/mainlogo2.png" alt="빈 이미지" />
+                      </div>
+                    </div>
+                )}
+                <div className={styles['menu-info__card']}>
+                  <span title={menu.name}>{menu.name}</span>
+                  <span>
+                    {!!menu.single_price && (
+                      menu.single_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                    )}
+                    원
+                  </span>
                 </div>
-              )
-            ))}
-            {menuCategories.menus.map((menu) => (
-              menu.option_prices === null ? (
-                <div className={styles['menu-info']} key={menu.id}>
+              </div>
+            ) : (
+              menu.option_prices.map((item) => (
+                <div className={styles['menu-info']} key={menu.id + item.option}>
                   {menu.image_urls.length > 0 ? (
                     menu.image_urls.map((img, idx) => (
                       <div key={`${img}`} className={styles.image}>
@@ -87,51 +147,19 @@ function MenuTable({ storeMenuCategories, onClickImage }: MenuTableProps) {
                       </div>
                   )}
                   <div className={styles['menu-info__card']}>
-                    <span title={menu.name}>{menu.name}</span>
+                    <span>{`${menu.name} - ${item.option}`}</span>
                     <span>
-                      {!!menu.single_price && (
-                        menu.single_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-                      )}
+                      {item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
                       원
                     </span>
                   </div>
                 </div>
-              ) : (
-                menu.option_prices.map((item) => (
-                  <div className={styles['menu-info']} key={menu.id + item.option}>
-                    {menu.image_urls.length > 0 ? (
-                      menu.image_urls.map((img, idx) => (
-                        <div key={`${img}`} className={styles.image}>
-                          <button
-                            className={styles.image__button}
-                            type="button"
-                            onClick={() => onClickImage(menu.image_urls, idx)}
-                          >
-                            <img src={`${img}`} alt={`${menu.name}`} />
-                          </button>
-                        </div>
-                      ))) : (
-                        <div className={styles['empty-image']}>
-                          <div>
-                            <img width="54px" height="50px" src="https://static.koreatech.in/assets/img/mainlogo2.png" alt="빈 이미지" />
-                          </div>
-                        </div>
-                    )}
-                    <div className={styles['menu-info__card']}>
-                      <span>{`${menu.name} - ${item.option}`}</span>
-                      <span>
-                        {item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-                        원
-                      </span>
-                    </div>
-                  </div>
-                ))
-              )
-            ))}
-          </div>
-        ))}
-      </div>
-    </>
+              ))
+            )
+          ))}
+        </div>
+      ))}
+    </div>
   );
 }
 
