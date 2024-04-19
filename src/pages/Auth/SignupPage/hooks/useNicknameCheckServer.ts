0713c4a971c03a3409ad7f84137add170cae8ef4
최원빈 <wonbeen@tossbank.com>
2024-04-20 03:38:17 +0900

    refactor: 닉네임 중복체크 관련 로직 리팩토링

--- src/pages/Auth/SignupPage/hooks/useNicknameCheckServer.ts
+++ src/pages/Auth/SignupPage/hooks/useNicknameCheckServer.ts
@@ -1,45 +1,32 @@
-import { useQuery } from '@tanstack/react-query';
+import { isKoinError, sendClientError } from '@bcsdlab/koin';
+import { useMutation } from '@tanstack/react-query';
 import { nicknameDuplicateCheck } from 'api/auth';
-import { NicknameDuplicateCheckResponse } from 'api/auth/entity';
-import { KoinError } from 'interfaces/APIError';
-import { useEffect } from 'react';
 
 import showToast from 'utils/ts/showToast';
 
-const NICKNAME_DUPLICATE_CHECK_KEY = 'nickname-duplicate-check';
-
-function useNicknameCheckServer(nickname: string) {
-  const { data, error, status } = useQuery<
-  Awaited<ReturnType<typeof nicknameDuplicateCheck>>,
-  KoinError,
-  NicknameDuplicateCheckResponse | undefined,
-  [string, string]
-  >(
-    {
-      queryKey: [NICKNAME_DUPLICATE_CHECK_KEY, nickname],
-      queryFn: async ({ queryKey }) => {
-        const [, nicknameParam] = queryKey;
-        return nicknameDuplicateCheck(nicknameParam);
-      },
-      enabled: !!nickname,
-    },
-  );
-
-  useEffect(() => {
-    if (status === 'success') {
+function useNicknameCheckServer() {
+  const { data, status, mutate } = useMutation({
+    mutationFn: nicknameDuplicateCheck,
+    onSuccess: () => {
       showToast('success', '사용 가능한 닉네임입니다.');
-    } else if (status === 'error') {
-      if (error?.status === 409) {
-        showToast('error', '사용 불가능한 닉네임입니다.');
-      } else if (error?.status === 412) {
-        showToast('error', '올바르지 않은 닉네임 형식입니다.');
-      } else {
+    },
+    onError: (error) => {
+      if (isKoinError(error)) {
+        if (error.status === 409) {
+          showToast('error', '사용 불가능한 닉네임입니다.');
+          return;
+        }
+        if (error.status === 412) {
+          showToast('error', '올바르지 않은 닉네임 형식입니다.');
+          return;
+        }
+        sendClientError(error);
         showToast('error', '네트워크 연결을 확인해주세요.');
       }
-    }
-  }, [status, error]);
+    },
+  });
 
-  return { data, error, status };
+  return { data, status, mutate };
 }
 
 export default useNicknameCheckServer;
