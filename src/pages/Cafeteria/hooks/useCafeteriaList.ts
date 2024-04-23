12657ecd3cfe9fb7fc95fafad8aedb2bc74b863f
정해성 <gotjd0230@naver.com>
2024-04-23 17:39:04 +0900

    refactor: fetcher 단순화

--- src/pages/Cafeteria/hooks/useCafeteriaList.ts
+++ src/pages/Cafeteria/hooks/useCafeteriaList.ts
@@ -1,24 +1,14 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { cafeteria } from 'api';
-import { type KoinError } from 'interfaces/APIError';
 import { CafeteriaMenu } from 'interfaces/Cafeteria';
 
 const CAFETERIA_LIST_KEY = 'CAFETERIA_LIST_KEY';
 
 function useCafeteriaList(date: string) {
-  const { data: cafeteriaList } = useSuspenseQuery<
-  Awaited<ReturnType<typeof cafeteria.default>>,
-  KoinError,
-  Array<CafeteriaMenu>,
-  [string, string]
-  >(
+  const { data: cafeteriaList } = useSuspenseQuery(
     {
       queryKey: [CAFETERIA_LIST_KEY, date],
-      queryFn: async ({ queryKey }) => {
-        const [, queryDate] = queryKey;
-
-        return cafeteria.default(queryDate);
-      },
+      queryFn: async () => cafeteria.default(date),
       select: (data) => {
         if ('status' in data || !Array.isArray(data)) {
           return [];
