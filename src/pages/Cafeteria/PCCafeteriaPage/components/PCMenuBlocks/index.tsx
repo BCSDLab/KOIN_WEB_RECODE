b7aa5723b63ffe2149c64ba900f8f0fa27830101
정해성 <gotjd0230@naver.com>
2024-04-28 01:23:40 +0900

    feat: 메뉴 카드 스타일 구현, 스타일 적용

--- src/pages/Cafeteria/PCCafeteriaPage/components/PCMenuBlocks/index.tsx
+++ src/pages/Cafeteria/PCCafeteriaPage/components/PCMenuBlocks/index.tsx
@@ -3,7 +3,24 @@ import styles from './PCMenuBlocks.module.scss';
 export default function PCMenuBlocks() {
   return (
     <div className={styles.container}>
-      .
+      <div className={styles.header}>
+        <div className={styles.header__place}>A코너</div>
+        <div className={styles.header__detail}>786kcal • 1000원/1000원</div>
+        <span className={styles.header__chip} />
+      </div>
+
+      <div className={styles.content}>
+        <img
+          className={styles.content__image}
+          src="https://via.placeholder.com/150"
+          alt="menu"
+        />
+        <div className={styles.content__menus}>
+          <div className={styles.content__menu}>메뉴 이름</div>
+          <div className={styles.content__menu}>메뉴 이름</div>
+          <div className={styles.content__menu}>메뉴 이름</div>
+        </div>
+      </div>
     </div>
   );
 }
