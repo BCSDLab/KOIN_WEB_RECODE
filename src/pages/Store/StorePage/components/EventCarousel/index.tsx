918145abcc293a3d10ff88cf6118725e30a78e3c
Daeeui Kim <kimeodml@gmail.com>
2024-04-29 19:19:25 +0900

    fix: 이벤트 배너 디자인 수정

--- src/pages/Store/StorePage/components/EventCarousel/index.tsx
+++ src/pages/Store/StorePage/components/EventCarousel/index.tsx
@@ -1,18 +1,16 @@
 import { useState, useEffect } from 'react';
-import { ReactComponent as EmptyImageIcon } from 'assets/svg/empty-thumbnail.svg';
 import { useGetAllEvents } from 'pages/Store/StorePage/components/hooks/useGetAllEvents';
 import { useNavigate } from 'react-router-dom';
 import useMediaQuery from 'utils/hooks/useMediaQuery';
 import styles from './EventCarousel.module.scss';
 
-const colors = ['#6DBBDD', '#4590BB', '#175C8E', '#10477A'];
+const SLIDE_WIDTH = 387; // width + gap
 
 export default function EventCarousel() {
   const isMobile = useMediaQuery();
   const carouselList = useGetAllEvents();
   const navigate = useNavigate();
   const [currentIndex, setCurrentIndex] = useState(0);
-  const slideWidth = isMobile ? 266 : 387; // width + gap
 
   useEffect(() => {
     const slideTime = setTimeout(() => {
@@ -25,38 +23,36 @@ export default function EventCarousel() {
     };
   }, [carouselList, currentIndex]);
 
+  const transfromValue = isMobile ? `translateX(-${100 * currentIndex}vw)` : `translateX(-${SLIDE_WIDTH * currentIndex}px)`;
+
   return (
     <div className={styles.container}>
-      {carouselList && carouselList.map((item, idx) => (
-        <div key={item.content}>
-          <button
-            type="button"
-            className={styles['swipe-item']}
-            style={{ backgroundColor: `${colors[Math.abs(((idx + 1) % 4) - 2)]}`, transform: `translateX(-${slideWidth * currentIndex}px)` }}
-            onClick={() => navigate(`/store/${item.shop_id}`)}
-          >
-            {item.thumbnail_images.length > 0 ? (
-              <img
-                src={item.thumbnail_images[0]}
-                alt="가게 이미지"
-                className={styles['swipe-item__image']}
-              />
-            ) : (
-              <div className={styles['swipe-item__empty-image']}>
-                <EmptyImageIcon />
-              </div>
-            ) }
-            <div className={styles['swipe-item__text']}>
-              <div style={{ textAlign: 'start' }}>
-                <span className={styles['swipe-item__name']}>
-                  {item.shop_name}
-                </span>
-                {' 에서'}
-              </div>
-              <div className={styles['swipe-item__nowrap']}>할인 혜택을 받아보세요!</div>
+      {carouselList && carouselList.map((item) => (
+        <button
+          type="button"
+          className={styles['swipe-item']}
+          style={{ transform: transfromValue }}
+          onClick={() => navigate(`/store/${item.shop_id}`)}
+        >
+          {item.thumbnail_images.length > 0 ? (
+            <div className={styles['swipe-item__image']}>
+              <img src={item.thumbnail_images[0]} alt="가게 이미지" />
             </div>
-          </button>
-        </div>
+          ) : (
+            <div className={styles['swipe-item__empty-image']}>
+              <img src="http://static.koreatech.in/assets/img/rectangle_icon.png" alt="썸네일 없음" />
+            </div>
+          ) }
+          <div className={styles['swipe-item__text']}>
+            <div>
+              <span className={styles['swipe-item__name']}>
+                {item.shop_name}
+              </span>
+              {' 에서'}
+            </div>
+            <div className={styles['swipe-item__nowrap']}>할인 혜택을 받아보세요!</div>
+          </div>
+        </button>
       ))}
     </div>
   );
