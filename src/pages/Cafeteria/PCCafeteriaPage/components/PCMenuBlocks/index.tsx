cac261605e9ddef2191c2d575c8dc1936f267f86
정해성 <gotjd0230@naver.com>
2024-04-29 10:25:48 +0900

    feat: masonry 스타일 적용

--- src/pages/Cafeteria/PCCafeteriaPage/components/PCMenuBlocks/index.tsx
+++ src/pages/Cafeteria/PCCafeteriaPage/components/PCMenuBlocks/index.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable no-param-reassign */
 import { CafeteriaMenu, MealType } from 'interfaces/Cafeteria';
 import { ReactComponent as NoPhoto } from 'assets/svg/no-photography-pc.svg';
+import { useEffect, useRef } from 'react';
 import styles from './PCMenuBlocks.module.scss';
 
 const order = ['A코너', 'B코너', 'C코너', '능수관', '2캠퍼스'];
@@ -14,14 +16,33 @@ export default function PCMenuBlocks({ mealType, cafeteriaList }: Props) {
   const sortedCafeteriaList = filteredCafeteriaList.sort((a, b) => {
     const indexA = order.indexOf(a.place);
     const indexB = order.indexOf(b.place);
-
     return indexA - indexB;
   });
 
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const blocks = containerRef.current.children;
+      const columnHeights = [0, 0, 0];
+      let columnIndex = 0;
+
+      Array.from(blocks).forEach((block) => {
+        const x = columnIndex * (276 + 12); // 열 인덱스에 따라 x 위치 계산
+        const y = columnHeights[columnIndex]; // 현재 열의 높이에서 시작
+        (block as HTMLElement).style.transform = `translate(${x}px, ${y}px)`;
+        columnHeights[columnIndex] += (block as HTMLElement).clientHeight + 12; // 열 높이 업데이트
+        columnIndex = (columnIndex + 1) % columnHeights.length; // 다음 열 인덱스로 업데이트
+      });
+
+      containerRef.current.style.height = `${Math.max(...columnHeights)}px`; // 컨테이너의 높이 업데이트
+    }
+  }, [sortedCafeteriaList]);
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={containerRef}>
       {sortedCafeteriaList.map((item) => (
-        <div className={styles.block}>
+        <div className={styles.block} key={item.id}>
           <div className={styles.header}>
             <div className={styles.header__place}>{item.place}</div>
             <div className={styles.header__detail}>
@@ -43,7 +64,7 @@ export default function PCMenuBlocks({ mealType, cafeteriaList }: Props) {
             ) : <span className={styles.content__image}><NoPhoto /></span>)}
             <div className={styles.content__menu}>
               {item.menu.map((dish) => (
-                <div>{dish}</div>
+                <div key={dish}>{dish}</div>
               ))}
             </div>
           </div>
