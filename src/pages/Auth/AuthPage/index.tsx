70a7c5b30b95575fadea126350a621e18d48db88
dooohun <ehgns0305@naver.com>
2024-05-04 15:46:20 +0900

    feat: Auth 페이지에 메인 페이지로 이동하는 버튼 추가

--- src/pages/Auth/AuthPage/index.tsx
+++ src/pages/Auth/AuthPage/index.tsx
@@ -4,9 +4,12 @@ import styles from './Auth.module.scss';
 function AuthPage() {
   return (
     <div className={styles.template}>
+      <Link className={styles['template__left-arrow']} to="/">
+        <img className={styles['template__left-arrow-image']} src="https://static.koreatech.in/assets/ic-room/left-arrow.png" alt="메인화면으로 이동" />
+      </Link>
       <div className={styles.template__content}>
         <Link className={styles.template__logo} to="/">
-          <img className={styles.template__img} src="https://static.koreatech.in/assets/img/logo_primary.png" alt="logo" />
+          <img className={styles.template__image} src="https://static.koreatech.in/assets/img/logo_primary.png" alt="logo" />
         </Link>
         <Outlet />
       </div>
