ba39531956da48787de641aa0e04f1e29bfc8295
chaeseungyun <thslr57@naver.com>
2024-05-07 17:16:04 +0900

    feat: 스크롤 로깅 훅 추가

---
+++ src/utils/hooks/useScrollLogging.ts
@@ -0,0 +1,30 @@
+import { StoreCategoriesResponse, StoreDetailResponse } from 'api/store/entity';
+import { useEffect, useState } from 'react';
+import useLogger from './useLogger';
+import useParamsHandler from './useParamsHandler';
+
+export const useScorllLogging = (
+  title: string,
+  data: StoreCategoriesResponse | StoreDetailResponse | undefined,
+) => {
+  const [currentHeignt, setCurrentHeight] = useState<number>(window.scrollY);
+  const { params } = useParamsHandler();
+  const logger = useLogger();
+  const onScroll = () => setCurrentHeight(window.scrollY);
+
+  useEffect(() => {
+    if (document.body.scrollHeight * 0.7 > currentHeignt) {
+      window.addEventListener('scroll', onScroll);
+    }
+    if (document.body.scrollHeight * 0.7 < currentHeignt && data) {
+      const currentCategoryId = Number(params.category);
+      if ('delivery_price' in data) {
+        logger.actionEventClick({ actionTitle: 'BUSINESS', title, value: `search in ${data.name}` });
+      }
+      if ('total_count' in data) {
+        logger.actionEventClick({ actionTitle: 'BUSINESS', title, value: `search in ${data.shop_categories[currentCategoryId].name}` });
+      }
+    }
+    return () => window.removeEventListener('scroll', onScroll); // 웹 사이트 높이의 70퍼센트를 넘을 때 로깅
+  }, [currentHeignt, logger, data, params.category, title]);
+};
