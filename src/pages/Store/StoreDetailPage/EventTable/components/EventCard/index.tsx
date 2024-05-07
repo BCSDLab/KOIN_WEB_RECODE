a073d65a20e25970f10d540d9e307f9d67cdc723
(hoooooony) <(cjh41820@gmail.com)>
2024-05-07 17:08:03 +0900

    feat : 기본 이미지 추가

--- src/pages/Store/StoreDetailPage/EventTable/components/EventCard/index.tsx
+++ src/pages/Store/StoreDetailPage/EventTable/components/EventCard/index.tsx
@@ -13,14 +13,9 @@ export default function EventCard({ event }: { event: StoreEvent }) {
       setHiddenInfo(false);
     } else setHiddenInfo(true);
   };
-  return (
-    <div
-      className={cn({
-        [styles.eventCard]: true,
-        [styles['eventCard--nonHidden']]: hiddenInfo === false,
-      })}
-    >
-      {event.thumbnail_images ? (
+  const renderThumbnail = () => {
+    if (event.thumbnail_images.length > 0) {
+      return (
         <img
           src={event.thumbnail_images[0]}
           alt={event.title}
@@ -29,16 +24,38 @@ export default function EventCard({ event }: { event: StoreEvent }) {
             [styles['eventThumbail--nonHidden']]: hiddenInfo === false,
           })}
         />
-      ) : (
+      );
+    }
+    if (hiddenInfo) {
+      return (
         <img
-          src="https://static.koreatech.in/assets/img/empty-thumbnail.png"
+          src="https://static.koreatech.in/assets/img/mainlogo2.png"
           alt="KOIN service logo"
-          className={cn({
-            [styles.eventThumbail]: true,
-            [styles['eventThumbail--nonHidden']]: hiddenInfo === false,
-          })}
+          className={styles.eventThumbail}
         />
-      )}
+      );
+    }
+
+    return (
+      <div className={styles['default-event-thumbnail-img-container']}>
+        <img
+          className={styles['default-event-thumbnail-img']}
+          src="https://static.koreatech.in/assets/img/shop-event-thumbnail-default-img.png"
+          alt="이미지를 준비중 입니다."
+        />
+        <div className={styles['default-event-thumbnail-text']}>사장님이 이미지를 준비 중입니다.</div>
+      </div>
+    );
+  };
+
+  return (
+    <div
+      className={cn({
+        [styles.eventCard]: true,
+        [styles['eventCard--nonHidden']]: hiddenInfo === false,
+      })}
+    >
+      {renderThumbnail()}
       <div className={styles['event-info']}>
         <div className={styles['event-info__header']}>
           <div className={styles.title}>{event.title}</div>
