f6e044cfe2876cb48b760f9c0c74322289519711
hana <rlagksk8428@gmail.com>
2024-05-18 23:32:04 +0900

    fix: mainpage button 수정

--- src/pages/TimetablePage/components/MyLectureTimetable/index.tsx
+++ src/pages/TimetablePage/components/MyLectureTimetable/index.tsx
@@ -6,6 +6,7 @@ import useImageDownload from 'utils/hooks/useImageDownload';
 import useTimetableDayList from 'utils/hooks/useTimetableDayList';
 import { useSemester } from 'utils/zustand/semester';
 import { useTempLecture } from 'utils/zustand/myTempLecture';
+import { useNavigate } from 'react-router-dom';
 import styles from '../../DefaultPage/DefaultPage.module.scss';
 import Timetable from '../../../../components/TimetablePage/Timetable';
 import useLectureList from '../../hooks/useLectureList';
@@ -13,7 +14,7 @@ import useMyLectures from '../../hooks/useMyLectures';
 
 export default function MainTimetable() {
   const { myLectures } = useMyLectures();
-
+  const navigate = useNavigate();
   const { onImageDownload: onTimetableImageDownload, divRef: timetableRef } = useImageDownload();
   const semester = useSemester();
   const tempLecture = useTempLecture();
@@ -30,6 +31,8 @@ export default function MainTimetable() {
   return (
     <div className={styles['page__timetable-wrap']}>
       <div className={styles.page__filter}>
+        {/* TODO: CurriculumListBox 수정 필요 */}
+        {/* <CurriculumListBox /> */}
         <button
           type="button"
           className={styles.page__button}
@@ -37,6 +40,14 @@ export default function MainTimetable() {
         >
           <img src="https://static.koreatech.in/assets/img/ic-image.png" alt="이미지" />
           이미지로 저장하기
+        </button>
+        <button
+          type="button"
+          className={styles.page__button}
+          onClick={() => navigate('/timetable-edit')}
+        >
+          <img src="https://static.koreatech.in/assets/img/ic-image.png" alt="이미지" />
+          시간표 수정
         </button>
       </div>
       <div ref={timetableRef} className={styles.page__timetable}>
