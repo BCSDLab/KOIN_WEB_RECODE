5428f6e1f6d511afed36009e278572dce238ce57
dooohun <ehgns0305@naver.com>
2024-05-26 14:35:35 +0900

    refactor: 학기 설정 가능함으로 원상태로 복귀

--- src/pages/Timetable/hooks/useMyLectures.ts
+++ src/pages/Timetable/hooks/useMyLectures.ts
@@ -1,17 +1,11 @@
-import { useEffect } from 'react';
 import useTokenState from 'utils/hooks/useTokenState';
 import { useLecturesState } from 'utils/zustand/myLectures';
-import { useSemester, useSemesterAction } from 'utils/zustand/semester';
+import { useSemester } from 'utils/zustand/semester';
 import useTimetableInfoList from './useTimetableInfoList';
 
 export default function useMyLectures() {
   const token = useTokenState();
   const semester = useSemester();
-  // 현재 학기를 선택하지 못하므로 임시로 2024-1로 설정
-  const { updateSemester } = useSemesterAction();
-  useEffect(() => {
-    updateSemester('20241');
-  }, [updateSemester]);
   const myLecturesFromLocalStorageValue = useLecturesState();
   const { data: myLecturesFromServer } = useTimetableInfoList(semester, token);
 
