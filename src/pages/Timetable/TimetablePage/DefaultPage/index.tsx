6a8fbb496451857748143f3bf94ec1b6b8f6a5e1
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-06-08 17:15:31 +0900

    Update src/pages/Timetable/TimetablePage/DefaultPage/index.tsx
    Co-authored-by: 해진 <86779590+haejinyun@users.noreply.github.com>

--- src/pages/Timetable/TimetablePage/DefaultPage/index.tsx
+++ src/pages/Timetable/TimetablePage/DefaultPage/index.tsx
@@ -3,7 +3,7 @@ import LoadingSpinner from 'components/common/LoadingSpinner';
 import TimetableHeader from 'pages/Timetable/components/TimetableHeader';
 import React, { Suspense } from 'react';
 import TimetableList from 'pages/Timetable/components/TimetableList';
-import MainTimetable from '../../components/MyLectureTimetable';
+import MainTimetable from 'pages/Timetable/components/MyLectureTimetable';
 import styles from './DefaultPage.module.scss';
 
 export default function DefaultPage() {
