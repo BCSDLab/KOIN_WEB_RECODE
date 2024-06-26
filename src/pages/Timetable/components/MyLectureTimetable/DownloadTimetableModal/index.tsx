d2c68006c512bbb167651f685ab96280c0b01e2b
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-06-26 17:15:55 +0900

    feat: 모달 구현(UI 초안)

--- src/pages/Timetable/components/MyLectureTimetable/DownloadTimetableModal/index.tsx
+++ src/pages/Timetable/components/MyLectureTimetable/DownloadTimetableModal/index.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
 import Timetable from 'components/TimetablePage/Timetable';
 import useImageDownload from 'utils/hooks/useImageDownload';
 import useMyLectures from 'pages/Timetable/hooks/useMyLectures';
 import useTimetableDayList from 'utils/hooks/useTimetableDayList';
+import { ReactComponent as CloseIcon } from 'assets/svg/close-icon-black.svg';
 import { useTimeString } from 'utils/zustand/myLectures';
 import styles from './DownloadTimetableModal.module.scss';
 
@@ -57,12 +59,42 @@ export default function DownloadTimetableModal({
     onClose();
   };
   const { timeString } = useTimeString();
+  const [selectedUsage, setSelectedUsage] = React.useState('PC');
+  const handleRadioChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedUsage(e.target.value);
+  };
 
   return (
     <div className={styles.background} aria-hidden>
       <div className={styles.container}>
-        <button type="button" className={styles.container__button} onClick={() => onClickImageDownload('PC')}>PC용</button>
-        <button type="button" className={styles.container__button} onClick={() => onClickImageDownload('Mobile')}>모바일용</button>
+        <div className={styles.container__header}>
+          <div className={styles['container__header--text']}>시간표 이미지 저장</div>
+          <CloseIcon onClick={onClose} className={styles['container__header--close-button']} />
+        </div>
+        <div className={styles['container__image-option']}>이미지 옵션</div>
+        <div className={styles['container__option-select']}>
+          <input
+            type="radio"
+            id="PC"
+            name="saveUsage"
+            value="PC"
+            onChange={handleRadioChange}
+            checked={selectedUsage === 'PC'}
+          />
+          <label htmlFor="PC" className={styles['container__radio-button']}>PC용</label>
+          <input
+            type="radio"
+            id="Mobile"
+            name="saveUsage"
+            value="Mobile"
+            onChange={handleRadioChange}
+            checked={selectedUsage === 'Mobile'}
+          />
+          <label htmlFor="Mobile" className={styles['container__radio-button']}>모바일용</label>
+        </div>
+        <div className={styles.container__button}>
+          <button type="button" className={styles['container__button--download']} onClick={() => onClickImageDownload(selectedUsage)}>저장하기</button>
+        </div>
       </div>
       <div ref={pcTimetableRef} className={styles['container__timetable-image']}>
         <TimetableForPCDownload rowNumber={timeString.length} />
