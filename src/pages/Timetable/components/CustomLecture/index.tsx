e1d7806a078bae1812004b59ac149ca228228990
dbshaejin <dbshaejin@gmail.com>
2024-06-26 12:40:05 +0900

    feat: 시간 listBox 디자인 추가 및 필수 아이콘 수정

--- src/pages/Timetable/components/CustomLecture/index.tsx
+++ src/pages/Timetable/components/CustomLecture/index.tsx
@@ -32,13 +32,15 @@ function CustomLecture() {
   const onChangeMin = () => {
 
   };
-
+  // 해당 파트의 api 명세가 어떻게 나오느냐에 따라 html 구조의 변동이 있을 수 있을 것 같습니다.
   return (
     <form onSubmit={handleAddLecture} className={styles['form-container']}>
       <div className={styles['form-group']}>
         <label htmlFor="courseName">
-          <span className={styles['require-mark']}>*</span>
-          수업명
+          <div className={styles['form-group__title']}>
+            수업명
+            <span className={styles['require-mark']}>*</span>
+          </div>
         </label>
         <div className={styles['form-group__block']} />
         <input type="text" id="courseName" name="courseName" placeholder="수업명을 입력하세요." />
@@ -48,12 +50,12 @@ function CustomLecture() {
         <div className={styles['form-group__block']} />
         <input type="text" name="professorName" placeholder="수업명을 입력하세요." />
       </div>
-
-      {/* 해당 파트의 api 명세가 어떻게 나오느냐에 따라 html 구조의 변동이 있을 수 있을 것 같습니다. */}
       <div className={styles['form-group-time']}>
         <label htmlFor="place">
-          <span className={styles['require-mark']}>*</span>
-          시간
+          <div className={styles['form-group__title']}>
+            시간
+            <span className={styles['require-mark']}>*</span>
+          </div>
         </label>
         <div className={styles['form-group-time__container']}>
           <div className={styles['form-group-time__weekdays']}>
@@ -65,24 +67,22 @@ function CustomLecture() {
           </div>
           <div className={styles['form-group-time__time']}>
             <div className={styles['form-group-time__time-section']}>
-              <Listbox list={HOUR} value="00시" onChange={onChangeHours} version="inModal" />
-              <Listbox list={MIN} value="00분" onChange={onChangeMin} version="inModal" />
+              <Listbox list={HOUR} value="00시" onChange={onChangeHours} version="addLecture" />
+              <Listbox list={MIN} value="00분" onChange={onChangeMin} version="addLecture" />
             </div>
             <span>-</span>
             <div className={styles['form-group-time__time-section']}>
-              <Listbox list={HOUR} value="00시" onChange={onChangeHours} version="inModal" />
-              <Listbox list={MIN} value="00분" onChange={onChangeMin} version="inModal" />
+              <Listbox list={HOUR} value="00시" onChange={onChangeHours} version="addLecture" />
+              <Listbox list={MIN} value="00분" onChange={onChangeMin} version="addLecture" />
             </div>
           </div>
         </div>
       </div>
-
       <div className={styles['form-group']}>
         <label htmlFor="place">장소</label>
         <div className={styles['form-group__block']} />
         <input type="text" name="place" placeholder="장소를 입력하세요." />
       </div>
-
       <button type="button" className={styles['form-group-add-button']}>
         <span>시간 및 장소 추가</span>
         <AddIcon />
