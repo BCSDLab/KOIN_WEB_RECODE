e2a938ed11ea59ade3aa4869c268cc7afd59b162
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-07-04 02:37:26 +0900

    remove: 주석 제거

--- src/utils/zustand/myTempLecture.ts
+++ src/utils/zustand/myTempLecture.ts
@@ -1,33 +1,6 @@
 import { LectureInfo } from 'interfaces/Lecture';
 import { create } from 'zustand';
 
-// type State = {
-//   tempLecture: LectureInfo | null;
-// };
-//
-// type Action = {
-//   action: {
-//     updateTempLecture: (tempLecture: State['tempLecture']) => void
-//   }
-// };
-//
-// const useTempLectureStore = create<State & Action>((set, get) => ({
-//   tempLecture: null,
-//   action: {
-//     updateTempLecture: (tempLecture) => {
-//       if (get().tempLecture === tempLecture) {
-//         set(() => ({ tempLecture: null }));
-//         return;
-//       }
-//       set(() => ({ tempLecture }));
-//     },
-//   },
-// }));
-//
-// export const useTempLecture = () => useTempLectureStore((state) => state.tempLecture);
-//
-// export const useTempLectureAction = () => useTempLectureStore((store) => store.action);
-
 interface TempLectureState {
   tempLecture: LectureInfo | null;
   setTempLecture: (tempLecture: LectureInfo | null) => void;
