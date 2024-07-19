a00e8ce1bb84ceea70d137d6e86ad958b01264a2
정해성 <gotjd0230@naver.com>
2024-07-19 17:38:09 +0900

    refactor: 학생식당 운영시간 api 연결(web)

--- src/api/coopshop/entity.ts
+++ src/api/coopshop/entity.ts
@@ -2,23 +2,19 @@ import { type APIResponse } from 'interfaces/APIResponse';
 
 export type Semester = '학기' | '하계방학' | '동계방학';
 
-export type OpenTime = {
+export type Opens = {
   day_of_week: string, // 요일 또는 평일, 주말, 공휴일 등
   type: '아침' | '점심' | '저녁' | null,
   open_time: string | null, // HH:mm
   close_time: string | null, // HH:mm
 };
 
-export type Coopshop = {
+export interface CoopshopResponse extends APIResponse {
   id: number;
   name: string; // 학생식당, 세탁소, 복사실 등
   semester: Semester;
-  open: OpenTime[];
+  opens: Opens[];
   phone: string | null;
   location: string;
   remarks: string | null;
-};
-
-export interface CoopshopResponse extends APIResponse {
-  data: Coopshop;
 }
--- src/pages/Cafeteria/PCCafeteriaPage/components/CafeteriaInfo/index.tsx
+++ src/pages/Cafeteria/PCCafeteriaPage/components/CafeteriaInfo/index.tsx
@@ -1,38 +1,47 @@
 import { useEffect, useRef } from 'react';
 import { ReactComponent as CloseIcon } from 'assets/svg/close-icon-grey.svg';
 import useCoopshopCafeteria from 'pages/Cafeteria/hooks/useCoopshopCafeteria';
+import { Opens } from 'api/coopshop/entity';
 import styles from './CafeteriaInfo.module.scss';
 
-// 2024년 6월 21일 기준으로 작성된 학생식당 운영시간
-const OPERATING_SCHEDULES = {
-  semesterWeekday: [
-    { time: '아침', startTime: '8:00', endTime: '9:00' },
-    { time: '점심', startTime: '11:30', endTime: '13:30' },
-    { time: '저녁', startTime: '17:30', endTime: '18:30' },
-  ],
-  semesterWeekend: [
-    { time: '아침', startTime: '미운영', endTime: '미운영' },
-    { time: '점심', startTime: '11:30', endTime: '13:00' },
-    { time: '저녁', startTime: '17:30', endTime: '18:30' },
-  ],
-  vacationWeekday: [
-    { time: '아침', startTime: '8:00', endTime: '9:00' },
-    { time: '점심', startTime: '11:30', endTime: '13:30' },
-    { time: '저녁', startTime: '17:30', endTime: '18:30' },
-  ],
-  vacationWeekend: [
-    { time: '아침', startTime: '휴점(예약)', endTime: '휴점(예약)' },
-    { time: '점심', startTime: '11:30', endTime: '13:30' },
-    { time: '저녁', startTime: '17:30', endTime: '18:30' },
-  ],
-};
+function ScheduleTable({ title, schedules }: { title: string, schedules: Opens[] }) {
+  return (
+    <div className={styles.table}>
+      <span className={styles.table__title}>
+        {title}
+        &nbsp;
+        운영시간
+      </span>
+      <table className={styles.table__content}>
+        <thead>
+          <tr>
+            <th>시간</th>
+            <th>시작시간</th>
+            <th>마감시간</th>
+          </tr>
+        </thead>
+        <tbody>
+          {schedules.map((schedule) => (
+            <tr key={schedule.type}>
+              <td>{schedule.type}</td>
+              <td>{schedule.open_time}</td>
+              <td>{schedule.close_time}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
 interface CafeteriaInfoProps {
   closePopup: () => void;
 }
 
 export default function CafeteriaInfo({ closePopup }: CafeteriaInfoProps) {
-  const cafeteriaInfo = useCoopshopCafeteria();
+  const { cafeteriaInfo } = useCoopshopCafeteria();
+  const weekday = cafeteriaInfo.opens.filter((schedule) => schedule.day_of_week === '평일');
+  const weekend = cafeteriaInfo.opens.filter((schedule) => schedule.day_of_week === '주말');
   const backgroundRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -58,15 +67,19 @@ export default function CafeteriaInfo({ closePopup }: CafeteriaInfoProps) {
       <div className={styles.box}>
         <div className={styles.header}>
           <div className={styles.header__title}>
-            <span className={styles.header__main}>학생식당 하계방학중 운영시간</span>
-            {/* 학기중, 하계방학중, 동계방학중 */}
+            <span className={styles.header__main}>
+              {cafeteriaInfo.name}
+              &nbsp;
+              {cafeteriaInfo.semester}
+              중 운영시간
+            </span>
             <span className={styles.header__sub}>
               <span className={styles['header__sub--bold']}>위치</span>
-              학생회관 2층
+              {cafeteriaInfo.location}
             </span>
             <span className={styles.header__sub}>
               <span className={styles['header__sub--bold']}>전화번호</span>
-              560-1278
+              {cafeteriaInfo.phone}
             </span>
           </div>
           <button
@@ -79,51 +92,8 @@ export default function CafeteriaInfo({ closePopup }: CafeteriaInfoProps) {
           </button>
         </div>
 
-        <div className={styles.table}>
-          <span className={styles.table__title}>평일 운영시간</span>
-          <table className={styles.table__content}>
-            <thead>
-              <tr>
-                <th>시간</th>
-                <th>시작시간</th>
-                <th>마감시간</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* 6.21 기준 방학으로 구현함 */}
-              {OPERATING_SCHEDULES.vacationWeekday.map((hour) => (
-                <tr key={hour.time}>
-                  <td>{hour.time}</td>
-                  <td>{hour.startTime}</td>
-                  <td>{hour.endTime}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-
-        <div className={styles.table}>
-          <span className={styles.table__title}>주말 운영시간</span>
-          <table className={styles.table__content}>
-            <thead>
-              <tr>
-                <th>시간</th>
-                <th>시작시간</th>
-                <th>마감시간</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* 6.21 기준 방학으로 구현함 */}
-              {OPERATING_SCHEDULES.vacationWeekend.map((hour) => (
-                <tr key={hour.time}>
-                  <td>{hour.time}</td>
-                  <td>{hour.startTime}</td>
-                  <td>{hour.endTime}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        <ScheduleTable title="평일" schedules={weekday} />
+        <ScheduleTable title="주말" schedules={weekend} />
       </div>
     </div>
   );
