62433845eed9a9285ac3b87490f29f1a43388617
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-07-20 12:59:03 +0900

    fix: 예비수강종료로 복구

--- src/pages/TimetablePage/DefaultPage/index.tsx
+++ src/pages/TimetablePage/DefaultPage/index.tsx
@@ -1,8 +1,6 @@
 /* eslint-disable no-restricted-imports */
 import LoadingSpinner from 'components/common/LoadingSpinner';
 import React, { Suspense } from 'react';
-import useBooleanState from 'utils/hooks/useBooleanState';
-import fixesImage from 'assets/svg/fixes-image.jpg';
 import Curriculum from '../components/Curriculum';
 import LectureList from '../components/LectureList';
 import MyLectureList from '../components/MyLectureList';
@@ -10,17 +8,9 @@ import MyLectureTimetable from '../components/MyLectureTimetable';
 import styles from './DefaultPage.module.scss';
 
 export default function DefaultPage() {
-  const [isModalOpen, openModal, closeModal] = useBooleanState(false);
   return (
     <>
-      <div className={styles.page__head}>
-        <h1 className={styles.page__title}>시간표</h1>
-        <div className={styles.page__instructions}>
-          학교 측에 의해 강의 시간이 변경된 과목이 있으니 변경 전에 해당 과목을 시간표에 추가하신 학우분은
-          나의 시간표에서 삭제 후 다시 추가해주시기 바랍니다.&nbsp;
-          <button type="button" onClick={openModal} className={styles['page__instructions--button']}>변경된 강의 확인하기</button>
-        </div>
-      </div>
+      <h1 className={styles.page__title}>시간표</h1>
       <Suspense fallback={<div className={styles['central-loading-spinner']}><LoadingSpinner size="100" /></div>}>
         <div className={styles.page__content}>
           {/* 강의 목록 */}
@@ -33,17 +23,6 @@ export default function DefaultPage() {
           <Curriculum />
         </div>
       </Suspense>
-      {
-        isModalOpen
-        && (
-          <div className={styles.background} aria-hidden>
-            <div className={styles.container}>
-              <img src={fixesImage} alt="개설정보 수정사항" />
-              <button type="button" onClick={closeModal} className={styles.container__close}>닫기</button>
-            </div>
-          </div>
-        )
-      }
     </>
   );
 }
