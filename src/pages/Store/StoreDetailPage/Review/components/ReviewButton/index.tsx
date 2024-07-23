cd6d8feea211b66827d974accd2a4401896899e2
Daeeui Kim <kimeodml@gmail.com>
2024-07-23 19:51:18 +0900

    fix: 리뷰 작성하기 경로 수정

--- src/pages/Store/StoreDetailPage/Review/components/ReviewButton/index.tsx
+++ src/pages/Store/StoreDetailPage/Review/components/ReviewButton/index.tsx
@@ -1,8 +1,8 @@
 import styles from './index.module.scss';
 
-export default function ReviewButton() {
+export default function ReviewButton({ onClick }: { onClick: ()=> void }) {
   return (
-    <button type="button" className={styles.container}>
+    <button type="button" className={styles.container} onClick={onClick}>
       리뷰 작성하기
     </button>
   );
--- src/pages/Store/StoreDetailPage/Review/index.tsx
+++ src/pages/Store/StoreDetailPage/Review/index.tsx
@@ -1,13 +1,16 @@
 import { Suspense } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AverageRating from './components/AverageRating/AverageRating';
 import ReviewButton from './components/ReviewButton';
 import styles from './index.module.scss';
 
-export default function ReviewPage() {
+export default function ReviewPage({ id }: { id: string }) {
+  const navigate = useNavigate();
+
   return (
     <Suspense fallback={<div />}>
       <div className={styles.container}>
-        <ReviewButton />
+        <ReviewButton onClick={() => navigate(`/review/${id}`)} />
         <AverageRating />
       </div>
     </Suspense>
