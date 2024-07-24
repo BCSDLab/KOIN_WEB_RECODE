a32936dd03eb4abc0ce20c4bb4737987acbf999b
chaeseungyun <thslr57@naver.com>
2024-07-24 17:43:23 +0900

    refactor: 신고하기 추가

---
+++ src/pages/Store/StoreDetailPage/Review/components/SelectButton/SelectButton.tsx
@@ -0,0 +1,32 @@
+import { ReactComponent as Trash } from 'assets/svg/trash-can-icon.svg';
+import { ReactComponent as Pen } from 'assets/svg/pen-icon.svg';
+import { ReactComponent as Complaint } from 'assets/svg/Review/complaint.svg';
+import styles from './SelectButton.module.scss';
+
+interface Props {
+  is_mine: boolean;
+}
+export default function SelectButton({ is_mine }: Props) {
+  return (
+    <div className={styles.container}>
+      {is_mine ? (
+        <>
+          <div className={styles.section}>
+            수정하기
+            <Pen />
+          </div>
+          <div className={styles.section}>
+            삭제하기
+            <Trash />
+          </div>
+        </>
+      ) : (
+        <div className={styles.section}>
+          신고하기
+          {' '}
+          <Complaint />
+        </div>
+      )}
+    </div>
+  );
+}
