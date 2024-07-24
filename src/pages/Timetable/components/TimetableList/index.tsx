ac64bc90be25402d944e46a944e6e6b7b7f54468
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-07-25 04:50:50 +0900

    feat: 시간표프레임 추가/수정/삭제 구현

--- src/pages/Timetable/components/TimetableList/index.tsx
+++ src/pages/Timetable/components/TimetableList/index.tsx
@@ -5,35 +5,57 @@ import TimetableSettingModal from 'pages/Timetable/components/TimetableList/Time
 import { ReactComponent as AddIcon } from 'assets/svg/add-icon.svg';
 import { ReactComponent as SettingIcon } from 'assets/svg/setting-icon.svg';
 import SemesterListbox from 'pages/Timetable/components/SemesterList';
+import { useSemester } from 'utils/zustand/semester';
+import useTokenState from 'utils/hooks/useTokenState';
+import useTimetableFrameList from 'pages/Timetable/hooks/useTimetableFrameList';
+import { TimetableFrameInfo } from 'api/timetable/entity';
+import useAddTimetableFrame from 'pages/Timetable/hooks/useAddTimetableFrame';
 import styles from './TimetableList.module.scss';
 
 export default function TimetableList() {
   const [isModalOpen, openModal, closeModal] = useBooleanState(false);
-  /* 나의 시간표 api 만들어지면 수정될 부분 */
-  const timetableList = ['1안', '2안', '3안'];
+  const semester = useSemester();
+  const token = useTokenState();
+  const { data: timetableFrameList } = useTimetableFrameList(token, semester);
+  const [focusFrame, setFocusFrame] = React.useState<TimetableFrameInfo | null>(null);
+  const { mutate: addTimetableFrame } = useAddTimetableFrame(token);
+  const handleOpenModal = (frame: TimetableFrameInfo) => {
+    setFocusFrame(frame);
+    openModal();
+  };
 
   return (
     <div className={styles['timetable-list']}>
       <SemesterListbox />
       <ul className={styles['timetable-list__list']} role="listbox">
-        {timetableList.map((value) => (
-          <div className={styles['timetable-list__list--item']} key={value}>
+        {timetableFrameList?.map((frame) => (
+          <div className={styles['timetable-list__list--item']} key={frame.id}>
             <li>
-              {value}
+              {frame.timetable_name}
             </li>
-            <button type="button" className={styles['timetable-list__list--setting']} onClick={openModal}>
+            <button
+              type="button"
+              className={styles['timetable-list__list--setting']}
+              onClick={() => handleOpenModal(frame)}
+            >
               <SettingIcon />
               설정
             </button>
           </div>
         ))}
-        <button type="button" className={styles['timetable-list__list--add']}>
-          <div>시간표 추가하기</div>
+        <button
+          type="button"
+          className={styles['timetable-list__list--add']}
+          onClick={() => addTimetableFrame({ semester: String(semester) })}
+        >
+          시간표 추가하기
           <AddIcon />
         </button>
       </ul>
       {isModalOpen && (
         <TimetableSettingModal
+          focusFrame={focusFrame!}
+          setFocusFrame={setFocusFrame}
           onClose={closeModal}
         />
       )}
