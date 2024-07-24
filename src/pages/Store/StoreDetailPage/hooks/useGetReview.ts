b97e0c8a530ce04675535d025d25a7a9be2b8379
chaeseungyun <thslr57@naver.com>
2024-07-24 17:05:50 +0900

    refactor: 무한 스크롤을 위한 getNextPageParam 수정

--- src/pages/Store/StoreDetailPage/hooks/useGetReview.ts
+++ src/pages/Store/StoreDetailPage/hooks/useGetReview.ts
@@ -4,12 +4,19 @@ import useTokenState from 'utils/hooks/useTokenState';
 
 export const useGetReview = (id: number) => {
   const token = useTokenState();
-  const { data, hasNextPage, fetchNextPage } = useSuspenseInfiniteQuery({
+  const {
+    data, hasNextPage, fetchNextPage, isFetchingNextPage,
+  } = useSuspenseInfiniteQuery({
     queryKey: ['review', id],
     initialPageParam: 1,
     queryFn: ({ pageParam }) => getReviewList(id, pageParam, token),
-    getNextPageParam: (last) => last.current_page + 1,
+    getNextPageParam: (last) => {
+      if (last.total_page > last.current_page) return last.current_page + 1;
+      return undefined; // 마지막 페이지면 무한 스크롤 중단
+    },
   });
 
-  return { data, hasNextPage, fetchNextPage };
+  return {
+    data, hasNextPage, fetchNextPage, isFetchingNextPage,
+  };
 };
