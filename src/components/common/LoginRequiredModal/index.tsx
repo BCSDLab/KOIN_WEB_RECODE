e6bbdef10d4eade4b659414cd60c4e750c0fe56f
Daeeui Kim <kimeodml@gmail.com>
2024-08-02 02:03:50 +0900

    feat: 비회원 모달창 공용 컴포넌트로 추가

---
+++ src/components/common/LoginRequiredModal/index.tsx
@@ -0,0 +1,30 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import styles from './LoginRequiredModal.module.scss';
+
+interface Props {
+  title: string;
+  description:string;
+  closeModal:()=>void;
+}
+
+export default function LoginRequiredModal({ title = '', description = '', closeModal }: Props) {
+  const navigate = useNavigate();
+  const location = useLocation();
+  return (
+    <div className={styles.container}>
+      <div className={styles.modal}>
+        <div className={styles.modal__title}>
+          {title}
+          <span>로그인</span>
+          을 해주세요.
+        </div>
+        <div className={styles.modal__description}>{description}</div>
+        <div className={styles.modal__description}>회원가입 또는 로그인 후 이용해주세요 :-)</div>
+        <div className={styles.modal__button}>
+          <button type="button" onClick={closeModal}>취소하기</button>
+          <button type="button" onClick={() => navigate('/auth', { state: { from: location } })}>로그인하기</button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Store/StoreDetailPage/Review/components/ReviewButton/index.tsx
+++ src/pages/Store/StoreDetailPage/Review/components/ReviewButton/index.tsx
@@ -1,9 +1,41 @@
+import { createPortal } from 'react-dom';
+import LoginRequiredModal from 'components/common/LoginRequiredModal';
+import useBooleanState from 'utils/hooks/useBooleanState';
+import { useUser } from 'utils/hooks/useUser';
 import styles from './index.module.scss';
 
+const REVEIW_LOGIN = [
+  '리뷰 작성 시 ',
+  '리뷰 작성은 회원만 사용 가능합니다.',
+];
+
 export default function ReviewButton({ onClick }: { onClick: ()=> void }) {
+  const [isModalOpen, openModal, closeModal] = useBooleanState(false);
+  const { data: userInfo } = useUser();
+
   return (
-    <button type="button" className={styles.container} onClick={onClick}>
-      리뷰 작성하기
-    </button>
+    <>
+      <button
+        type="button"
+        className={styles.container}
+        onClick={() => {
+          if (userInfo) {
+            onClick();
+          } else {
+            openModal();
+          }
+        }}
+      >
+        리뷰 작성하기
+      </button>
+      {isModalOpen && createPortal(
+        <LoginRequiredModal
+          title={REVEIW_LOGIN[0]}
+          description={REVEIW_LOGIN[1]}
+          closeModal={closeModal}
+        />,
+        document.body,
+      )}
+    </>
   );
 }
