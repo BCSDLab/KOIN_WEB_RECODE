5f36e0ed32879447ddbbc9c929c25579ddcf6e80
Daeeui Kim <kimeodml@gmail.com>
2024-08-03 19:24:50 +0900

    fix: useModalPortal 훅 사용

--- src/pages/Store/StoreDetailPage/Review/components/ReviewButton/index.tsx
+++ src/pages/Store/StoreDetailPage/Review/components/ReviewButton/index.tsx
@@ -1,6 +1,6 @@
-import { createPortal } from 'react-dom';
+import { Portal } from 'components/common/Modal/PortalProvider';
 import LoginRequiredModal from 'components/common/LoginRequiredModal';
-import useBooleanState from 'utils/hooks/useBooleanState';
+import useModalPortal from 'utils/hooks/useModalPortal';
 import { useUser } from 'utils/hooks/useUser';
 import styles from './index.module.scss';
 
@@ -10,32 +10,32 @@ const REVEIW_LOGIN = [
 ];
 
 export default function ReviewButton({ goReviewPage }: { goReviewPage: ()=> void }) {
-  const [isModalOpen, openModal, closeModal] = useBooleanState(false);
   const { data: userInfo } = useUser();
+  const portalManager = useModalPortal();
+
+  const openLoginModal = () => {
+    portalManager.open((portalOption: Portal) => (
+      <LoginRequiredModal
+        title={REVEIW_LOGIN[0]}
+        description={REVEIW_LOGIN[1]}
+        closeModal={portalOption.close}
+      />
+    ));
+  };
 
   return (
-    <>
-      <button
-        type="button"
-        className={styles.container}
-        onClick={() => {
-          if (userInfo) {
-            goReviewPage();
-          } else {
-            openModal();
-          }
-        }}
-      >
-        리뷰 작성하기
-      </button>
-      {isModalOpen && createPortal(
-        <LoginRequiredModal
-          title={REVEIW_LOGIN[0]}
-          description={REVEIW_LOGIN[1]}
-          closeModal={closeModal}
-        />,
-        document.body,
-      )}
-    </>
+    <button
+      type="button"
+      className={styles.container}
+      onClick={() => {
+        if (userInfo) {
+          goReviewPage();
+        } else {
+          openLoginModal();
+        }
+      }}
+    >
+      리뷰 작성하기
+    </button>
   );
 }
