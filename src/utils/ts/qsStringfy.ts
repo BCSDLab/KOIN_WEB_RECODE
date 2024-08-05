60b3a4bb7b94d72a959a203664609f72159aaf70
murpin <qw04011@gmail.com>
2024-08-05 21:27:00 +0900

    feat: qsStringfy 타입 명시

--- src/utils/ts/qsStringfy.ts
+++ src/utils/ts/qsStringfy.ts
@@ -1,15 +1,21 @@
-export default function qsStringify(params: any, prefix?: string): string {
+type Primitive = string | number | boolean | null;
+type QueryParam = Primitive | Primitive[] | QueryParams;
+interface QueryParams {
+  [key: string]: QueryParam;
+}
+
+export default function qsStringify<T extends QueryParams>(params: T, prefix?: string): string {
   const queryString = Object.keys(params)
     .map((key) => {
       const value = params[key];
       const encodedKey = prefix ? `${prefix}[${encodeURIComponent(key)}]` : encodeURIComponent(key);
 
       if (Array.isArray(value)) {
-        return value.map((item) => `${encodedKey}=${encodeURIComponent(item)}`).join('&');
+        return value.map((item) => `${encodedKey}=${encodeURIComponent(String(item))}`).join('&');
       } if (typeof value === 'object' && value !== null) {
-        return qsStringify(value, encodedKey);
+        return qsStringify(value as QueryParams, encodedKey);
       }
-      return `${encodedKey}=${encodeURIComponent(value)}`;
+      return `${encodedKey}=${encodeURIComponent(String(value))}`;
     })
     .filter((part) => part.length > 0)
     .join('&');
