9e9beb3bdeacd7a57b24b33bc651883e6ab471b0
Daeeui Kim <kimeodml@gmail.com>
2024-08-06 16:13:42 +0900

    feat: 리뷰하기, 리뷰 수정하기 페이지 추가

---
+++ src/pages/StoreReviewPage/EditReviewPage.tsx
@@ -0,0 +1,21 @@
+import { useGetReview } from 'pages/Store/StoreDetailPage/hooks/useGetReview';
+import useStoreDetail from 'pages/Store/StoreDetailPage/hooks/useStoreDetail';
+import { useLocation, useParams } from 'react-router-dom';
+import { useEditStoreReview } from './hooks/useEditStoreReview';
+import ReviewForm from './ReviewForm';
+
+function EditReviewPage() {
+  const params = useParams();
+  const location = useLocation();
+  const reviewId = location.state.from;
+  const { storeDetail } = useStoreDetail(params.id!);
+  const { data } = useGetReview(Number(params.id));
+  const initialData = data.pages.flatMap((page) => page.reviews)
+    .filter((review) => review.review_id === reviewId);
+  const { mutate } = useEditStoreReview(String(storeDetail.id), reviewId);
+  return (
+    <ReviewForm storeDetail={storeDetail} mutate={mutate} initialData={initialData[0]} />
+  );
+}
+
+export default EditReviewPage;
