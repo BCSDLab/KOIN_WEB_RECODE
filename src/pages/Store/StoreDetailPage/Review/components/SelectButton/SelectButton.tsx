11370037913e48a273a3d83e895bec04e49c4546
(hoooooony) <(cjh41820@gmail.com)>
2024-08-07 17:54:58 +0900

    feat : 비로그인시 로그인 유도 모달

--- src/pages/Store/StoreDetailPage/Review/components/SelectButton/SelectButton.tsx
+++ src/pages/Store/StoreDetailPage/Review/components/SelectButton/SelectButton.tsx
@@ -2,6 +2,10 @@ import { ReactComponent as Trash } from 'assets/svg/trash-can-icon.svg';
 import { ReactComponent as Pen } from 'assets/svg/pen-icon.svg';
 import { ReactComponent as Complaint } from 'assets/svg/Review/complaint.svg';
 import { useNavigate, useParams } from 'react-router-dom';
+import { useUser } from 'utils/hooks/state/useUser';
+import LoginRequiredModal from 'components/common/LoginRequiredModal';
+import useModalPortal from 'utils/hooks/layout/useModalPortal';
+import { Portal } from 'components/common/Modal/PortalProvider';
 import styles from './SelectButton.module.scss';
 /* eslint-disable react/require-default-props */
 interface Props {
@@ -9,10 +13,25 @@ interface Props {
   shop_id?: number;
   review_id: number;
 }
-
+const REVEIW_REPORT_LOGIN = [
+  '리뷰 신고 시 ',
+  '리뷰 신고는 회원만 사용 가능합니다.',
+];
 export default function SelectButton({ is_mine, shop_id, review_id }: Props) {
   const params = useParams();
   const navigate = useNavigate();
+  const { data: userInfo } = useUser();
+  const portalManager = useModalPortal();
+
+  const openLoginModal = () => {
+    portalManager.open((portalOption: Portal) => (
+      <LoginRequiredModal
+        title={REVEIW_REPORT_LOGIN[0]}
+        description={REVEIW_REPORT_LOGIN[1]}
+        closeModal={portalOption.close}
+      />
+    ));
+  };
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
@@ -35,7 +54,17 @@ export default function SelectButton({ is_mine, shop_id, review_id }: Props) {
             </button>
           </>
         ) : (
-          <button type="button" className={styles.section} onClick={() => navigate(`/report/review/shopid/${shop_id}/reviewid/${review_id}`)}>
+          <button
+            type="button"
+            className={styles.section}
+            onClick={() => {
+              if (userInfo) {
+                navigate(`/report/review/shopid/${shop_id}/reviewid/${review_id}`);
+              } else {
+                openLoginModal();
+              }
+            }}
+          >
             신고하기
             {' '}
             <Complaint />
