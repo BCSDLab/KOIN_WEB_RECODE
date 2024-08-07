ffd8c11222319ef4cdeaae15079b1bafc3db5714
chaeseungyun <thslr57@naver.com>
2024-08-07 10:39:45 +0900

    refactor: 바텀 토스트 적용

--- src/pages/Store/StoreDetailPage/hooks/useDeleteReview.ts
+++ src/pages/Store/StoreDetailPage/hooks/useDeleteReview.ts
@@ -1,11 +1,16 @@
 import { useMutation } from '@tanstack/react-query';
 import { deleteReview } from 'api/store';
+import { useKoinToast } from 'utils/hooks/koinToast/useKoinToast';
 import useTokenState from 'utils/hooks/state/useTokenState';
 
 export const useDeleteReview = (shopId: string, reviewId: number) => {
   const token = useTokenState();
+  const openToast = useKoinToast();
   const mutation = useMutation({
     mutationFn: () => deleteReview(reviewId, shopId, token),
+    onSuccess: () => {
+      openToast({ message: '리뷰가 삭제되었습니다.' });
+    },
   });
 
   return mutation;
