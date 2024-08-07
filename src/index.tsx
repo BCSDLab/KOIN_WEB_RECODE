cac5a4f4714dc18b180a9d010ce0c0e7ca04adfb
(hoooooony) <(cjh41820@gmail.com)>
2024-08-07 17:49:11 +0900

    fix : lint 오류

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
-
 import PortalProvider from 'components/common/Modal/PortalProvider';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
