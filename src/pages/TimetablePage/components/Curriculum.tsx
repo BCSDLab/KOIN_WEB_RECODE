ee84ad7cf45c13041baa36c0485701c6f4b643a6
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-08-10 23:43:41 +0900

    feat: 시간표 로그 추가

--- src/pages/TimetablePage/components/Curriculum.tsx
+++ src/pages/TimetablePage/components/Curriculum.tsx
@@ -3,10 +3,12 @@ import ErrorBoundary from 'components/common/ErrorBoundary';
 import LoadingSpinner from 'components/common/LoadingSpinner';
 import useDeptList from 'pages/Auth/SignupPage/hooks/useDeptList';
 import React from 'react';
+import useLogger from 'utils/hooks/analytics/useLogger';
 import styles from '../DefaultPage/DefaultPage.module.scss';
 
 function Curriculum() {
   const { data: deptList } = useDeptList();
+  const logger = useLogger();
   return (
     <div>
       <h3 className={styles['page__title--sub']}>커리큘럼</h3>
@@ -18,6 +20,14 @@ function Curriculum() {
                 <a
                   className={styles.page__curriculum}
                   href={dept.curriculum_link}
+                  onClick={() => {
+                    logger.actionEventClick({
+                      actionTitle: 'USER',
+                      title: 'timetable',
+                      value: `click_${dept.name}`,
+                    });
+                  }}
+                  rel="noreferrer"
                 >
                   {dept.name}
                 </a>
@@ -27,6 +37,14 @@ function Curriculum() {
               <a
                 className={styles.page__curriculum}
                 href="https://www.koreatech.ac.kr/board.es?mid=a10103010000&bid=0002"
+                onClick={() => {
+                  logger.actionEventClick({
+                    actionTitle: 'USER',
+                    title: 'timetable',
+                    value: 'click_대학요람',
+                  });
+                }}
+                rel="noreferrer"
               >
                 대학 요람
               </a>
