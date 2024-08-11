05bcb8239f88a705d983a4c293984dfdcbe09779
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-08-11 13:52:37 +0900

    feat: 햄버거로 기존 페이지 탈출 로그 추가

--- src/components/common/Header/MobileHeader/Panel/index.tsx
+++ src/components/common/Header/MobileHeader/Panel/index.tsx
@@ -1,5 +1,5 @@
 import { cn } from '@bcsdlab/utils';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { CATEGORY, Submenu } from 'static/category';
 import useLogger from 'utils/hooks/analytics/useLogger';
 import { useLogout } from 'utils/hooks/auth/useLogout';
@@ -18,6 +18,7 @@ interface PanelProps {
 export default function Panel({ openModal }: PanelProps) {
   const { isSidebarOpen, closeSidebar } = useMobileSidebar();
   const { data: userInfo } = useUser();
+  const { pathname } = useLocation();
   const logout = useLogout();
   const logger = useLogger();
   const navigate = useNavigate();
@@ -31,6 +32,11 @@ export default function Panel({ openModal }: PanelProps) {
     if (title === '주변상점') logger.actionEventClick({ actionTitle: 'BUSINESS', title: 'hamburger_shop', value: '주변상점' });
     if (title === '복덕방') logger.actionEventClick({ actionTitle: 'BUSINESS', title: 'hamburger', value: '복덕방' });
     if (title === '시간표') logger.actionEventClick({ actionTitle: 'USER', title: 'hamburger', value: '시간표' });
+  };
+
+  // 기존 페이지에서 햄버거를 통해 다른 페이지로 이동할 때의 로그입니다.
+  const logExitExistingPage = () => {
+    if (pathname === '/timetable') logger.actionEventClick({ actionTitle: 'USER', title: 'timetable_back', value: '햄버거' });
   };
 
   const handleMyInfoClick = () => {
@@ -49,6 +55,7 @@ export default function Panel({ openModal }: PanelProps) {
 
   const handleSubmenuClick = (submenu: Submenu) => {
     logShortcut(submenu.title);
+    logExitExistingPage();
     closeSidebar();
     if (submenu.openInNewTab) {
       window.open(submenu.link, '_blank');
