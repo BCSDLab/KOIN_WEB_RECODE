02f47a215ac45009164ec68ea0e0bef165d98e48
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-08-14 14:23:41 +0900

    feat: 2-10 웹 로그 추가

--- src/pages/TimetablePage/DefaultPage/index.tsx
+++ src/pages/TimetablePage/DefaultPage/index.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable no-restricted-globals */
 /* eslint-disable no-restricted-imports */
 import LoadingSpinner from 'components/common/LoadingSpinner';
 import React, { Suspense } from 'react';
+import useLogger from 'utils/hooks/analytics/useLogger';
 import Curriculum from '../components/Curriculum';
 import LectureList from '../components/LectureList';
 import MyLectureList from '../components/MyLectureList';
@@ -8,6 +10,37 @@ import MyLectureTimetable from '../components/MyLectureTimetable';
 import styles from './DefaultPage.module.scss';
 
 export default function DefaultPage() {
+  const logger = useLogger();
+  const isClickedFirst = React.useRef(false);
+  const handlePopState = React.useCallback(() => {
+    history.back();
+    sessionStorage.removeItem('timetableVisited');
+    logger.actionEventClick({
+      actionTitle: 'USER',
+      title: 'timetable_back',
+      value: '뒤로가기버튼',
+      previous_page: '시간표',
+      current_page: '메인',
+      duration_time: (new Date().getTime() - Number(sessionStorage.getItem('enterTimetablePage'))) / 1000,
+    });
+  }, [logger]);
+
+  React.useEffect(() => {
+    const hasVisited = sessionStorage.getItem('timetableVisited');
+    if (!hasVisited) {
+      history.pushState({ state: 'timetable' }, '');
+      sessionStorage.setItem('timetableVisited', 'true');
+      isClickedFirst.current = true;
+    }
+  }, []);
+
+  React.useEffect(() => {
+    window.addEventListener('popstate', handlePopState);
+    return (() => {
+      window.removeEventListener('popstate', handlePopState);
+    });
+  }, [handlePopState]);
+
   return (
     <>
       <h1 className={styles.page__title}>시간표</h1>
