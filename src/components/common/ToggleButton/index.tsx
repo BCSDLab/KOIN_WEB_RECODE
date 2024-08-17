2b0def730e2035d20bd38c850513bec4296c6d7a
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-08-17 15:18:17 +0900

    refactor: lint 오류 수정

--- src/components/common/ToggleButton/index.tsx
+++ src/components/common/ToggleButton/index.tsx
@@ -7,7 +7,7 @@ interface ToggleButtonProps {
 }
 
 function ToggleButton({ width, height, handleToggle } : ToggleButtonProps) {
-  // 적정 너비:높이는 '2:1'입니다.
+  // 적정 '너비:높이'는 '2:1'입니다.
   return (
     <div style={{ width: `${width}px`, height: `${height}px` }}>
       <input type="checkbox" className={styles.button} onClick={handleToggle} />
