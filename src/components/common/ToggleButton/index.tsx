6cfd7334e3e37706045e2edb7ef983083b7455c0
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-08-17 14:52:47 +0900

    feat: 토글버튼 공통 컴포넌트 생성

--- src/components/common/ToggleButton/index.tsx
+++ src/components/common/ToggleButton/index.tsx
@@ -3,12 +3,14 @@ import styles from './ToggleButton.module.scss';
 interface ToggleButtonProps {
   width: string,
   height: string,
+  handleToggle: () => void,
 }
 
-function ToggleButton({ width, height } : ToggleButtonProps) {
+function ToggleButton({ width, height, handleToggle } : ToggleButtonProps) {
+  // 적정 너비:높이는 '2:1'입니다.
   return (
     <div style={{ width: `${width}px`, height: `${height}px` }}>
-      <input type="checkbox" className={styles.button} />
+      <input type="checkbox" className={styles.button} onClick={handleToggle} />
     </div>
   );
 }
