c02684d620f8e9b07869699bfcc56694c888da28
MinGu Jeong <51395707+MinGu-Jeong@users.noreply.github.com>
2024-08-25 22:29:08 +0900

    [공지사항] 공지사항 페이징 버그 수정 (hotfix) (#452)

--- src/components/Post/Pagination/index.tsx
+++ src/components/Post/Pagination/index.tsx
@@ -51,7 +51,7 @@ function Pagination(props: PaginationProps) {
         type="button"
         aria-label="이전 페이지로"
         className={styles.pagination__move}
-        onClick={() => onClickMove(onHandlePrevPage(Number(params.page) - 1))}
+        onClick={() => onClickMove(onHandlePrevPage(Number(params.page || 1) - 1))}
       >
         이전으로
       </button>
@@ -94,7 +94,11 @@ function Pagination(props: PaginationProps) {
         type="button"
         aria-label="다음 페이지로"
         className={styles.pagination__move}
-        onClick={() => setParams('page', onHandleNextPage(Number(params.page) + 1, Number(totalPageNum)), { deleteBeforeParam: false, replacePage: true })}
+        onClick={() => setParams(
+          'page',
+          onHandleNextPage(Number(params.page || 1) + 1, Number(totalPageNum)),
+          { deleteBeforeParam: false, replacePage: true },
+        )}
       >
         다음으로
       </button>
