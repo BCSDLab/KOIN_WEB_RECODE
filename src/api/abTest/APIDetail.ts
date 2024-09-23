6867542f4e8e8b94491f0c57067110420f29c5c2
(hoooooony) <(cjh41820@gmail.com)>
2024-09-23 20:33:47 +0900

    feat: abtest api 인터페이스 구현

---
+++ src/api/abTest/APIDetail.ts
@@ -0,0 +1,53 @@
+import { APIRequest, HTTP_METHOD } from 'interfaces/APIRequest';
+import { ABTestAssignResponse } from './entity';
+
+export class ABTestAssign<R extends ABTestAssignResponse> implements APIRequest<R> {
+  method = HTTP_METHOD.POST;
+
+  path = '/abtest/assign';
+
+  response!: R;
+
+  data: { title: string, };
+
+  headers: Record<string, string | number> = {};
+
+  auth = true;
+
+  constructor(
+    title: string,
+    public authorization?: string,
+    public accessHistoryId?: string | number,
+  ) {
+    this.data = ({ title });
+
+    if (this.accessHistoryId) {
+      this.headers.access_history_id = Number(this.accessHistoryId);
+    }
+  }
+}
+
+export class GetMyABTest<R extends string> implements APIRequest<R> {
+  method = HTTP_METHOD.GET;
+
+  path = '/abtest/me';
+
+  response!: R;
+
+  title: string;
+
+  headers: Record<string, string | number> = {};
+
+  auth = true;
+
+  constructor(
+    title: string,
+    public accessHistoryId: string | number,
+    public authorization?: string,
+  ) {
+    this.title = title;
+    this.headers = {
+      access_history_id: this.accessHistoryId,
+    };
+  }
+}
---
+++ src/api/abTest/entity.ts
@@ -0,0 +1,7 @@
+export interface GetMyABTestRequest {
+  title: string,
+}
+export interface ABTestAssignResponse {
+  variable_name: string,
+  access_history_id: number,
+}
