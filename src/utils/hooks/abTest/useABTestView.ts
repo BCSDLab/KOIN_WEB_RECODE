1c2e5498c4fca6fc0bd9dc0f0c5d5677c8b3dc4a
(hoooooony) <(cjh41820@gmail.com)>
2024-09-23 21:57:32 +0900

    feat : 토큰이 빈문자열인 경우 처리

--- src/utils/hooks/abTest/useABTestView.ts
+++ src/utils/hooks/abTest/useABTestView.ts
@@ -5,7 +5,7 @@ export const useABTestView = (title: string, authorization?: string) => {
   const accessHistoryId = localStorage.getItem('access_history_id');
   const { data: abTestView } = useSuspenseQuery({
     queryKey: ['abTestView', title, accessHistoryId],
-    queryFn: () => api.abTest.abTestAssign(title, authorization, accessHistoryId),
+    queryFn: () => api.abTest.abTestAssign(title, authorization === '' ? undefined : authorization, accessHistoryId),
   });
   localStorage.setItem('access_history_id', abTestView.access_history_id.toString());
   const result = abTestView;
