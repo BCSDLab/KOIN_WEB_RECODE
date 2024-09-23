84a1ce67ec038f72ffb4d304b4ebe4d8a8dd3a3f
murpin <qw04011@gmail.com>
2024-09-23 22:42:44 +0900

    feat: 혜택 카테고리 기능 추가

--- src/pages/Store/StoreBenefitPage/hooks/useBenefitCategory.ts
+++ src/pages/Store/StoreBenefitPage/hooks/useBenefitCategory.ts
@@ -10,7 +10,7 @@ const useBenefitCategory = () => {
   );
 
   return {
-    benefitCategory,
+    benefitCategory: benefitCategory?.benefits,
   };
 };
 
--- src/pages/Store/StoreBenefitPage/index.tsx
+++ src/pages/Store/StoreBenefitPage/index.tsx
@@ -1,44 +1,23 @@
 import useParamsHandler from 'utils/hooks/routing/useParamsHandler';
 import DesktopStoreList from 'pages/Store/StorePage/components/DesktopStoreList';
+import useBenefitCategory from 'pages/Store/StoreBenefitPage/hooks/useBenefitCategory';
 import useStoreBenefitList from 'pages/Store/StoreBenefitPage/hooks/useStoreBenefitList';
 import styles from './StoreBenefitPage.module.scss';
-
-const BENEFIT_LIST = [
-  {
-    id: 1,
-    logo: 'realkk',
-    text: '배달비 할인',
-  },
-  {
-    id: 2,
-    logo: 'realkk',
-    text: '최소주문금액 0원',
-  },
-  {
-    id: 3,
-    logo: 'realkk',
-    text: '서비스 증정',
-  },
-  {
-    id: 4,
-    logo: 'realkk',
-    text: '픽업 서비스',
-  },
-];
 
 function StoreBenefitPage() {
   const { params, setParams } = useParamsHandler();
   const { count, storeBenefitList } = useStoreBenefitList(params?.category ?? '1');
-
+  const { benefitCategory } = useBenefitCategory();
   const onClickBenefitTab = (id: number) => {
     setParams('category', `${id}`, { deleteBeforeParam: false, replacePage: false });
   };
+
   return (
     <div className={styles.section}>
       <div className={styles.header}>전화 주문 헤택</div>
       <div className={styles.section__tabs}>
         {
-          BENEFIT_LIST.map((item) => (
+          benefitCategory?.map((item) => (
             <button
               type="button"
               className={styles.tab}
@@ -50,7 +29,7 @@ function StoreBenefitPage() {
                   로고
                 </div>
                 <div className={styles['tab__content--text']}>
-                  {item.text}
+                  {item.title}
                 </div>
               </div>
             </button>
@@ -58,12 +37,14 @@ function StoreBenefitPage() {
         }
       </div>
       <div className={styles.section__title}>
-        계좌이체하면 배달비가 무료(할인)인 상점들을 모아뒀어요.
+        {
+
+        }
       </div>
       <div className={styles.section__content}>
         <div className={styles.option}>
           <div className={styles.option__count}>
-            총
+            총&nbsp;
             <strong>
               {count}
               개의 업체가
