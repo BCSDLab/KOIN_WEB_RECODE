0ec0d09b9790e0a1b76fa56df89e5c154d00ff8a
(hoooooony) <(cjh41820@gmail.com)>
2024-09-23 20:59:26 +0900

    feat : hook으로 완성

--- src/utils/hooks/abTest/useABTestView.ts
+++ src/utils/hooks/abTest/useABTestView.ts
@@ -1,3 +1,11 @@
-export const useABTestView = () => {
+import { abTestAssign } from 'api/abTest';
+import { useQuery } from '@tanstack/react-query';
 
+export const useABTestView = (title: string, authorization?: string, accessHistoryId?: string) => {
+  const { data: abTestView } = useQuery({
+    queryKey: ['abTestView', title, accessHistoryId],
+    queryFn: () => abTestAssign(title, authorization, accessHistoryId),
+  });
+
+  return { abTestView };
 };
