e2f3c499fa3fce0da504fc416dc8b550ad15407e
(hoooooony) <(cjh41820@gmail.com)>
2024-09-30 20:35:15 +0900

    fix : 테스트 부분 삭제

--- src/pages/BoardPage/index.tsx
+++ src/pages/BoardPage/index.tsx
@@ -2,13 +2,8 @@ import Header from 'components/common/Header';
 import Footer from 'components/common/Footer';
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
-import useTokenState from 'utils/hooks/state/useTokenState';
-import { useABTestView } from 'utils/hooks/abTest/useABTestView';
 
 function BoardPage() {
-  const token = useTokenState();
-  const ab = useABTestView('benefitPage', token);
-  console.log(ab);
   return (
     <>
       <Suspense fallback={null}>
