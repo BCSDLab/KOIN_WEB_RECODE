4f78dce87869e3a9ec6396a7d6c026a1078cd521
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-10-02 01:46:40 +0900

    feat: 시간및장소 삭제 기능 추가

--- src/pages/TimetablePage/components/CustomLecture/index.tsx
+++ src/pages/TimetablePage/components/CustomLecture/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { cn } from '@bcsdlab/utils';
 import { ReactComponent as AddIcon } from 'assets/svg/add-icon.svg';
+import { ReactComponent as CloseIcon } from 'assets/svg/close-icon-black.svg';
 import useTimetableV2Mutation from 'pages/TimetablePage/hooks/useTimetableV2Mutation';
 import Listbox from 'components/TimetablePage/Listbox';
 import { DAYS_STRING, HOUR, MINUTE } from 'static/timetable';
@@ -175,6 +176,14 @@ function CustomLecture({ frameId }: { frameId: string | undefined }) {
     }, 0);
   };
 
+  const handleDeleteTimeSpaceComponent = (index: number) => {
+    setTimeSpaceComponents((prev) => {
+      const updatedComponent = [...prev];
+      updatedComponent.splice(index, 1);
+      return updatedComponent;
+    });
+  };
+
   const handleLectureTimeByTime = (key: string, index: number) => (
     e: { target: { value: string } },
   ) => {
@@ -325,6 +334,17 @@ function CustomLecture({ frameId }: { frameId: string | undefined }) {
             place,
           }, index) => (
             <div className={styles['time-space-container__component']}>
+              <button
+                aria-label="delete-time-space-component"
+                type="button"
+                onClick={() => handleDeleteTimeSpaceComponent(index)}
+                className={cn({
+                  [styles['time-space-container__delete-button']]: true,
+                  [styles['time-space-container__delete-button--invisible']]: index === 0,
+                })}
+              >
+                <CloseIcon />
+              </button>
               <div className={styles['form-group-time']}>
                 <label
                   htmlFor="place"
