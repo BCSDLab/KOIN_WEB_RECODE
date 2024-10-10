378d60aec26ae292366bb0ec51459386fc2866e1
(hoooooony) <(cjh41820@gmail.com)>
2024-10-10 16:55:15 +0900

    hotFix : 사장님 링크 수정

--- src/static/category.ts
+++ src/static/category.ts
@@ -7,6 +7,7 @@ export interface Submenu {
   planFlag: boolean;
   tag: number | null;
   openInNewTab?: boolean;
+  stageLink?:string;
 }
 
 export type CategoryTitle = '서비스';
@@ -71,6 +72,7 @@ export const CATEGORY: Category[] = [
         planFlag: false,
         tag: null,
         openInNewTab: true,
+        stageLink: 'https://owner.stage.koreatech.in',
       },
       {
         title: '리뷰 작성하기',
