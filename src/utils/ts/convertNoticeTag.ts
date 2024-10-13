1a215e2b3bead0b2a2ec3360778796b908136b9c
MinGu Jeong <mingu830@gmail.com>
2024-10-13 23:37:54 +0900

    fix: 공지사항 게시판 board_id 수정

--- src/utils/ts/convertNoticeTag.ts
+++ src/utils/ts/convertNoticeTag.ts
@@ -8,8 +8,14 @@ function convertNoticeTag(type: number) {
       return '[학사공지]';
     case 8:
       return '[취업공지]';
-    default:
+    case 9:
       return '[코인공지]';
+    case 12:
+      return '[현장실습]';
+    case 13:
+      return '[학생생활]';
+    default:
+      return '[공지사항]';
   }
 }
 
