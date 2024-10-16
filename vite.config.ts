9efa46d1228b58b823c135d6abb12db359181b0f
MinGu Jeong <51395707+MinGu-Jeong@users.noreply.github.com>
2024-10-16 21:14:11 +0900

    Vite 마이그레이션 빌드 폴더 수정 (#554)

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
@@ -12,5 +13,8 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: 'build',
   },
 });
