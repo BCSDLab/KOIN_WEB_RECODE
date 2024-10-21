7d488756c5c1ab8cae8a053cc7ed50aea31d4ec5
JeongHoon Choi <cjh41820@gmail.com>
2024-10-21 15:10:59 +0900

    [common] AB공용 훅 수정 (#547)
    Co-authored-by: (hoooooony) <(cjh41820@gmail.com)>

--- src/utils/hooks/abTest/useABTestView.ts
+++ src/utils/hooks/abTest/useABTestView.ts
@@ -1,12 +1,25 @@
+/* eslint-disable max-len */
 import * as api from 'api';
 import { useSuspenseQuery } from '@tanstack/react-query';
 
 export const useABTestView = (title: string, authorization?: string) => {
   const accessHistoryId = localStorage.getItem('access_history_id');
+
   const { data: abTestView } = useSuspenseQuery({
     queryKey: ['abTestView', title, accessHistoryId],
-    queryFn: () => api.abTest.abTestAssign(title, authorization || undefined, accessHistoryId),
+    queryFn: async () => {
+      try {
+        const response = await api.abTest.abTestAssign(title, authorization || undefined, accessHistoryId);
+        return response;
+      } catch (error) {
+        return { access_history_id: null, variable_name: 'default' };
+      }
+    },
   });
-  localStorage.setItem('access_history_id', abTestView.access_history_id.toString());
-  return abTestView.variable_name;
+
+  if (abTestView.access_history_id) {
+    localStorage.setItem('access_history_id', abTestView.access_history_id.toString());
+  }
+
+  return abTestView.variable_name || 'default';
 };
