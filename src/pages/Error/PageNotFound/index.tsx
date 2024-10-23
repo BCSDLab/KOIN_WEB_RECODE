8b1a2fee773eb4d7a31b772c130ceb9349043c3c
MinGu Jeong <51395707+MinGu-Jeong@users.noreply.github.com>
2024-10-23 10:39:16 +0900

    [공통] 404페이지 추가 (#558)

---
+++ src/pages/Error/PageNotFound/index.tsx
@@ -0,0 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+import Error from 'assets/svg/page-not-found-error.svg';
+import styles from './PageNotFound.module.scss';
+
+export default function PageNotFound() {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.template}>
+      <div className={styles['error-icon']}>
+        <Error />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.content__title}>개발 중인 페이지입니다</div>
+        <div className={styles.content__description}>
+          죄송합니다. 현재 개발 중인 페이지입니다.
+          <br />
+          최대한 빠르게 오픈하도록 하겠습니다.
+        </div>
+        <button
+          type="button"
+          className={styles.content__button}
+          onClick={() => navigate('/')}
+        >
+          메인 화면 바로가기
+        </button>
+      </div>
+    </div>
+  );
+}
