afffb417257791fc5e75ac6b189ad0296ec678ac
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-10-24 17:13:23 +0900

    fix: nickname이 null인 경우 빈 문자열로 수정

--- src/pages/Auth/ModifyInfoPage/index.tsx
+++ src/pages/Auth/ModifyInfoPage/index.tsx
@@ -244,7 +244,7 @@ const NicknameForm = React.forwardRef<ICustomFormInput | null, ICustomFormInputP
     () => {
       // 닉네임 유효성 검사 로직
       let valid: string | true = true;
-      if (nickname !== userInfo?.nickname && (status !== 'success' || nickname !== currentCheckedNickname)) {
+      if (nickname !== (userInfo?.nickname || '') && (status !== 'success' || nickname !== currentCheckedNickname)) {
         valid = '닉네임 중복확인을 해주세요.';
       }
       return {
