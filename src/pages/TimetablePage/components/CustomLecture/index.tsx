7a5e19632ed9330c037e08f81e5fb87302730574
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-11-28 23:29:34 +0900

    fix: 종료시간 24시까지만 가능하게 수정

--- src/pages/TimetablePage/components/CustomLecture/index.tsx
+++ src/pages/TimetablePage/components/CustomLecture/index.tsx
@@ -404,7 +404,7 @@ function CustomLecture({ frameId }: { frameId: string | undefined }) {
                     <Listbox list={HOUR} value={time.startHour} onChange={handleLectureTimeByTime('startHour', index)} version="addLecture" />
                     <Listbox list={MINUTE} value={time.startMinute} onChange={handleLectureTimeByTime('startMinute', index)} version="addLecture" />
                     <span>-</span>
-                    <Listbox list={[...HOUR, { label: '24시', value: '24시' }]} value={time.endHour} onChange={handleLectureTimeByTime('endHour', index)} version="addLecture" />
+                    <Listbox list={time.endMinute === '30분' ? HOUR : [...HOUR, { label: '24시', value: '24시' }]} value={time.endHour} onChange={handleLectureTimeByTime('endHour', index)} version="addLecture" />
                     <Listbox list={time.endHour === '24시' ? [{ label: '00분', value: '00분' }] : MINUTE} value={time.endMinute} onChange={handleLectureTimeByTime('endMinute', index)} version="addLecture" />
                   </div>
                 </div>
