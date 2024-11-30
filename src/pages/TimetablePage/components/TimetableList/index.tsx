ecdace1d2f170dc1f005df57d281b402737a4580
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-11-30 16:13:48 +0900

    fix: frameList GET api 불필요한 호출 제거

--- src/pages/TimetablePage/components/TimetableList/index.tsx
+++ src/pages/TimetablePage/components/TimetableList/index.tsx
@@ -187,8 +187,6 @@ export default function TimetableList({
       {isModalOpen && (
         <TimetableSettingModal
           focusFrame={focusFrame!}
-          setCurrentFrameIndex={setCurrentFrameIndex}
-          currentFrameIndex={currentFrameIndex}
           onClose={closeModal}
         />
       )}
