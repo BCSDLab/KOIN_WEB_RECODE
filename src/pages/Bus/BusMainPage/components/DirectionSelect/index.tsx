ffe0e7a4c5bb12043b2507615d784c0bb0bfdae8
정해성 <gotjd0230@naver.com>
2024-12-07 23:33:31 +0900

    feat: 출발지, 목적지 선택

--- src/pages/Bus/BusMainPage/components/DirectionSelect/index.tsx
+++ src/pages/Bus/BusMainPage/components/DirectionSelect/index.tsx
@@ -1,18 +1,52 @@
-import ChangeIcon from 'assets/svg/Bus/change-icon.svg';
-import PlaceSelect, { PLACE_TYPE_KEYS } from 'pages/Bus/BusMainPage/components/PlaceSelect';
+import ExchangeIcon from 'assets/svg/Bus/exchange-icon.svg';
+import PlaceSelect from 'pages/Bus/BusMainPage/components/PlaceSelect';
+import { useState } from 'react';
 import styles from './DirectionSelect.module.scss';
 
+const PLACE_TYPE_KEYS = {
+  departure: 'departure',
+  destination: 'destination',
+} as const;
+
 export default function DirectionSelect() {
+  const [departure, setDeparture] = useState('');
+  const [destination, setDestination] = useState('');
+
+  const exchangePlace = () => {
+    const temp = departure;
+    setDeparture(destination);
+    setDestination(temp);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.box}>
         <div className={styles.direction}>
           <div className={styles.direction_select}>
-            <PlaceSelect type={PLACE_TYPE_KEYS.departure} />
+            <PlaceSelect
+              type={PLACE_TYPE_KEYS.departure}
+              place={departure}
+              oppositePlace={destination}
+              setPlace={setDeparture}
+              exchangePlace={exchangePlace}
+            />
           </div>
-          <ChangeIcon />
+          <button
+            className={styles['exchange-button']}
+            onClick={() => exchangePlace()}
+            type="button"
+            aria-label="출발지와 도착지를 바꾸기"
+          >
+            <ExchangeIcon aria-hidden="true" />
+          </button>
           <div className={styles.direction_select}>
-            <PlaceSelect type={PLACE_TYPE_KEYS.destination} />
+            <PlaceSelect
+              type={PLACE_TYPE_KEYS.destination}
+              place={destination}
+              oppositePlace={departure}
+              setPlace={setDestination}
+              exchangePlace={exchangePlace}
+            />
           </div>
         </div>
         <button
