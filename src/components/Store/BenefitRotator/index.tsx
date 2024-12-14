96dfc370c90664cd8ebe74138789ef45f44fdfc1
chaeseungyun <101871802+chaeseungyun@users.noreply.github.com>
2024-12-14 19:12:12 +0900

    [비즈니스] 비즈니스팀 QA 사항 수정 (#605)

--- src/components/Store/BenefitRotator/index.tsx
+++ src/components/Store/BenefitRotator/index.tsx
@@ -7,22 +7,31 @@ interface BenefitRotatorProps {
 
 function BenefitRotator({ benefits }: BenefitRotatorProps) {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isAnimation, setIsAnimation] = useState(false);
 
   useEffect(() => {
+    let intervalId: NodeJS.Timeout;
     if (Array.isArray(benefits) && benefits.length > 1) {
-      const intervalId = setInterval(() => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % benefits.length);
+      intervalId = setInterval(() => {
+        setIsAnimation(true);
+        setTimeout(() => {
+          setIsAnimation(false);
+          setCurrentIndex((prevIndex) => (prevIndex + 1) % benefits.length);
+        }, 100); // 애니메이션 지속 시간과 동일하게 설정
       }, 4000);
-
-      return () => clearInterval(intervalId);
     }
-
-    return undefined;
-  }, [benefits]);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [benefits, isAnimation]);
 
   return (
     <div className={styles['benefit-content']}>
-      {Array.isArray(benefits) ? benefits[currentIndex] : benefits}
+      {Array.isArray(benefits) ? (
+        <span className={isAnimation ? styles['benefit-content__fade-in'] : styles['benefit-content__fade-out']}>
+          {benefits[currentIndex]}
+        </span>
+      ) : benefits}
     </div>
   );
 }
