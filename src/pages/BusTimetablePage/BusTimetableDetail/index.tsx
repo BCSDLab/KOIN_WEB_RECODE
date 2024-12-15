e4c0980f990d5f8a88923853afb31ebe061f5b5e
d0dam <smallkdb@gmail.com>
2024-12-16 07:40:42 +0900

    refactor: 버스 순환 노선 시간표 디자인

--- src/pages/BusTimetablePage/BusTimetableDetail/index.tsx
+++ src/pages/BusTimetablePage/BusTimetableDetail/index.tsx
@@ -2,6 +2,7 @@ import useShuttleTimetableDetail from 'pages/BusTimetablePage/hooks/useShuttleTi
 import { useEffect } from 'react';
 import BusIcon from 'assets/svg/bus.svg';
 import styles from './BusTimetableDetail.module.scss';
+import { cn } from '@bcsdlab/utils';
 
 interface ShuttleTimetableDetailProps {
   routeId : string;
@@ -68,33 +69,29 @@ function BusTimetableDetail({ routeId }: ShuttleTimetableDetailProps) {
             </div>
           </div>
 
-          <div className={styles['time-tabal']}>
-            <div className={styles['time-tabal__header']}>
-              <div className={styles['time-tabal__classification']}>승하차장명</div>
-              <div className={styles['time-tabal__node-wrapper']}>
-                {shuttleTimetableDetail.node_info.map(({ name, detail }) => (
-                  <div className={styles['time-tabal__node']}>
-                    <div>{ name }</div>
-                    <div className={styles['time-tabal__node-detail']}>{ detail }</div>
-                  </div>
-                ))}
+          <div className={styles['time-table']} style={{ gridTemplateRows: `repeat(${shuttleTimetableDetail.node_info.length + 1}, 1fr)` }}>
+            <div className={styles['time-table__number']}>승하차장명</div>
+            {shuttleTimetableDetail.node_info.map(({ name, detail }) => (
+              <div
+                className={styles['time-table__node']}
+              >
+                <div>{name}</div>
+                {detail && <div className={styles['time-table__node-detail']}>{detail}</div>}
               </div>
-            </div>
+            ))}
 
             {shuttleTimetableDetail.route_info.map(({ name, detail, arrival_time }) => (
-              <div>
-                <div className={styles['time-tabal__number']}>
+              <>
+                <div className={styles['time-table__number']}>
                   {name}
                   {detail}
                 </div>
-                <div className={styles['time-tabal__time-wrapper']}>
-                  {arrival_time.map((time) => (
-                    <div className={styles['time-tabal__time']}>
-                      {time ? time.split('/')[0] : time}
-                    </div>
-                  ))}
-                </div>
-              </div>
+                {arrival_time.map((time) => (
+                  <div className={styles['time-table__time']}>
+                    {time ? time.split('/')[0] : time}
+                  </div>
+                ))}
+              </>
             ))}
           </div>
         </div>
