ae087e862c157b44389cd567a274c44d4966e4ac
정해성 <gotjd0230@naver.com>
2024-12-23 17:56:14 +0900

    feat: 인덱스 페이지 모바일 디자인 수정

--- src/pages/IndexPage/components/IndexBus/index.tsx
+++ src/pages/IndexPage/components/IndexBus/index.tsx
@@ -3,11 +3,16 @@ import { BUS_LINKS } from 'static/bus';
 import useLogger from 'utils/hooks/analytics/useLogger';
 import ROUTES from 'static/routes';
 import ChevronRight from 'assets/svg/IndexPage/Bus/chevron-right.svg';
+import QRCode from 'assets/svg/IndexPage/Bus/qr-code.svg';
+import useMediaQuery from 'utils/hooks/layout/useMediaQuery';
 import styles from './IndexBus.module.scss';
 
 function IndexBus() {
+  const isMobile = useMediaQuery();
   const logger = useLogger();
   const logBus = () => logger.actionEventClick({ actionTitle: 'CAMPUS', title: 'main_bus', value: '버스' });
+
+  const unibus = BUS_LINKS[2];
 
   return (
     <section className={styles.template}>
@@ -18,19 +23,27 @@ function IndexBus() {
         >
           버스
         </Link>
-      </div>
-      <div className={styles.cards}>
-        {BUS_LINKS.map(({
-          key, title, subtitle, link, SvgIcon,
-        }) => (
+        {isMobile && (
           <Link
-            to={link}
-            key={key}
-            className={styles.cards__card}
-            onClick={logBus}
+            to={unibus.link}
+            className={styles.unibus}
           >
-            <div className={styles['cards__card-segment']}>
-              <SvgIcon />
+            <QRCode />
+            <span className={styles.unibus__title}>셔틀 탑승권</span>
+          </Link>
+        )}
+      </div>
+      {isMobile ? (
+        <div className={styles.cards}>
+          {BUS_LINKS.slice(0, 2).map(({
+            key, title, subtitle, link,
+          }) => (
+            <Link
+              to={link}
+              key={key}
+              className={styles.cards__card}
+              onClick={logBus}
+            >
               <div className={styles['cards__card-guide']}>
                 <span className={styles['cards__card-title']}>
                   {title}
@@ -39,11 +52,37 @@ function IndexBus() {
                   {subtitle}
                 </span>
               </div>
-            </div>
-            <ChevronRight />
-          </Link>
-        ))}
-      </div>
+              <ChevronRight />
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <div className={styles.cards}>
+          {BUS_LINKS.map(({
+            key, title, subtitle, link, SvgIcon,
+          }) => (
+            <Link
+              to={link}
+              key={key}
+              className={styles.cards__card}
+              onClick={logBus}
+            >
+              <div className={styles['cards__card-segment']}>
+                <SvgIcon />
+                <div className={styles['cards__card-guide']}>
+                  <span className={styles['cards__card-title']}>
+                    {title}
+                  </span>
+                  <span className={styles['cards__card-subtitle']}>
+                    {subtitle}
+                  </span>
+                </div>
+              </div>
+              <ChevronRight />
+            </Link>
+          ))}
+        </div>
+      )}
     </section>
   );
 }
