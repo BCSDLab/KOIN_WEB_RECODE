51c21072f1dbc8421a51e203502f42140cf327aa
정해성 <gotjd0230@naver.com>
2024-12-30 21:35:43 +0900

    refactor: 모바일에서 guide 제거

--- src/pages/Bus/BusRoutePage/components/BusGuide/index.tsx
+++ src/pages/Bus/BusRoutePage/components/BusGuide/index.tsx
@@ -1,12 +1,9 @@
-import useMediaQuery from 'utils/hooks/layout/useMediaQuery';
 import styles from './BusGuide.module.scss';
 
 export default function BusGuide() {
-  const isMobile = useMediaQuery();
-
   return (
     <div className={styles.guide}>
-      {!isMobile && (<h1 className={styles.guide__title}>버스</h1>)}
+      <h1 className={styles.guide__title}>버스</h1>
       <p className={styles.guide__description}>목적지까지 가장 빠른 교통편을 알려드릴게요.</p>
     </div>
   );
