6176eeb28a8a44fab856fe21ae38921dd3e87226
정해성 <gotjd0230@naver.com>
2025-01-12 00:37:27 +0900

    feat: 글쓰기 페이지 추가

--- src/pages/Notice/LostItemPage/index.tsx
+++ src/pages/Notice/LostItemPage/index.tsx
@@ -1,16 +1,52 @@
 import { useLocation } from 'react-router-dom';
+import AddIcon from 'assets/svg/Notice/add.svg';
 import styles from './LostItemPage.module.scss';
+
+const titles = {
+  found: {
+    title: '습득물 신고',
+    subtitle: '습득한 물건을 자세히 설명해주세요!',
+  },
+  lost: {
+    title: '분실물 신고',
+    subtitle: '분실한 물건을 자세히 설명해주세요!',
+  },
+};
 
 type LostItemType = 'found' | 'lost';
 
 export default function LostItemPage() {
   const location = useLocation();
   const type: LostItemType = location.pathname.includes('/found') ? 'found' : 'lost';
-  console.log(type);
+  const { title, subtitle } = titles[type];
 
   return (
     <div className={styles.container}>
-      s
+      <div className={styles.wrapper}>
+        <div className={styles.header}>
+          <span className={styles.header__title}>{title}</span>
+          <span className={styles.header__subtitle}>{subtitle}</span>
+        </div>
+        <div className={styles.add}>
+          <button
+            className={styles.add__button}
+            type="button"
+            onClick={() => {}}
+          >
+            <AddIcon />
+            물품 추가
+          </button>
+        </div>
+        <div className={styles.complete}>
+          <button
+            className={styles.complete__button}
+            type="button"
+            onClick={() => {}}
+          >
+            작성 완료
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
