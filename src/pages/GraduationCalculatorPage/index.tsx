3824340bc6c2963dfb3820a1e54a7344a2552f94
hyejun <hyejunkkim228@gmail.com>
2025-01-18 20:42:06 +0900

    feat: 졸업학점계산기 도움말 모달

--- src/pages/GraduationCalculatorPage/index.tsx
+++ src/pages/GraduationCalculatorPage/index.tsx
@@ -1,14 +1,27 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 import AcademicCapIcon from 'assets/svg/academic-cap-icon.svg';
 import QuestionMarkIcon from 'assets/svg/question-mark-icon.svg';
+import useBooleanState from 'utils/hooks/state/useBooleanState';
+import useModalPortal from 'utils/hooks/layout/useModalPortal';
 import styles from './GraduationCalculatorPage.module.scss';
 import StudentForm from './components/StudentForm';
 import LectureTable from './components/LectureTable';
 import ExcelUploader from './components/ExcelUploader';
 import GeneralCourse from './components/GeneralCourse';
 import CreditChart from './components/CreditChart';
+import CalculatorHelpModal from './CalculatorHelpModal';
 
 function GraduationCalculatorPage() {
+  const portalManager = useModalPortal();
+  // const [isOpenModal, openModal, closeModal] = useBooleanState(false);
+
+  const handleInformationClick = () => {
+    portalManager.open(() => (
+      <CalculatorHelpModal closeInfo={portalManager.close} />
+    ));
+  };
+
   return (
     <div className={styles.page}>
       <div className={styles.graduation}>
@@ -17,9 +30,13 @@ function GraduationCalculatorPage() {
             <AcademicCapIcon />
           </div>
           <h1 className={styles.header__title}>졸업학점 계산기</h1>
-          <div className={styles['header__question-icon']}>
+          <button
+            type="button"
+            onClick={() => handleInformationClick()}
+            className={styles['header__question-icon']}
+          >
             <QuestionMarkIcon />
-          </div>
+          </button>
         </div>
         <div className={styles.content}>
           <div className={styles.content__inputs}>
