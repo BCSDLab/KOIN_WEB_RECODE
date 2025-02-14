9ccaf2ba9069f7d6688ca132ce1cfae6860efa51
김혜준 <114041848+hyejun0228@users.noreply.github.com>
2025-02-14 23:31:12 +0900

    [유저] 유저아이디 변경 (#658)
    Co-authored-by: hyejun <hyejunkkim228@gmail.com>

--- src/components/common/LogPage/index.tsx
+++ src/components/common/LogPage/index.tsx
@@ -6,25 +6,26 @@ import { UserResponse } from 'api/auth/entity';
 import { useUser } from 'utils/hooks/state/useUser';
 
 const userUniqueIdGenerator = (userInfo: UserResponse | null | undefined) => {
-  let uuid = localStorage.getItem('uuid');
+  if (userInfo?.anonymous_nickname) {
+    localStorage.setItem('uuid_logged_in', userInfo.anonymous_nickname);
+    return userInfo.anonymous_nickname;
+  }
+
+  let uuid = localStorage.getItem('uuid_guest');
+
   if (!uuid) {
     uuid = uuidv4();
-    localStorage.setItem('uuid', uuid);
-  }
-  // student_number가 있는 경우, 기존 uuid를 확인하여 새로 저장
-  if (userInfo?.student_number) {
-    // 기존 uuid에서 student_number를 분리
-    const existingUuid = uuid.split('-').pop();
-    uuid = `${userInfo.student_number}-${existingUuid}`;
-    localStorage.setItem('uuid', uuid);
+    localStorage.setItem('uuid_guest', uuid);
   }
 
   return uuid;
 };
+
 function LogPage() {
   const location = useLocation();
   const { data: userInfo } = useUser();
   const prevPathname = React.useRef('');
+
   useEffect(() => {
     const handlePageView = () => {
       if (prevPathname.current !== window.location.pathname) {
@@ -40,6 +41,7 @@ function LogPage() {
     };
     handlePageView();
   }, [location, userInfo]);
+
   return null;
 }
 export default LogPage;
