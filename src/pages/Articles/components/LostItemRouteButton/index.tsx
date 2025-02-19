797a03de3513218263591789a129e52db341b622
JeongWon-CHO <ahin0312@naver.com>
2025-02-20 03:18:54 +0900

    feat: 글쓰기 버튼 모바일뷰 추가

--- src/pages/Articles/components/LostItemRouteButton/index.tsx
+++ src/pages/Articles/components/LostItemRouteButton/index.tsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 import FoundIcon from 'assets/svg/Articles/found.svg';
 import LostIcon from 'assets/svg/Articles/lost.svg';
 import CloseIcon from 'assets/svg/Articles/close.svg';
-// import useBooleanState from 'utils/hooks/state/useBooleanState';
 import ROUTES from 'static/routes';
 import styles from './LostItemRouteButton.module.scss';
 
@@ -14,43 +13,55 @@ export default function LostItemRouteButton() {
   const [isWriting, setIsWriting] = useState(false);
 
   return (
-    <div className={styles.links}>
+    <>
       {isWriting && (
-      <div className={styles['links__writing-options']}>
-        <Link
-          className={styles['links__option-button']}
-          type="button"
-          to={ROUTES.LostItemFound()}
-          onClick={() => logItemWriteClick()}
-        >
-          <FoundIcon />
-          <div className={styles['links__option-text']}>
-            주인을 찾아요
-          </div>
-        </Link>
-
-        <Link
-          className={styles['links__option-button']}
-          type="button"
-          to={ROUTES.LostItemLost()} // 이 링크 아님 이거 임시임.
-          onClick={() => logItemWriteClick()}
-        >
-          <LostIcon />
-          <div className={styles['links__option-text']}>
-            잃어버렸어요
-          </div>
-        </Link>
-      </div>
+      <div
+        className={styles.overlay}
+        role="button"
+        tabIndex={0}
+        aria-label="오버레이"
+        onClick={() => setIsWriting(false)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' || e.key === ' ') {
+            setIsWriting(false);
+          }
+        }}
+      />
       )}
-      <button
-        className={styles.links__write}
-        type="button"
-        onClick={() => setIsWriting((prev) => !prev)}
-      >
-        {/* {linksOpen ? <CloseIcon /> : <PencilIcon />} */}
-        {isWriting ? <CloseIcon /> : <PencilIcon />}
-        글쓰기
-      </button>
-    </div>
+      <div className={`${styles.links} ${isWriting ? styles['links--active'] : ''}`}>
+        {isWriting && (
+          <div className={styles['links__writing-options']}>
+            <Link
+              className={styles['links__option-button']}
+              to={ROUTES.LostItemFound()}
+              onClick={() => logItemWriteClick()}
+
+            >
+              <FoundIcon />
+              <div className={styles['links__option-text']}>주인을 찾아요</div>
+            </Link>
+
+            <Link
+              className={styles['links__option-button']}
+              to={ROUTES.LostItemLost()}
+              onClick={() => logItemWriteClick()}
+
+            >
+              <LostIcon />
+              <div className={styles['links__option-text']}>잃어버렸어요</div>
+            </Link>
+          </div>
+        )}
+
+        <button
+          className={styles.links__write}
+          type="button"
+          onClick={() => setIsWriting((prev) => !prev)}
+        >
+          {isWriting ? <CloseIcon /> : <PencilIcon />}
+          글쓰기
+        </button>
+      </div>
+    </>
   );
 }
