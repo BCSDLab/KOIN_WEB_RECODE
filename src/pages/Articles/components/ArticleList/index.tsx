0f1b86dcc6842e28a31ea6473db470d5ca5a9336
JeongWon-CHO <ahin0312@naver.com>
2025-02-20 00:17:00 +0900

    feat: 신고된 게시글 클릭 금지 및 토스트 메시지 추가

--- src/pages/Articles/components/ArticleList/index.tsx
+++ src/pages/Articles/components/ArticleList/index.tsx
@@ -4,6 +4,7 @@ import useMediaQuery from 'utils/hooks/layout/useMediaQuery';
 import setArticleRegisteredDate from 'utils/ts/setArticleRegisteredDate';
 import { convertArticlesTag } from 'utils/ts/convertArticlesTag';
 import ROUTES from 'static/routes';
+import showToast from 'utils/ts/showToast';
 import styles from './ArticleList.module.scss';
 
 interface ArticleListProps {
@@ -36,71 +37,68 @@ export default function ArticleList({ articles }: ArticleListProps) {
   return (
     <div className={styles.list}>
       {articles.map((article) => {
-        if (article.board_id === 14) {
-          const { type, content, date } = parseLostItemTitle(article.title);
+        const { type, content, date } = parseLostItemTitle(article.title);
+
+        // 1. 신고된 게시글 (클릭 X, 토스트메시지, 내용 표시 다르게)
+        if (article.board_id === 14 && article.is_reported) {
           return (
-            <Link
-              className={styles.list__link}
-              to={getLink(article)}
+            <div
               key={article.id}
+              className={`${styles.list__link} ${styles.disabled}`}
+              role="button"
+              onClick={() => showToast('error', '신고된 게시글은 더 이상 볼 수 없습니다.')}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  showToast('error', '신고된 게시글은 더 이상 볼 수 없습니다.');
+                }
+              }}
+              tabIndex={0}
             >
               <div className={styles.list__id}>{article.id}</div>
               <div className={styles.title}>
                 <div className={styles.title__container}>
-                  <div className={styles['title__container-header']}>{ convertArticlesTag(article.board_id) }</div>
-                  {article.is_reported ? (
-                    <div>
-                      신고에 의해 숨김처리 되었습니다.
-                    </div>
-                  ) : (
-                    <>
-                      <div className={styles['title__container-type']}>{type}</div>
-                      <div className={styles['title__container-container']}>{content}</div>
-                      <div className={styles['title__container-container']}>|</div>
-                      <div className={styles['title__container-container']}>{date}</div>
-                    </>
-
-                  )}
-
+                  <div className={styles['title__container-header']}>{convertArticlesTag(article.board_id)}</div>
+                  <div>신고에 의해 숨김 처리 되었습니다.</div>
                 </div>
-                {setArticleRegisteredDate(article.registered_at)[1] && (
-                  <img
-                    className={styles['title__new-tag']}
-                    src="https://static.koreatech.in/upload/7f2af097aeeca368b0a491f9e00f80ca.png"
-                    alt="new"
-                  />
-                )}
               </div>
               <div className={styles.list__author}>{isMobile ? `${article.author}` : article.author}</div>
-              <div className={styles['list__registered-at']}>
-                {setArticleRegisteredDate(article.registered_at)[0]}
-              </div>
-            </Link>
+              <div className={styles['list__registered-at']}>{setArticleRegisteredDate(article.registered_at)[0]}</div>
+            </div>
           );
         }
 
+        // 2. 신고되지 않은 게시글 (클릭 O, 상세페이지로 이동)
         return (
-          <Link
-            className={styles.list__link}
-            to={getLink(article)}
-            key={article.id}
-          >
-            <div className={styles.list__id}>{ article.id }</div>
+          <Link className={styles.list__link} to={getLink(article)} key={article.id}>
+            <div className={styles.list__id}>{article.id}</div>
             <div className={styles.title}>
-              <div className={styles.title__header}>{ convertArticlesTag(article.board_id) }</div>
-              <div className={styles.title__content}>{ article.title }</div>
-              { setArticleRegisteredDate(article.registered_at)[1] && (
-              <img
-                className={styles['title__new-tag']}
-                src="https://static.koreatech.in/upload/7f2af097aeeca368b0a491f9e00f80ca.png"
-                alt="new"
-              />
+              <div className={styles.title__container}>
+                <div className={styles['title__container-header']}>{convertArticlesTag(article.board_id)}</div>
+                {/* 분실물 */}
+                {article.board_id === 14 ? (
+                  <>
+                    <div className={styles['title__container-type']}>{type}</div>
+                    <div className={styles['title__container-container']}>{content}</div>
+                    <div className={styles['title__container-container']}>|</div>
+                    <div className={styles['title__container-container']}>{date}</div>
+                  </>
+                ) : (
+                  <>
+                    {/* 일반 공지사항 */}
+                    <div className={styles.title__content}>{article.title}</div>
+                  </>
+                )}
+              </div>
+              {setArticleRegisteredDate(article.registered_at)[1] && (
+                <img
+                  className={styles['title__new-tag']}
+                  src="https://static.koreatech.in/upload/7f2af097aeeca368b0a491f9e00f80ca.png"
+                  alt="new"
+                />
               )}
             </div>
-            <div className={styles.list__author}>{ isMobile ? `${article.author}` : article.author }</div>
-            <div className={styles['list__registered-at']}>
-              { setArticleRegisteredDate(article.registered_at)[0] }
-            </div>
+            <div className={styles.list__author}>{isMobile ? `${article.author}` : article.author}</div>
+            <div className={styles['list__registered-at']}>{setArticleRegisteredDate(article.registered_at)[0]}</div>
           </Link>
         );
       })}
