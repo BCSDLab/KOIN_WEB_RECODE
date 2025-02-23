a30cf1946ac9b1e5f753b89d89b8c11838124375
hyejun <hyejunkkim228@gmail.com>
2025-02-23 20:17:52 +0900

    feat: 불필요한 요소 제거

--- src/pages/GraduationCalculatorPage/hooks/useExcelUpload.ts
+++ src/pages/GraduationCalculatorPage/hooks/useExcelUpload.ts
@@ -1,16 +1,12 @@
-import { useState } from 'react';
 import usePostGraduationExcel from 'pages/GraduationCalculatorPage/hooks/usePostGraduationExcel';
 import { GraduationExcelUploadForPost } from 'pages/GraduationCalculatorPage/ts/types';
 
 export function useExcelUpload() {
-  const [fileName, setFileName] = useState<string | null>(null);
   const { mutate } = usePostGraduationExcel();
 
   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (!file) return;
-
-    setFileName(file.name);
 
     const formData = new FormData();
     formData.append('file', file);
@@ -18,5 +14,5 @@ export function useExcelUpload() {
     mutate(formData as unknown as GraduationExcelUploadForPost);
   };
 
-  return { handleFileUpload, fileName };
+  return { handleFileUpload };
 }
