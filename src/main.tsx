5dba873b4e1f76647ad44a6530f7412e89749695
d0dam <smallkdb@gmail.com>
2025-02-25 17:38:36 +0900

    fix: QA 내용 반영

--- src/main.tsx
+++ src/main.tsx
@@ -21,17 +21,17 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <BrowserRouter>
-        <PortalProvider>
-          <QueryClientProvider client={queryClient}>
-            <App />
-          </QueryClientProvider>
-        </PortalProvider>
-      </BrowserRouter>
-    </RecoilRoot>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <RecoilRoot>
+    <BrowserRouter>
+      <PortalProvider>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </PortalProvider>
+    </BrowserRouter>
+  </RecoilRoot>,
+  // </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
