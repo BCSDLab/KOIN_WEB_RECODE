6d5b544c1e9dcac81e3ac5a668abd53bd6a7787c
Gwak-Seungju <tmwn1015@naver.com>
2025-02-26 22:18:10 +0900

    fix: enabled 삭제

--- src/pages/GraduationCalculatorPage/hooks/useCalculateCredits.ts
+++ src/pages/GraduationCalculatorPage/hooks/useCalculateCredits.ts
@@ -2,11 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import { graduationCalculator } from 'api';
 
 export default function useCalculateCredits(token: string) {
-  const agreeGraduationCredits = localStorage.getItem('agreeGraduationCredits');
+  // const agreeGraduationCredits = localStorage.getItem('agreeGraduationCredits');
 
   return useQuery({
     queryKey: ['creditsByCourseType'],
     queryFn: () => (token ? graduationCalculator.calculateGraduationCredits(token) : null),
-    enabled: !!agreeGraduationCredits,
   });
 }
