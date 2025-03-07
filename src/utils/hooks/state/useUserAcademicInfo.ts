25b21c8341f3db8698951146b25207cd7d34b70b
Gwak-Seungju <tmwn1015@naver.com>
2025-03-07 16:47:20 +0900

    fix: 수강한 강의 선택 시 전체 강의 보여주기

--- src/utils/hooks/state/useUserAcademicInfo.ts
+++ src/utils/hooks/state/useUserAcademicInfo.ts
@@ -1,11 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { auth } from 'api';
 import useTokenState from './useTokenState';
 
 export default function useUserAcademicInfo() {
   const token = useTokenState();
 
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: ['userAcademicinfo'],
 
     queryFn: () => (token ? auth.getUserAcademicInfo(token) : null),
