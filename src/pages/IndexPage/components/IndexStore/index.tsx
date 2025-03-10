f854a46cb784a9c81063b91caec43702fb41efaf
정해성 <gotjd0230@naver.com>
2025-03-10 21:29:20 +0900

    [비즈니스] 주변상점 인덱스 페이지 코드 리팩토링 (#737)

--- src/pages/IndexPage/components/IndexStore/index.tsx
+++ src/pages/IndexPage/components/IndexStore/index.tsx
@@ -1,10 +1,8 @@
 import { useNavigate, Link } from 'react-router-dom';
-// import useMediaQuery from 'utils/hooks/useMediaQuery';
 import { useStoreCategories } from 'pages/Store/StorePage/hooks/useCategoryList';
 import useLogger from 'utils/hooks/analytics/useLogger';
 import ROUTES from 'static/routes';
-import useMediaQuery from 'utils/hooks/layout/useMediaQuery';
-import BenefitIcon from 'assets/svg/benefit-icon.svg';
+import { Suspense } from 'react';
 import styles from './IndexStore.module.scss';
 
 interface Category {
@@ -13,15 +11,27 @@ interface Category {
   image_url: string;
 }
 
+interface CategoryWithEvent extends Category {
+  event: {
+    actionTitle: string;
+    event_label: string;
+    value: string;
+    event_category: string;
+    previous_page: string;
+    current_page: string;
+    duration_time: number;
+  };
+  route: string;
+}
+
 function IndexStore() {
-  const isMobile = useMediaQuery();
   const { data: categories } = useStoreCategories();
   const logger = useLogger();
   const navigate = useNavigate();
 
-  const handleStoreCategoryClick = (e: React.MouseEvent<HTMLDivElement>, category: Category) => {
-    e.preventDefault();
-    logger.actionEventClick({
+  const categoriesWithEvent = categories.shop_categories.map((category: Category) => ({
+    ...category,
+    event: {
       actionTitle: 'BUSINESS',
       event_label: 'main_shop_categories',
       value: category.name,
@@ -29,87 +39,58 @@ function IndexStore() {
       previous_page: '메인',
       current_page: category.name,
       duration_time: (new Date().getTime() - Number(sessionStorage.getItem('enterMain'))) / 1000,
-    });
-    navigate(`${ROUTES.Store()}?category=${category.id}&COUNT=1`);
+    },
+    route: `${ROUTES.Store()}?category=${category.id}&COUNT=1`,
+  }));
+
+  const categoriesWithBenefit: CategoryWithEvent[] = categoriesWithEvent.map(
+    (category) => (
+      category.name === '전체보기' ? ({
+        ...category,
+        name: '혜택',
+        image_url: 'https://static.koreatech.in/assets/img/icon/benefit_icon.png',
+        event: {
+          ...category.event,
+          event_label: 'main_shop_benefit',
+          value: '전화주문혜택',
+          current_page: 'benefit',
+        },
+        route: `${ROUTES.BenefitStore()}?category=1`,
+      }) : category
+    ),
+  );
+
+  const handleCategoryClick = (
+    e: React.MouseEvent<HTMLDivElement>,
+    category: CategoryWithEvent,
+  ) => {
+    e.preventDefault();
+    logger.actionEventClick(category.event);
+    navigate(category.route);
   };
-  const handleStoreClick = () => {
-    logger.actionEventClick({
-      actionTitle: 'BUSINESS',
-      event_label: 'main_shop_benefit',
-      value: '전화주문혜택',
-      event_category: 'click',
-      previous_page: '메인',
-      current_page: 'benefit',
-      duration_time: (new Date().getTime() - Number(sessionStorage.getItem('enterMain'))) / 1000,
-    });
-    navigate('/benefitstore?category=1');
-  };
+
   return (
     <section className={styles.template}>
       <Link to={ROUTES.Store()} className={styles.template__title}>주변 상점</Link>
-      <div className={styles.category__wrapper}>
-        {isMobile
-          ? categories?.shop_categories.slice(0, 12).map((category) => (
-            category.name === '전체보기' ? (
-              <div
-                key={category.id}
-                className={styles.category__benefit}
-                onClick={() => handleStoreClick()}
-                aria-hidden
-              >
-                <BenefitIcon />
-                혜택
-              </div>
-            )
-              : (
-                <div
-                  key={category.id}
-                  className={styles.category__item}
-                  onClick={(e) => handleStoreCategoryClick(e, category)}
-                  aria-hidden
-                >
-                  <img
-                    src={category.image_url}
-                    alt={category.name}
-                    className={styles.category__image}
-                  />
-                  {category.name}
-                </div>
-              )
-          ))
-          : categories?.shop_categories.slice(0, 12).map((category) => (
-            category.name === '전체보기' ? (
-              <div
-                key={category.id}
-                className={styles.category__benefit}
-                onClick={() => handleStoreClick()}
-                aria-hidden
-              >
-                <img
-                  src="https://static.koreatech.in/assets/img/icon/call_icon_2.png"
-                  alt={category.name}
-                  className={styles.category__image}
-                />
-                전화 주문 혜택
-              </div>
-            )
-              : (
-                <div
-                  key={category.id}
-                  className={styles.category__item}
-                  onClick={(e) => handleStoreCategoryClick(e, category)}
-                  aria-hidden
-                >
-                  <img
-                    src={category.image_url}
-                    alt={category.name}
-                    className={styles.category__image}
-                  />
-                  {category.name}
-                </div>
-              )
+      <Suspense fallback={null}>
+        <div className={styles.category__wrapper}>
+          {categoriesWithBenefit.map((category) => (
+            <div
+              key={category.id}
+              className={styles.category__item}
+              onClick={(e) => handleCategoryClick(e, category)}
+              aria-hidden
+            >
+              <img
+                src={category.image_url}
+                alt={category.name}
+                className={styles.category__image}
+              />
+              {category.name}
+            </div>
           ))}
-      </div>
+        </div>
+      </Suspense>
     </section>
   );
 }
