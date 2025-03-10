3a7b222c76ad147ffe218ed19cd7f74e3d8f78aa
정해성 <gotjd0230@naver.com>
2025-03-10 14:00:35 +0900

    [hotfix] 식당 운영 시간이 아닌 잘못된 값 보이는 문제 수정 (#733)

--- src/api/coopshop/APIDetail.ts
+++ src/api/coopshop/APIDetail.ts
@@ -2,9 +2,10 @@ import { APIRequest, HTTP_METHOD } from 'interfaces/APIRequest';
 import type { CoopShopDetailResponse, CoopShopResponse } from './entity';
 
 const COOPSHOP_IDS = {
-  학생식당: 10,
-  세탁소: 2,
-  참빛관편의점: 3,
+  학생식당: 1,
+  복지관식당: 2,
+  대즐: 3,
+  서점: 4,
 } as const;
 
 export class CoopShop<R extends CoopShopResponse> implements APIRequest<R> {
