c0d6400d0962f9273071c4ac6c6100f24318f0ff
SeoYeJin <noonesyj@naver.com>
2025-03-30 20:05:59 +0900

    refactor: queryKey 수정

--- src/components/ui/Banner/hooks/useBanners.ts
+++ src/components/ui/Banner/hooks/useBanners.ts
@@ -4,11 +4,8 @@ import { banner } from 'api';
 const useBanners = (categoryId: number) => {
   const { data: banners } = useSuspenseQuery(
     {
-      queryKey: ['banners', categoryId],
-      queryFn: async ({ queryKey }) => {
-        const queryFnParams = queryKey[1];
-        return banner.getBanners(queryFnParams as number);
-      },
+      queryKey: ['banners', `banner-${categoryId}`],
+      queryFn: () => banner.getBanners(Number(categoryId)),
     },
   );
   return { banners };
