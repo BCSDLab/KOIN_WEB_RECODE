bc62759c03df65e28fb9ed14a29e2d1f008511a9
dooohun <ehgns0305@naver.com>
2025-04-07 19:17:09 +0900

    feat: 커스텀 체크 박스 forwardRef로 변경

--- src/pages/Auth/SignupPage/components/CustomCheckbox/index.tsx
+++ src/pages/Auth/SignupPage/components/CustomCheckbox/index.tsx
@@ -1,10 +1,17 @@
-import { InputHTMLAttributes } from 'react';
+import { forwardRef, InputHTMLAttributes } from 'react';
 import styles from './CustomCheckbox.module.scss';
 
 type CustomCheckboxProps = InputHTMLAttributes<HTMLInputElement>;
 
-export default function CustomCheckbox({ ...options }: CustomCheckboxProps) {
-  return (
-    <input type="checkbox" className={styles['terms-checkbox']} {...options} />
-  );
-}
+const CustomCheckbox = forwardRef<HTMLInputElement, CustomCheckboxProps>(
+  ({ className, ...options }, ref) => (
+    <input
+      type="checkbox"
+      className={`${styles['terms-checkbox']} ${className || ''}`}
+      ref={ref}
+      {...options}
+    />
+  ),
+);
+
+export default CustomCheckbox;
