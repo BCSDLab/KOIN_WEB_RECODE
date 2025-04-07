90bb12a76314e20c70f4c8e405e73f9214ec50a9
hyejun <hyejunkkim228@gmail.com>
2025-04-08 03:15:16 +0900

    feat: 모바일 회원유형 선택 페이지 구현

--- src/pages/Auth/SignupPage/Steps/MobileUserTypeStep/index.tsx
+++ src/pages/Auth/SignupPage/Steps/MobileUserTypeStep/index.tsx
@@ -1,3 +1,5 @@
+import styles from './MobileUserTypeStep.module.scss';
+
 type UserType = '학생' | '외부인';
 
 interface MobileUserTypeStepProps {
@@ -6,13 +8,16 @@ interface MobileUserTypeStepProps {
 
 function MobileUserTypeStep({ onSelectType }: MobileUserTypeStepProps) {
   return (
-    <div>
-      <button type="button" onClick={() => onSelectType('학생')}>
-        한국기술교육대학교 학생
-      </button>
-      <button type="button" onClick={() => onSelectType('외부인')}>
-        외부인
-      </button>
+    <div className={styles.container}>
+      <div className={styles.box} />
+      <div className={styles['button-container']}>
+        <button className={`${styles['button-container__button']} ${styles['button-container__button--orange']}`} type="button" onClick={() => onSelectType('학생')}>
+          한국기술교육대학교 학생
+        </button>
+        <button className={`${styles['button-container__button']} ${styles['button-container__button--blue']}`} type="button" onClick={() => onSelectType('외부인')}>
+          외부인
+        </button>
+      </div>
     </div>
   );
 }
