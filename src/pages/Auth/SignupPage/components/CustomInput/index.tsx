8560bc2b4616e4b84c16449fb5f90b2503513817
hyejun <hyejunkkim228@gmail.com>
2025-04-08 03:14:34 +0900

    feat: 회원가입 전용 selector 추가

--- src/pages/Auth/SignupPage/components/CustomInput/index.tsx
+++ src/pages/Auth/SignupPage/components/CustomInput/index.tsx
@@ -1,39 +1,42 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import { useState } from 'react';
+import { ComponentPropsWithoutRef, useState } from 'react';
 import CloseIcon from 'assets/svg/Login/close.svg';
 import EyeOpenIcon from 'assets/svg/Login/eye-open.svg';
 import EyeCloseIcon from 'assets/svg/Login/eye-close.svg';
 import ErrorIcon from 'assets/svg/Login/error.svg';
 import CorrectIcon from 'assets/svg/Login/correct.svg';
 import WarningIcon from 'assets/svg/Login/warning.svg';
+import { useFormContext } from 'react-hook-form';
 import styles from './CustomInput.module.scss';
 
-interface CustomInputProps {
-  value: string;
-  onChange: (value: string) => void;
+export type InputMessage = {
+  type: 'error' | 'warning' | 'success' | 'info';
+  content: string;
+} | null;
+
+interface CustomInputProps extends ComponentPropsWithoutRef<'input'> {
   placeholder?: string;
   type?: 'text' | 'password';
-  message?: string;
-  messageType?: 'error' | 'correct' | 'warning';
-  isDeleteButton?: boolean;
+  message?: InputMessage;
+  isDelete?: boolean;
   isVisibleButton?: boolean;
+  isTimer?: boolean;
+  timerValue?: number;
 }
 
 function CustomInput({
   value,
-  onChange,
   placeholder,
   type = 'text',
-  message,
-  messageType,
-  isDeleteButton = false,
+  message = null,
+  isDelete = false,
   isVisibleButton = false,
+  isTimer = false,
+  timerValue = 180,
+  ...etc
 }: CustomInputProps) {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-
-  const handleDelete = () => {
-    onChange('');
-  };
+  const { setValue } = useFormContext();
 
   const togglePasswordVisibility = () => {
     setIsPasswordVisible((prev) => !prev);
@@ -48,19 +51,37 @@ function CustomInput({
 
   const inputType = getInputType();
 
+  const formatTime = (totalSeconds: number) => {
+    const minutes = Math.floor(totalSeconds / 60)
+      .toString()
+      .padStart(2, '0');
+    const seconds = (totalSeconds % 60).toString().padStart(2, '0');
+    return `${minutes}:${seconds}`;
+  };
+
   return (
     <div className={styles.inputContainer}>
       <div className={styles.inputWrapper}>
+
         <input
           className={styles.inputWrapper__input}
           type={inputType}
           placeholder={placeholder}
           value={value}
-          onChange={(e) => onChange(e.target.value)}
+          {...etc}
         />
 
-        {isDeleteButton && value && (
-          <button type="button" onClick={handleDelete} className={styles.inputWrapper__optionButton}>
+        {isTimer && !timerValue && (
+        <span className={styles.inputWrapper__timer}>
+          {formatTime(timerValue)}
+        </span>
+        )}
+        {isDelete && value && (
+          <button
+            type="button"
+            onClick={() => setValue(etc.name!, '')}
+            className={styles.inputWrapper__optionButton}
+          >
             <CloseIcon />
           </button>
         )}
@@ -71,28 +92,21 @@ function CustomInput({
             onClick={togglePasswordVisibility}
             className={styles.inputWrapper__optionButton}
           >
-            {isPasswordVisible ? <EyeCloseIcon /> : <EyeOpenIcon />}
+            {isPasswordVisible ? <EyeOpenIcon /> : <EyeCloseIcon />}
           </button>
         )}
 
       </div>
+      {message && (
       <div className={styles.messageWrapper}>
-        {messageType === 'error' && <ErrorIcon />}
-        {messageType === 'correct' && <CorrectIcon />}
-        {messageType === 'warning' && <WarningIcon />}
-        {message && (
-        <p
-          className={`
-        ${styles.messageWrapper__message}
-        ${messageType ? styles[`messageWrapper__message--${messageType}`] : ''}
-      `}
-        >
-
-          {message}
+        {message.type === 'error' && <ErrorIcon />}
+        {message.type === 'success' && <CorrectIcon />}
+        {message.type === 'warning' && <WarningIcon />}
+        <p className={`${styles.messageWrapper__message} ${styles[`messageWrapper__message--${message.type}`]}`}>
+          {message.content}
         </p>
-        )}
-
       </div>
+      )}
 
     </div>
   );
