90f211caa6de405f165862cf42c098ded0fcdcca
dooohun <ehgns0305@naver.com>
2025-04-07 19:19:20 +0900

    feat: 이용약관 페이지 기능 추가

--- src/pages/Auth/SignupPage/Steps/Terms/index.tsx
+++ src/pages/Auth/SignupPage/Steps/Terms/index.tsx
@@ -1,6 +1,8 @@
 import { cn } from '@bcsdlab/utils';
 import CustomCheckbox from 'pages/Auth/SignupPage/components/CustomCheckbox';
 import { privacy, koin, marketing } from 'static/terms';
+import { useState } from 'react';
+import { useFormContext } from 'react-hook-form';
 import styles from './Terms.module.scss';
 
 interface TermsProps {
@@ -8,50 +10,117 @@ interface TermsProps {
 }
 
 export default function Terms({ onNext }: TermsProps) {
+  const { register, setValue } = useFormContext();
+  const [agreeState, setAgreeState] = useState({
+    privacy: false,
+    koinUsage: false,
+    marketing: false,
+  });
+
+  const handleAllAgreeToggle = () => {
+    const isAllChecked = !Object.values(agreeState).every(Boolean);
+    setAgreeState(() => ({
+      privacy: isAllChecked,
+      koinUsage: isAllChecked,
+      marketing: isAllChecked,
+    }));
+    Object.keys(agreeState).forEach((field) => setValue(field, isAllChecked));
+  };
+
+  const handleSingleAgreeToggle = (e: React.MouseEvent<HTMLElement>) => {
+    const { id } = e.currentTarget;
+    setAgreeState((prev) => ({
+      ...prev,
+      [id]: !prev[id as keyof typeof prev],
+    }));
+  };
+
   return (
     <div className={styles.container}>
       <span className={styles.title}>회원가입</span>
       <div className={styles.divider} />
       <div className={styles['container-terms']}>
-        <label htmlFor="terms-agree" className={styles['all-terms-agree__label']}>
-          <CustomCheckbox id="terms-agree" />
-          <span className={styles['all-terms-agree__title']}>아래 이용약관에 모두 동의합니다.</span>
+        <label
+          htmlFor="terms-agree"
+          className={styles['all-terms-agree__label']}
+        >
+          <CustomCheckbox
+            id="terms-agree"
+            onChange={handleAllAgreeToggle}
+            checked={Object.values(agreeState).every(Boolean)}
+          />
+          <span className={styles['all-terms-agree__title']}>
+            아래 이용약관에 모두 동의합니다.
+          </span>
         </label>
         <div className={styles['small-divider']} />
-        <div className={styles.term}>
-          <CustomCheckbox />
+        <label className={styles.term} htmlFor="agreeToPrivacyPolicy">
+          <CustomCheckbox
+            id="agreeToPrivacyPolicy"
+            checked={agreeState.privacy}
+            onClick={handleSingleAgreeToggle}
+            {...register('agreeToPrivacyPolicy')}
+          />
           <span className={styles.term__title}>[필수] 개인정보 이용약관</span>
-        </div>
-        <textarea className={styles.term__content} readOnly defaultValue={privacy} />
-        <div className={cn({
-          [styles.term]: true,
-          [styles['term__koin-use']]: true,
-        })}
+        </label>
+        <textarea
+          className={styles.term__content}
+          readOnly
+          defaultValue={privacy}
+        />
+        <label
+          className={cn({
+            [styles.term]: true,
+            [styles['term__koin-use']]: true,
+          })}
+          htmlFor="agreeToKoinTerms"
         >
-          <CustomCheckbox />
+          <CustomCheckbox
+            id="agreeToKoinTerms"
+            checked={agreeState.koinUsage}
+            onClick={handleSingleAgreeToggle}
+            {...register('agreeToKoinTerms')}
+          />
           <span className={styles.term__title}>[필수] 코인 이용약관</span>
-        </div>
-        <textarea className={styles.term__content} readOnly defaultValue={koin} />
-        <div className={cn({
-          [styles.term]: true,
-          [styles.term__marketing]: true,
-        })}
+        </label>
+        <textarea
+          className={styles.term__content}
+          readOnly
+          defaultValue={koin}
+        />
+        <label
+          className={cn({
+            [styles.term]: true,
+            [styles.term__marketing]: true,
+          })}
+          htmlFor="agreeToMarketing"
         >
-          <CustomCheckbox />
+          <CustomCheckbox
+            id="agreeToMarketing"
+            checked={agreeState.marketing}
+            onClick={handleSingleAgreeToggle}
+            {...register('agreeToMarketing', { required: false })}
+          />
           <span className={styles.term__title}>[선택] 마케팅 수신 동의</span>
-        </div>
-        <textarea className={styles.term__content} readOnly defaultValue={marketing} />
+        </label>
+        <textarea
+          className={styles.term__content}
+          readOnly
+          defaultValue={marketing}
+        />
       </div>
       <div className={styles.divider} />
       <button
-        type="button"
+        type="submit"
         className={styles['term-button']}
-        // disabled
         onClick={onNext}
+        disabled={!(agreeState.privacy && agreeState.koinUsage)}
       >
         다음
       </button>
-      <span className={styles.copyright}>COPYRIGHT © 2023 BCSD LAB ALL RIGHTS RESERVED.</span>
+      <span className={styles.copyright}>
+        COPYRIGHT © 2023 BCSD LAB ALL RIGHTS RESERVED.
+      </span>
     </div>
   );
 }
