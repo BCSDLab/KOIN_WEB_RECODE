f5bcb5cd0ee34deabc044c184582a553fef0c7a5
dooohun <ehgns0305@naver.com>
2025-04-28 22:09:08 +0900

    feat: 로그인 페이지 이미지 추가

--- src/pages/Auth/AuthPage/index.tsx
+++ src/pages/Auth/AuthPage/index.tsx
@@ -1,10 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
+import ROUTES from 'static/routes';
 import styles from './Auth.module.scss';
 
 function AuthPage() {
   return (
     <div className={styles.template}>
       <div className={styles.template__content}>
+        <Link className={styles.template__logo} to={ROUTES.Main()}>
+          <img className={styles.template__image} src="https://static.koreatech.in/assets/img/logo_primary.png" alt="main logo" />
+        </Link>
         <Outlet />
       </div>
     </div>
