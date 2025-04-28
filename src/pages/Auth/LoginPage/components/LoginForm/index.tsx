25c56b6b869645e2703476e2600b0a632f3b288e
dooohun <ehgns0305@naver.com>
2025-04-28 20:29:25 +0900

    feat: 로그인 폼 컴포넌트 분리

---
+++ src/pages/Auth/LoginPage/components/LoginForm/index.tsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import useBooleanState from 'utils/hooks/state/useBooleanState';
+import { useLogin } from 'pages/Auth/LoginPage/hooks/useLogin';
+import BlindIcon from 'assets/svg/blind-icon.svg';
+import ShowIcon from 'assets/svg/show-icon.svg';
+import useLogger from 'utils/hooks/analytics/useLogger';
+import { cn } from '@bcsdlab/utils';
+import useMediaQuery from 'utils/hooks/layout/useMediaQuery';
+import styles from './LoginForm.module.scss';
+
+interface IClassUser {
+  userId: HTMLInputElement | null;
+  password: HTMLInputElement | null;
+}
+
+export default function LoginForm() {
+  const [isPasswordVisible, , , toggleIsPasswordVisible] = useBooleanState(false);
+  const [isAutoLoginFlag, , , toggleIsAutoLoginFlag] = useBooleanState(false);
+  const isMobile = useMediaQuery();
+  const submitLogin = useLogin({ isAutoLoginFlag });
+  const loginRef = React.useRef<IClassUser>({
+    userId: null,
+    password: null,
+  });
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { userId, password } = loginRef.current;
+    submitLogin({
+      userId: userId ? userId.value : '',
+      password: password ? password.value : '',
+    });
+  };
+  const logger = useLogger();
+
+  return (
+    <>
+      <form className={styles.loginform} onSubmit={onSubmit}>
+        <input
+          ref={(inputRef) => {
+            loginRef.current.userId = inputRef;
+          }}
+          className={styles['form-input']}
+          autoComplete="username"
+          name="userId"
+          placeholder="아이디(Koreatech ID/전화번호)"
+        />
+        <div className={styles['form-input-wrapper']}>
+          <input
+            ref={(inputRef) => {
+              loginRef.current.password = inputRef;
+            }}
+            className={styles['form-input']}
+            type={isPasswordVisible ? 'text' : 'password'}
+            autoComplete="current-password"
+            name="password"
+            placeholder="비밀번호"
+          />
+          <button
+            type="button"
+            className={styles['password-toggle-button']}
+            aria-label={isPasswordVisible ? '비밀번호 숨기기' : '비밀번호 보기'}
+            onClick={toggleIsPasswordVisible}
+          >
+            {isPasswordVisible ? <ShowIcon /> : <BlindIcon />}
+          </button>
+        </div>
+        <button
+          type="submit"
+          className={cn({
+            [styles.loginform__button]: true,
+            [styles['loginform__first-button']]: true,
+          })}
+          onClick={() => {
+            logger.actionEventClick({
+              team: 'USER',
+              event_label: 'login',
+              value: '로그인완료',
+            });
+          }}
+        >
+          로그인
+        </button>
+        {isMobile && (
+        <button
+          type="button"
+          className={styles.loginform__button}
+          onClick={() => {
+            logger.actionEventClick({
+              team: 'USER',
+              event_label: 'login',
+              value: '회원가입',
+            });
+          }}
+        >
+          회원가입
+        </button>
+        )}
+      </form>
+      {!isMobile && (
+      <div aria-hidden="true" className={styles['auto-login']}>
+        <label className={styles['auto-login__label']} htmlFor="autoLoginCheckBox">
+          <input
+            className={styles['auto-login__checkbox']}
+            checked={isAutoLoginFlag}
+            onChange={toggleIsAutoLoginFlag}
+            type="checkbox"
+            id="autoLoginCheckBox"
+          />
+          자동 로그인
+        </label>
+      </div>
+      )}
+    </>
+  );
+}
