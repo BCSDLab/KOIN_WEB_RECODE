3843fc512044e3299095ccb4d271ddb3caaf6b39
dooohun <ehgns0305@naver.com>
2025-04-30 11:03:25 +0900

    feat: 로그인 화면에서만 로고가 뜨도록 추가

--- src/pages/Auth/AuthPage/index.tsx
+++ src/pages/Auth/AuthPage/index.tsx
@@ -1,14 +1,17 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 import ROUTES from 'static/routes';
 import styles from './Auth.module.scss';
 
 function AuthPage() {
+  const location = useLocation();
   return (
     <div className={styles.template}>
       <div className={styles.template__content}>
+        {location.pathname === ROUTES.Auth() && (
         <Link className={styles.template__logo} to={ROUTES.Main()}>
           <img className={styles.template__image} src="https://static.koreatech.in/assets/img/logo_primary.png" alt="main logo" />
         </Link>
+        )}
         <Outlet />
       </div>
     </div>
