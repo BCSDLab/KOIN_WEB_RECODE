cd79165cdb38cd4e1e248c195e10d1da9075ae40
JeongWon-CHO <ahin0312@naver.com>
2025-05-01 00:23:10 +0900

    feat: 이메일 예외처리 추가

--- src/static/auth.ts
+++ src/static/auth.ts
@@ -2,6 +2,8 @@ export const REGEX = {
   PASSWORD: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_\-+=~`[\]{}|\\:;"'<>,.?/])[A-Za-z\d!@#$%^&*()_\-+=~`[\]{}|\\:;"'<>,.?/]{6,18}$/,
   NICKNAME: /^[가-힣a-zA-Z0-9]{1,10}$/,
   EMAIL: /^[a-z_0-9]{1,12}$/,
+  USERID: /^.{1,13}$/,
+  STUDENT_NUMBER: /^\d{8,10}$/,
 };
 
 export const MESSAGES = {
@@ -27,11 +29,22 @@ export const MESSAGES = {
     CORRECT: '인증번호가 일치합니다.',
     STOP: '1일 발송 한도를 초과했습니다. 24시간 이후 재시도 바랍니다.',
   },
+  USERID: {
+    AVAILABLE: '사용 가능한 아이디입니다.',
+    DUPLICATED: '이미 사용 중인 아이디입니다.',
+    INVALID: '올바른 아이디 양식이 아닙니다. 다시 입력해 주세요.',
+  },
+  STUDENT_NUMBER: {
+    FORMAT: '올바른 학번 양식이 아닙니다. 다시 입력해 주세요.',
+  },
+  EMAIL: {
+    FORMAT: '올바른 이메일 형식이 아닙니다. 다시 입력해 주세요.',
+  },
 };
 
 export type UserType = '학생' | '외부인';
 
 export const GENDER_OPTIONS = [
-  { label: '남성', value: 'male' },
-  { label: '여성', value: 'female' },
+  { label: '남성', value: '0' },
+  { label: '여성', value: '1' },
 ];
