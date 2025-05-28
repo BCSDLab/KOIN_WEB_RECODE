58d138c2bd97caf450ceb4bce3abae51c18fd0ef
seoyejin <127932065+Yejin0070@users.noreply.github.com>
2025-05-28 17:32:40 +0900

    [캠퍼스] 동아리 목록 모바일뷰 수정 (#870)

--- src/pages/Club/ClubListPage/index.tsx
+++ src/pages/Club/ClubListPage/index.tsx
@@ -1,4 +1,5 @@
 import { cn } from '@bcsdlab/utils';
+import useLogger from 'utils/hooks/analytics/useLogger';
 import useMediaQuery from 'utils/hooks/layout/useMediaQuery';
 import useModalPortal from 'utils/hooks/layout/useModalPortal';
 import { Portal } from 'components/modal/Modal/PortalProvider';
@@ -10,6 +11,11 @@ import useClubLike from 'pages/Club/hooks/useClubLike';
 import { Selector } from 'components/ui/Selector';
 import HeartOutline from 'assets/svg/Club/heart-outline-icon.svg';
 import HeartFilled from 'assets/svg/Club/heart-filled-icon.svg';
+import MikeIcon from 'assets/svg/Club/mike-icon.svg';
+import ExerciseIcon from 'assets/svg/Club/exercise-icon.svg';
+import HobbyIcon from 'assets/svg/Club/hobby-icon.svg';
+import ReligionIcon from 'assets/svg/Club/religion-icon.svg';
+import BookIcon from 'assets/svg/Club/book-icon.svg';
 import ClubAuthModal from 'pages/Club/components/ClubAuthModal';
 import styles from './ClubListPage.module.scss';
 
@@ -21,6 +27,7 @@ const SORT_OPTIONS = [
 ];
 
 function ClubListPage() {
+  const logger = useLogger();
   const portalManager = useModalPortal();
   const isMobile = useMediaQuery();
   const token = useTokenState();
@@ -33,13 +40,29 @@ function ClubListPage() {
   const totalCount = clubList.length;
   const { mutate: clubLikeMutate } = useClubLike();
 
+  const handleCreateClubClick = () => {
+    logger.actionEventClick({
+      team: 'CAMPUS',
+      event_category: 'click',
+      event_label: 'club_main_create',
+      value: '생성하기',
+    });
+    navigate('/clubs/new');
+  };
+
   const onChangeSort = (e: { target: { value: string } }) => {
     const changedSort = e.target.value;
     searchParams.set('sort', changedSort);
     setSearchParams(searchParams);
   };
 
-  const handleCategoryClick = (id: number) => {
+  const handleCategoryClick = (name: string, id: number) => {
+    logger.actionEventClick({
+      team: 'CAMPUS',
+      event_category: 'click',
+      event_label: 'club_main_category',
+      value: `${name}`,
+    });
     const isSelected = selectedCategoryId === id;
     if (isSelected) {
       searchParams.delete('categoryId');
@@ -47,6 +70,16 @@ function ClubListPage() {
       searchParams.set('categoryId', String(id));
     }
     setSearchParams(searchParams);
+  };
+
+  const handleCardClick = (name: string, id: number) => {
+    logger.actionEventClick({
+      team: 'CAMPUS',
+      event_category: 'click',
+      event_label: 'club_main_select',
+      value: `${name}`,
+    });
+    navigate(`/clubs/${id}`);
   };
 
   const handleLikeClick = (
@@ -78,7 +111,7 @@ function ClubListPage() {
           <button
             type="button"
             className={styles['header__add-button']}
-            onClick={() => navigate('/clubs/new')}
+            onClick={() => handleCreateClubClick()}
           >
             동아리 생성하기
           </button>
@@ -88,14 +121,26 @@ function ClubListPage() {
             {!isMobile && <p className={styles.categories__label}>Category</p>}
             {clubCategories.map((category) => (
               <button
-                key={category.id}
-                type="button"
-                onClick={() => handleCategoryClick(category.id)}
                 className={cn({
                   [styles.categories__button]: true,
                   [styles['categories__button--selected']]: category.id === selectedCategoryId,
                 })}
+                key={category.id}
+                type="button"
+                onClick={() => handleCategoryClick(category.name, category.id)}
               >
+                <div
+                  className={cn({
+                    [styles['categories__button-icon']]: true,
+                    [styles['categories__button-icon--selected']]: category.id === selectedCategoryId,
+                  })}
+                >
+                  {category.id === 1 && <MikeIcon />}
+                  {category.id === 2 && <ExerciseIcon />}
+                  {category.id === 3 && <HobbyIcon />}
+                  {category.id === 4 && <ReligionIcon />}
+                  {category.id === 5 && <BookIcon />}
+                </div>
                 {category.name}
               </button>
             ))}
@@ -125,7 +170,7 @@ function ClubListPage() {
                 type="button"
                 key={club.id}
                 className={styles.card}
-                onClick={() => navigate(`/clubs/${club.id}`)}
+                onClick={() => handleCardClick(club.name, club.id)}
               >
                 <div className={styles.card__info}>
                   <div className={styles['card__info-header']}>
@@ -135,16 +180,16 @@ function ClubListPage() {
                   <div className={styles['card__info-likes']}>
                     <button
                       type="button"
-                      onClick={(e) => handleLikeClick(e, club.isLiked, club.id)}
+                      onClick={(e) => handleLikeClick(e, club.is_liked, club.id)}
                     >
-                      {club.isLiked ? <HeartFilled /> : <HeartOutline />}
+                      {club.is_liked ? <HeartFilled /> : <HeartOutline />}
                     </button>
                     <p>{club.likes}</p>
                   </div>
                 </div>
                 <img
                   className={styles.card__logo}
-                  src={club.imageUrl}
+                  src={club.image_url}
                   alt={club.name}
                 />
               </button>
