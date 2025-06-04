9fac2c9ed71b5f712cc5ec0682e5fe9c755e6cc5
dooohun <ehgns0305@naver.com>
2025-06-04 19:49:59 +0900

    fix: 유저 타입 가드 함수의 인자로 null 값도 받을 수 있도록 변경

--- src/utils/ts/userTypeGuards.ts
+++ src/utils/ts/userTypeGuards.ts
@@ -1,10 +1,10 @@
 import { UserResponse } from 'api/auth/entity';
 import { UnionUserResponse } from 'utils/hooks/state/useUser';
 
-export function isStudentUser(user: UnionUserResponse): user is UserResponse {
+export function isStudentUser(user: UnionUserResponse | null): user is UserResponse {
   return (user as UserResponse).student_number !== undefined;
 }
 
-export function isGeneralUser(user: UnionUserResponse): user is UserResponse {
+export function isGeneralUser(user: UnionUserResponse | null): user is UserResponse {
   return (user as UserResponse).student_number === undefined;
 }
