1b97993ec3c16b1293b89b55fcf26b2953e772c3
JeongWon-CHO <ahin0312@naver.com>
2025-06-05 18:31:25 +0900

    refactor: map에 key 값 추가

--- src/pages/Auth/FindPasswordPage/Complete/index.tsx
+++ src/pages/Auth/FindPasswordPage/Complete/index.tsx
@@ -5,16 +5,17 @@ import ROUTES from 'static/routes';
 import useMediaQuery from 'utils/hooks/layout/useMediaQuery';
 import styles from './Complete.module.scss';
 
+const TITLE = '비밀번호 변경 완료';
+const SUB_TITLE = [
+  { key: 'line1', text: '비밀번호가 변경되었습니다.' },
+  { key: 'line2', text: '새로운 비밀번호로 로그인 부탁드립니다.' },
+];
+
 function CompletePage() {
   const navigate = useNavigate();
   const isMobile = useMediaQuery();
 
   const goToLogin = () => navigate(ROUTES.Auth());
-
-  const title = '비밀번호 변경 완료';
-  const subTitles = isMobile
-    ? ['비밀번호가 변경되었습니다.', '새로운 비밀번호로 로그인 부탁드립니다.']
-    : ['비밀번호 변경이 완료되었습니다.', '홈페이지에서 바로 로그인이 가능합니다.'];
 
   return (
     <div className={styles.container}>
@@ -23,14 +24,11 @@ function CompletePage() {
       </div>
 
       <div className={styles['title-container']}>
-        <h2 className={styles.title}>{title}</h2>
+        <h2 className={styles.title}>{TITLE}</h2>
 
         <div className={styles['subTitle-container']}>
-          {subTitles.map((line, idx) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <div key={idx} className={styles.subTitle}>
-              {line}
-            </div>
+          {SUB_TITLE.map(({ key, text }) => (
+            <div key={key} className={styles.subTitle}>{text}</div>
           ))}
         </div>
       </div>
