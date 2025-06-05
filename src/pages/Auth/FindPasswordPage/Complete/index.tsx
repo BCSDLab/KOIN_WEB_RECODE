aeee53fd0647192d83a08615c145e294d72c8f95
JeongWon-CHO <ahin0312@naver.com>
2025-06-05 14:47:32 +0900

    feat: 완료 페이지 추가

--- src/pages/Auth/FindPasswordPage/Complete/index.tsx
+++ src/pages/Auth/FindPasswordPage/Complete/index.tsx
@@ -1,23 +1,48 @@
+import LogoIcon from 'assets/svg/Login/logo.svg';
 import { useNavigate } from 'react-router-dom';
 import ROUTES from 'static/routes';
+import useMediaQuery from 'utils/hooks/layout/useMediaQuery';
+import styles from './Complete.module.scss';
 
 function CompletePage() {
   const navigate = useNavigate();
+  const isMobile = useMediaQuery();
 
-  const goToLogin = () => {
-    navigate(ROUTES.Auth());
-  };
+  const goToLogin = () => navigate(ROUTES.Auth());
+
+  const title = '비밀번호 변경 완료';
+  const subTitles = isMobile
+    ? ['비밀번호가 변경되었습니다.', '새로운 비밀번호로 로그인 부탁드립니다.']
+    : ['비밀번호 변경이 완료되었습니다.', '홈페이지에서 바로 로그인이 가능합니다.'];
 
   return (
-    <div>
-      Complete Page
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <LogoIcon />
+      </div>
 
-      <button
-        type="button"
-        onClick={goToLogin}
-      >
-        로그인 화면으로
-      </button>
+      <div className={styles['title-container']}>
+        <h2 className={styles.title}>{title}</h2>
+
+        <div className={styles['subTitle-container']}>
+          {subTitles.map((line, idx) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <div key={idx} className={styles.subTitle}>
+              {line}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className={styles['button-container']}>
+        <button
+          type="button"
+          className={styles['button-container__button']}
+          onClick={goToLogin}
+        >
+          로그인 화면 바로가기
+        </button>
+      </div>
     </div>
   );
 }
