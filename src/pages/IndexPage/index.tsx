93eaf6471341561b5d732a2088841de609e6058b
SeoYeJin <noonesyj@naver.com>
2025-06-08 16:05:47 +0900

    feat: 배너 B 제거

--- src/pages/IndexPage/index.tsx
+++ src/pages/IndexPage/index.tsx
@@ -1,8 +1,5 @@
 import { Suspense } from 'react';
-import { useABTestView } from 'utils/hooks/abTest/useABTestView';
-import useTokenState from 'utils/hooks/state/useTokenState';
 import Banner from 'components/ui/Banner';
-import BannerB from 'components/ui/BannerB';
 import useBannerCategories from 'components/ui/Banner/hooks/useBannerCategories';
 import IndexBus from './components/IndexBus';
 import IndexClub from './components/IndexClub';
@@ -14,18 +11,12 @@ import styles from './IndexPage.module.scss';
 
 function IndexPage() {
   const bannerCategory = (useBannerCategories() || [])[0];
-  const token = useTokenState();
-  const ABView = useABTestView('main_banner_ui', token);
 
   return (
     <Suspense fallback={null}>
       <main className={styles.template}>
         <Suspense fallback={null}>
-          { ABView === 'BannerA' ? (
-            <Banner categoryName={bannerCategory.name} categoryId={bannerCategory.id} />
-          ) : (
-            <BannerB categoryName={bannerCategory.name} categoryId={bannerCategory.id} />
-          )}
+          <Banner categoryName={bannerCategory.name} categoryId={bannerCategory.id} />
         </Suspense>
         <div className={styles['left-container']}>
           <IndexStore />
