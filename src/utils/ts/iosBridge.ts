d6c8c39dd8873e716685e1d0f2962117df176a63
(hoooooony) <(cjh41820@gmail.com)>
2025-06-09 22:10:07 +0900

    fix : 불필요한 테스트용 토스트 삭제

--- src/utils/ts/iosBridge.ts
+++ src/utils/ts/iosBridge.ts
@@ -1,10 +1,8 @@
 import { useTokenStore } from 'utils/zustand/auth';
-import showToast from './showToast';
 
 export function setTokensFromNative(access: string, refresh: string) {
   if (access) useTokenStore.getState().setToken(access);
   if (refresh) useTokenStore.getState().setRefreshToken(refresh);
-  showToast('default', `엑세스 토큰 :${access} 리프레쉬토큰 : ${refresh}`);
 }
 
 export function requestTokensFromNative() {
