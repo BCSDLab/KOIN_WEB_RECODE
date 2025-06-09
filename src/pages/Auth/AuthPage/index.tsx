3a094247e1aad26765bdc828af9d519db66ca353
(hoooooony) <(cjh41820@gmail.com)>
2025-06-09 19:06:23 +0900

    feat : 웹뷰 뒤로가기를 위한 뒤로가기 버튼

--- src/pages/Auth/AuthPage/index.tsx
+++ src/pages/Auth/AuthPage/index.tsx
@@ -1,11 +1,36 @@
-import { Outlet, Link, useLocation } from 'react-router-dom';
+import {
+  Outlet, Link, useLocation, useNavigate,
+} from 'react-router-dom';
 import ROUTES from 'static/routes';
+import useMediaQuery from 'utils/hooks/layout/useMediaQuery';
+import BlackArrowBackIcon from 'assets/svg/black-arrow-back-icon.svg';
+import { backbuttonTapp } from 'utils/ts/iosBridge';
 import styles from './Auth.module.scss';
 
 function AuthPage() {
   const location = useLocation();
+  const isMobile = useMediaQuery();
+  const navigate = useNavigate();
+  const handleClickBack = () => {
+    if (typeof window !== 'undefined'
+    && window.webkit?.messageHandlers != null) {
+      backbuttonTapp();
+    } else {
+      navigate(-1);
+    }
+  };
   return (
     <div className={styles.template}>
+      {isMobile && (
+        <button
+          type="button"
+          aria-label="뒤로가기"
+          className={styles['back-button']}
+          onClick={handleClickBack}
+        >
+          <BlackArrowBackIcon />
+        </button>
+      )}
       <div className={styles.template__content}>
         {location.pathname === ROUTES.Auth() && (
         <Link className={styles.template__logo} to={ROUTES.Main()}>
