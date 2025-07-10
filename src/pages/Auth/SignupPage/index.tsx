2905071100ca698ae56bf3a1229280cb89248e99
hyejun <hyejunkkim228@gmail.com>
2025-07-10 20:20:45 +0900

    fix: 함수명 수정

--- src/pages/Auth/SignupPage/index.tsx
+++ src/pages/Auth/SignupPage/index.tsx
@@ -1,5 +1,7 @@
 import LoadingSpinner from 'components/feedback/LoadingSpinner';
-import { Suspense, useEffect, useState } from 'react';
+import {
+  Suspense, useCallback, useEffect, useState,
+} from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import ChevronLeftIcon from 'assets/svg/Login/chevron-left.svg';
 import useMediaQuery from 'utils/hooks/layout/useMediaQuery';
@@ -54,12 +56,16 @@ function SignupPage() {
 
   const isVerificationStepPassed = methods.getValues('name') && methods.getValues('phone_number') && methods.getValues('gender');
 
+  const goToFirstStep = useCallback(() => {
+    nextStep('약관동의');
+  }, [nextStep]);
+
   useEffect(() => {
     if ((currentStep === '회원유형선택' && !isVerificationStepPassed) || (currentStep === '정보입력' && !userType)) {
       showToast('warning', '잘못된 접근입니다.');
-      nextStep('약관동의');
+      goToFirstStep();
     }
-  }, [currentStep, userType, methods, nextStep, isVerificationStepPassed]);
+  }, [currentStep, userType, isVerificationStepPassed, goToFirstStep]);
 
   return (
     <Suspense fallback={<LoadingSpinner size="50px" />}>
