ba555d76e4aedc7fcf18623baaad7b918a16aadb
이준영 <ff1451@gmail.com>
2025-07-12 14:31:41 +0900

    refactor: PC View, Mobile View 파일 통합

--- src/pages/Club/NewClubRecruitment/components/MobileView/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function MobileView() {
-  return (
-    <div>
-      <h1>Mobile View</h1>
-      <p>This is the mobile view for the new club recruitment page.</p>
-    </div>
-  );
-}
