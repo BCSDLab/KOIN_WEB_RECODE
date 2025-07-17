83ff595c6bd26150c807ef583e29d71c542519e0
이준영 <ff1451@gmail.com>
2025-07-18 02:33:47 +0900

    fix: 시간이 없는 날짜값을 변경하는 과정에서 UTC 기준으로 변경되는 문제 해결

--- src/pages/Club/ClubRecruitmentEditPage/index.tsx
+++ src/pages/Club/ClubRecruitmentEditPage/index.tsx
@@ -19,7 +19,10 @@ function splitKoreanDate(date: Date): [string, string] {
 }
 
 function getYyyyMmDd(date: Date): string {
-  return date.toISOString().slice(0, 10);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
 }
 
 const TODAY = getYyyyMmDd(new Date());
