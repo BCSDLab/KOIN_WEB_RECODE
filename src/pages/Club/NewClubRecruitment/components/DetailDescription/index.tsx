aa938eb4ad4a84e6843e3080f2d867c0abe2f92e
이준영 <ff1451@gmail.com>
2025-07-20 08:59:29 +0900

    feat: textarea auto resizing

--- src/pages/Club/NewClubRecruitment/components/DetailDescription/index.tsx
+++ src/pages/Club/NewClubRecruitment/components/DetailDescription/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import styles from './DetailDescription.module.scss';
 
 interface DetailDescriptionProps {
@@ -6,6 +7,16 @@ interface DetailDescriptionProps {
 }
 
 export default function DetailDescription({ value, onChange }: DetailDescriptionProps) {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+      textareaRef.current.style.maxHeight = '500px';
+    }
+  }, [value]);
+
   return (
     <div className={styles.form__item}>
       <div className={styles.form__label}>상세 내용</div>
@@ -15,6 +26,7 @@ export default function DetailDescription({ value, onChange }: DetailDescription
         value={value}
         onChange={(e) => onChange(e.target.value)}
         placeholder="상세 내용을 적어주세요."
+        ref={textareaRef}
       />
     </div>
   );
