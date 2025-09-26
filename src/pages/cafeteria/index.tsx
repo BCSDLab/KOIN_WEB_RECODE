567ee7b8438e12110269cc61b004d5c9e4397457
dooohun <ehgns0305@naver.com>
2025-09-26 23:17:41 +0900

    fix: 식단 메인페이지 접속 시 로깅되도록 변경

--- src/pages/cafeteria/index.tsx
+++ src/pages/cafeteria/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useScrollToTop from 'utils/hooks/ui/useScrollToTop';
 import useMediaQuery from 'utils/hooks/layout/useMediaQuery';
 import MobileCafeteriaPage from 'components/cafeteria/MobileCafeteriaPage';
@@ -6,12 +6,28 @@ import { DiningType } from 'api/dinings/entity';
 import { DiningTime } from 'components/cafeteria/utils/time';
 import { useDatePicker } from 'components/cafeteria/hooks/useDatePicker';
 import PCCafeteriaPage from 'components/cafeteria/PCCafeteriaPage';
+import { useSessionLogger } from 'utils/hooks/analytics/useSessionLogger';
+import useTokenState from 'utils/hooks/state/useTokenState';
+import { useABTestView } from 'utils/hooks/abTest/useABTestView';
 import styles from './Cafeteria.module.scss';
 
 function CafeteriaPage() {
   const isMobile = useMediaQuery();
   const [diningType, setDiningType] = useState<DiningType>(new DiningTime().getType());
   const { currentDate } = useDatePicker();
+  const sessionLogger = useSessionLogger();
+  const token = useTokenState();
+  const designVariant = useABTestView('dining_store', token);
+
+  useEffect(() => {
+    sessionLogger.actionSessionEvent({
+      event_label: 'dining2shop_1',
+      value: designVariant === 'control' ? 'design_A' : 'design_B',
+      event_category: 'a/b test 로깅(메인화면 식단 진입)',
+      session_name: 'dining2shop',
+      session_lifetime_minutes: 30,
+    });
+  });
 
   useScrollToTop();
 
@@ -23,11 +39,13 @@ function CafeteriaPage() {
             <MobileCafeteriaPage
               diningType={diningType}
               setDiningType={setDiningType}
+              designVariant={designVariant}
             />
           ) : (
             <PCCafeteriaPage
               diningType={diningType}
               setDiningType={setDiningType}
+              designVariant={designVariant}
             />
           )}
       </div>
