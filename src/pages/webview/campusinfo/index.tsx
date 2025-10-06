34927b0ffdc2b97b161f20adcb59deb7e78c852c
Lee Junyoung <54898597+ff1451@users.noreply.github.com>
2025-10-06 17:34:59 +0900

    [교내 시설물 정보] 웹뷰 환경에서 레이아웃 렌더링 (#1005)

--- src/pages/webview/campusinfo/index.tsx
+++ src/pages/webview/campusinfo/index.tsx
@@ -3,3 +3,5 @@ import CampusInfo from 'components/CampusInfo';
 export default function WebviewCampusInfo() {
   return <CampusInfo />;
 }
+
+WebviewCampusInfo.getLayout = (page: React.ReactNode) => page;
