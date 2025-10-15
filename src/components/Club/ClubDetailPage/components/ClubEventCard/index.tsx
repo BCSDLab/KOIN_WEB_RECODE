263a36bdc61f7e95f1a41ca64cbee3d9b2db5079
이준영 <ff1451@gmail.com>
2025-10-16 01:16:40 +0900

    feat: 행사 알림 UI 구현

--- src/components/Club/ClubDetailPage/components/ClubEventCard/index.tsx
+++ src/components/Club/ClubDetailPage/components/ClubEventCard/index.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@bcsdlab/utils';
 import { ClubEvent } from 'api/club/entity';
+import SmallBellIcon from 'assets/svg/Club/small_bell-icon.svg'
 import useLogger from 'utils/hooks/analytics/useLogger';
 import useMediaQuery from 'utils/hooks/layout/useMediaQuery';
 import styles from './ClubEventCard.module.scss';
@@ -40,6 +41,8 @@ export default function ClubEventCard({ event, setEventId, clubName }: ClubEvent
   const isMobile = useMediaQuery();
   const logger = useLogger();
 
+  const isUpcomingMobile = isMobile && event.status === 'UPCOMING';
+
   const handleClickEventCard = () => {
     logger.actionEventClick({
       team: 'CAMPUS',
@@ -61,15 +64,13 @@ export default function ClubEventCard({ event, setEventId, clubName }: ClubEvent
   };
 
   return (
-    <div
-      role="button"
+    <button
+      type="button"
       className={cn({
         [styles['club-event-card']]: true,
         [styles['club-event-card--ended']]: event.status === 'ENDED',
       })}
       onClick={handleClickEventCard}
-      tabIndex={0}
-      onKeyDown={handleKeyDownEventCard}
     >
       {event.image_urls.length > 0 && (
         <img src={event.image_urls[0]} alt={event.name} className={styles['club-event-card__image']} />
@@ -85,39 +86,62 @@ export default function ClubEventCard({ event, setEventId, clubName }: ClubEvent
             [styles['club-event-card__status--ongoing']]: event.status === 'ONGOING',
           })}
           >
-            {getStatusLabel(event.status)}
+            {isUpcomingMobile ? (
+              <div
+                role="button"
+                className={styles['club-event-card__status-button']}
+                aria-label="행사 예정 상태 버튼"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  console.log('알림 설정/해제');
+                }}
+              >
+                {getStatusLabel(event.status)}
+                <SmallBellIcon
+                  className={styles['club-event-card__status-icon']}
+                  aria-hidden
+                  focusable="false"
+                />
+              </div>
+            ) : (
+              <>
+                {getStatusLabel(event.status)}
+              </>
+            )}
           </div>
         </div>
-        <p>
-          {isMobile ? (
-            <>
-              <div>
+        <div className={styles['club-event-card__body']}>
+          <div>
+            {isMobile ? (
+              <>
+                <div>
+                  {splitDateTime(event.start_date)}
+                  {' '}
+                  ~
+                </div>
+                <div>
+                  {splitDateTime(event.end_date)}
+                </div>
+              </>
+            ) : (
+              <>
+                진행 날짜 :
+                {' '}
                 {splitDateTime(event.start_date)}
                 {' '}
                 ~
-              </div>
-              <div>
+                {' '}
                 {splitDateTime(event.end_date)}
-              </div>
-            </>
-          ) : (
-            <>
-              진행 날짜 :
-              {' '}
-              {splitDateTime(event.start_date)}
-              {' '}
-              ~
-              {' '}
-              {splitDateTime(event.end_date)}
-            </>
-          )}
-        </p>
-        <p>
-          행사 소개 :
-          {' '}
-          {event.introduce}
-        </p>
+              </>
+            )}
+          </div>
+          <div>
+            행사 소개 :
+            {' '}
+            {event.introduce}
+          </div>
+        </div>
       </div>
-    </div>
+    </button>
   );
 }
