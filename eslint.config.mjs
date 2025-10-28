a01a9b26f5ecf808719ce6e57f9fd9e1a66903d7
이준영 <ff1451@gmail.com>
2025-10-29 02:36:51 +0900

    chore: lint 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,29 +7,24 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import importPlugin from 'eslint-plugin-import';
 import globals from 'globals';
 
-const nextRules = {
-  ...(next.configs.recommended.rules),
-  ...(next.configs['core-web-vitals'].rules),
-};
-
-const reactRules = {
-  ...(react.configs.recommended.rules),
-  ...(react.configs['jsx-runtime'].rules),
-}
-
-const importFlat = importPlugin.flatConfigs.recommended;
-const a11yFlat = jsxA11y.flatConfigs.recommended;
-
 export default [
-  importFlat,
-  a11yFlat,
+  importPlugin.flatConfigs.recommended,
+  jsxA11y.flatConfigs.recommended,
+  ...tseslint.configs.recommended,
 
   {
-    ignores: ['**/.next/**', '**/dist/**', '**/.yarn/**', 'node_modules/**', '**/*.d.ts'],
+    files: ['src/**/*.{ts,tsx,js,jsx}'],
+    ignores: [
+      '**/.next/**',
+      '**/dist/**',
+      '**/.yarn/**',
+      'node_modules/**',
+      '**/*.d.ts',
+      '**/.pnp.*',
+      'prettier.config.js',
+      'eslint.config.mjs',
+    ],
   },
-  // 기존 Typescript ESLint 미설정, 논의 후 적용 필요
-  // ...tseslint.configs.recommendedTypeChecked, 
-
   {
     files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
@@ -58,33 +53,46 @@ export default [
       node: { tryExtensions: ['.js', '.jsx', '.ts', '.tsx', '.scss'] },
     },
     rules: {
-      // Next 추천 규칙 적용
-      ...nextRules,
-      ...reactRules,
+      ...next.configs.recommended.rules,
+      ...next.configs['core-web-vitals'].rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      // 훅 관련 규칙은 다른 pr에서 수정 중이라 완료되면 병합하겠습니다.
+      // ...reactHooks.configs['recommended-latest'].rules,
 
-      'react-hooks/exhaustive-deps': 'error',
-      // 기존 airbnb 규칙과 다른 규칙들로 인한 에러 방지를 위해 임시 규칙 off 처리
-      'react/display-name': 'off',
-      'react/jsx-key': 'off',
-      
-      // airbnb 대체용 포매팅 규칙, prettier로 대체 논의 필요
-      quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
-      semi: ['error', 'always'],
-      'comma-dangle': ['error', 'always-multiline'],
-      'object-curly-spacing': ['error', 'always'],
-      'array-bracket-spacing': ['error', 'never'],
-      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0, maxBOF: 0 }],
-      'no-trailing-spaces': ['error'],
-      'eol-last': ['error', 'always'],
-      'arrow-spacing': ['error', { before: true, after: true }],
-      'keyword-spacing': ['error', { before: true, after: true }],
-      'comma-spacing': ['error', { before: false, after: true }],
+      // 'import/order': [
+      //   'error',
+      //   {
+      //     groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'object', 'type', 'index'],
+      //     pathGroups: [
+      //       { pattern: 'react', group: 'external', position: 'before' },
+      //       { pattern: 'next', group: 'external', position: 'before' },
+      //       { pattern: 'next/**', group: 'external', position: 'before' },
+      //       { pattern: '@/**', group: 'internal', position: 'after' },
 
-      'no-restricted-imports': ['error', {
-        patterns: [{ group: ['../*'], message: 'Usage of relative parent imports is not allowed.' }],
-      }],
+      //       { pattern: '**/*.module.{css,scss,sass}', group: 'index', position: 'after' },
+      //       { pattern: '**/*.{css,scss,sass}', group: 'index', position: 'after' },
+      //       { pattern: './**/*.module.{css,scss,sass}', group: 'index', position: 'after' },
+      //       { pattern: './**/*.{css,scss,sass}', group: 'index', position: 'after' },
+      //       { pattern: '*.module.{css,scss,sass}', group: 'index', position: 'after' },
+      //       { pattern: '*.{css,scss,sass}', group: 'index', position: 'after' },
+      //     ],
+
+      //     pathGroupsExcludedImportTypes: ['react', 'next'],
+      //     alphabetize: { order: 'asc', caseInsensitive: true },
+      //   },
+      // ],
+
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [{ group: ['../*'], message: 'Usage of relative parent imports is not allowed.' }],
+        },
+      ],
 
       'import/extensions': 'off',
+      'react/jsx-key': 'off',
+      'react/display-name': 'off',
       'react/react-in-jsx-scope': 'off',
       'react/require-default-props': 'off',
       'max-classes-per-file': 'off',
@@ -93,6 +101,7 @@ export default [
       'jsx-a11y/label-has-associated-control': ['error', { required: { some: ['nesting', 'id'] } }],
       'linebreak-style': 'off',
       'import/prefer-default-export': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
 ];
