002da0aacce495c42b0a2a4e72882865614bd797
dooohun <ehgns0305@naver.com>
2025-11-03 19:22:24 +0900

    refactor: storeBenefitList를 select를 사용하여 필요한 데이터만 가져다 쓸 수 있도록 수정

--- src/components/Store/StoreBenefitPage/hooks/useStoreBenefitList.ts
+++ src/components/Store/StoreBenefitPage/hooks/useStoreBenefitList.ts
@@ -1,20 +1,21 @@
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import * as api from 'api';
 
 const useStoreBenefitList = (id: string) => {
-  const { data: storeBenefitList } = useQuery({
+  return useSuspenseQuery({
     queryKey: ['storeBenefit', id],
     queryFn: async ({ queryKey }) => {
       const queryFnParams = queryKey[1];
 
       return api.store.getStoreBenefitList(queryFnParams);
     },
+    select: (data) => {
+      return {
+        storeBenefitList: data.shops,
+        count: data.count,
+      };
+    },
   });
-
-  return {
-    count: storeBenefitList?.count,
-    storeBenefitList: storeBenefitList?.shops,
-  };
 };
 
 export default useStoreBenefitList;
