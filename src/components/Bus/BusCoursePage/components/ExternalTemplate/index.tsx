b7e3fa7bd27063bb57acc19fbf188fd67320a9e3
JeongWon-CHO <ahin0312@naver.com>
2025-12-06 18:47:33 +0900

    refactor: 안 쓰는 css 삭제 및 네이밍 수정

--- src/components/Bus/BusCoursePage/components/ExternalTemplate/index.tsx
+++ src/components/Bus/BusCoursePage/components/ExternalTemplate/index.tsx
@@ -4,14 +4,14 @@ import styles from './ExternalTemplate.module.scss';
 export default function Template({ typeNumber, arrivalList }: { typeNumber: number; arrivalList: string[][] }) {
   return (
     <div className={styles.timetable} aria-expanded="true">
-      <div className={styles['timetable__label-wrapper']}>
-        <div className={styles.timetable__label}>{BUS_TYPES[typeNumber].tableHeaders[0]}</div>
-        <div className={styles.timetable__label}>{BUS_TYPES[typeNumber].tableHeaders[1]}</div>
+      <div className={styles['timetable-label']}>
+        <div className={styles['timetable-label__text']}>{BUS_TYPES[typeNumber].tableHeaders[0]}</div>
+        <div className={styles['timetable-label__text']}>{BUS_TYPES[typeNumber].tableHeaders[1]}</div>
       </div>
       {arrivalList.map(([arrival, time], idx) => (
-        <div className={styles.timetable__row} key={`${arrival} - ${time} - ${idx}`}>
-          <span className={styles.timetable__cell_am}>{arrival}</span>
-          <span className={styles.timetable__cell_pm}>{time}</span>
+        <div className={styles['timetable-content']} key={`${arrival} - ${time} - ${idx}`}>
+          <span className={styles['timetable-content__am']}>{arrival}</span>
+          <span className={styles['timetable-content__pm']}>{time}</span>
         </div>
       ))}
     </div>
