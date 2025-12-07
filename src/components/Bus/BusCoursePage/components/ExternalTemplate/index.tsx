78fdc0dae8d41160277efb798c67e036ea8a7655
JeongWon-CHO <128157441+JeongWon-CHO@users.noreply.github.com>
2025-12-07 21:14:31 +0900

    [버스] 코드 분리 리팩토링 (#1058)

---
+++ src/components/Bus/BusCoursePage/components/ExternalTemplate/index.tsx
@@ -0,0 +1,19 @@
+import { BUS_TYPES } from 'static/bus';
+import styles from './ExternalTemplate.module.scss';
+
+export default function Template({ typeNumber, arrivalList }: { typeNumber: number; arrivalList: string[][] }) {
+  return (
+    <div className={styles.timetable} aria-expanded="true">
+      <div className={styles['timetable-label']}>
+        <div className={styles['timetable-label__text']}>{BUS_TYPES[typeNumber].tableHeaders[0]}</div>
+        <div className={styles['timetable-label__text']}>{BUS_TYPES[typeNumber].tableHeaders[1]}</div>
+      </div>
+      {arrivalList.map(([arrival, time], idx) => (
+        <div className={styles['timetable-content']} key={`${arrival} - ${time} - ${idx}`}>
+          <span className={styles['timetable-content__am']}>{arrival}</span>
+          <span className={styles['timetable-content__pm']}>{time}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
