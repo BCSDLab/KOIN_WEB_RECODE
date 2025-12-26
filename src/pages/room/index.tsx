9fa3e2e622f370781ed6a3effcd4b71949fa42e9
Lee Junyoung <54898597+ff1451@users.noreply.github.com>
2025-12-26 14:28:31 +0900

    [Next.js 마이그레이션] 복덕방 페이지 ssr 적용 (#1080)

--- src/pages/room/index.tsx
+++ src/pages/room/index.tsx
@@ -1,3 +1,6 @@
+import { QueryClient, dehydrate } from '@tanstack/react-query';
+import { room } from 'api';
+import { SSRLayout } from 'components/layout';
 import RoomList from 'components/Room/components/RoomList';
 import useMarker from 'components/Room/RoomPage/hooks/useMarker';
 import useNaverMap from 'components/Room/RoomPage/hooks/useNaverMap';
@@ -7,6 +10,21 @@ import useScrollToTop from 'utils/hooks/ui/useScrollToTop';
 import styles from './RoomPage.module.scss';
 
 const LOCATION = { latitude: 36.764617, longitude: 127.283154 };
+
+export const getServerSideProps = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['roomList'],
+    queryFn: () => room.getRoomList(),
+  });
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
 
 function RoomPage() {
   const isMobile = useMediaQuery();
@@ -27,3 +45,5 @@ function RoomPage() {
 }
 
 export default RoomPage;
+
+RoomPage.getLayout = (page: React.ReactElement) => <SSRLayout>{page}</SSRLayout>;
