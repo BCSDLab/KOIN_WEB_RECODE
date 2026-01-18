9edc2750f912892521831968f1a39abde0a85e62
이준영 <ff1451@gmail.com>
2026-01-18 22:03:45 +0900

    refactor: 불필요한 변환 로직 제거

--- src/components/Articles/hooks/useLostItemArticles.ts
+++ src/components/Articles/hooks/useLostItemArticles.ts
@@ -1,7 +1,6 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { articles } from 'api';
 import { ItemArticleRequestDTO } from 'api/articles/entity';
-import { transformLostItemArticles } from 'components/Articles/utils/transform';
 import useTokenState from 'utils/hooks/state/useTokenState';
 
 const useLostItemArticles = (data: ItemArticleRequestDTO) => {
@@ -11,7 +10,7 @@ const useLostItemArticles = (data: ItemArticleRequestDTO) => {
     queryKey: ['lostItem', data],
     queryFn: async () => {
       const response = await articles.getLostItemArticles(token, data);
-      return transformLostItemArticles(response);
+      return response;
     },
   });
 
