3a5e10c986ebb65edc6c9bbeb1e259c11a863d1a
이준영 <ff1451@gmail.com>
2026-01-20 20:45:34 +0900

    feat: 버튼 레이아웃 변경 및 필수 값 문구 추가

--- src/components/Articles/components/LostItemPageTemplate/index.tsx
+++ src/components/Articles/components/LostItemPageTemplate/index.tsx
@@ -38,26 +38,42 @@ export default function LostItemPageTemplate({
             {isMobile ? subtitle : title}
             {isMobile && <span>{isFound ? <FoundIcon /> : <LostIcon />}</span>}
           </span>
-          <span className={styles.header__description}>{description}</span>
+          <div className={styles['header__info-wrapper']}>
+            <span className={styles.header__description}>{description}</span>
+            <span className={styles.header__info}>
+              <span>*</span> 표시는 필수 입력란입니다.
+            </span>
+          </div>
         </div>
         <div className={styles.forms}>{children}</div>
-        {onAddButtonClick && (
-          <div className={styles.add}>
-            <button className={styles.add__button} type="button" onClick={onAddButtonClick}>
+
+        <div className={styles.buttons}>
+          {onAddButtonClick && (
+            <button className={styles['buttons__add']} type="button" onClick={onAddButtonClick}>
               <AddIcon />
               물품 추가
             </button>
+          )}
+          <div className={styles['buttons__group']}>
+            {!isMobile && (
+              <button
+                className={`${styles['buttons__button']} ${styles['buttons__button--cancel']}`}
+                type="button"
+                onClick={onBottomButtonClick}
+                disabled={isBottomButtonDisabled}
+              >
+                취소하기
+              </button>
+            )}
+            <button
+              className={styles['buttons__button']}
+              type="button"
+              onClick={onBottomButtonClick}
+              disabled={isBottomButtonDisabled}
+            >
+              {bottomButtonText}
+            </button>
           </div>
-        )}
-        <div className={styles.complete}>
-          <button
-            className={styles.complete__button}
-            type="button"
-            onClick={onBottomButtonClick}
-            disabled={isBottomButtonDisabled}
-          >
-            {bottomButtonText}
-          </button>
         </div>
       </div>
     </div>
