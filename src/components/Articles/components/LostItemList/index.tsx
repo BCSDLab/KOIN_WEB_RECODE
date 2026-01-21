e783b7596eb38ed72b1c25aeaf73f056df0e3a99
JeongWon-CHO <ahin0312@naver.com>
2026-01-21 15:44:21 +0900

    refactor: grid 형태 변경으로 인한 리팩토링

--- src/components/Articles/components/LostItemList/index.tsx
+++ src/components/Articles/components/LostItemList/index.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import { LostItemArticleForGetDTO } from 'api/articles/entity';
 import setArticleRegisteredDate from 'components/Articles/utils/setArticleRegisteredDate';
 import ROUTES from 'static/routes';
-import useMediaQuery from 'utils/hooks/layout/useMediaQuery';
 import showToast from 'utils/ts/showToast';
 import styles from './LostItemList.module.scss';
 
@@ -12,10 +11,8 @@ interface LostItemListProps {
 }
 
 export default function LostItemList({ articles }: LostItemListProps) {
-  const isMobile = useMediaQuery();
-
   return (
-    <div className={styles.list}>
+    <div className={styles.lostItemList}>
       {articles.map((article) => {
         const [registeredDate, isNewArticle] = setArticleRegisteredDate(article.registered_at);
         const detailLink = ROUTES.LostItemDetail({ id: String(article.id), isLink: true });
@@ -24,31 +21,38 @@ export default function LostItemList({ articles }: LostItemListProps) {
           return (
             <button
               key={article.id}
-              className={`${styles.list__link} ${styles.disabled}`}
+              type="button"
+              className={styles.lostItemList__rowDisabled}
               onClick={() => showToast('error', '신고된 게시글은 볼 수 없습니다.')}
             >
-              <div className={styles.list__id}>{article.id}</div>
-              <div className={styles.title}>
-                <div className={styles.title__container}>
-                  <div className={styles.reported_text}>신고에 의해 숨김 처리 되었습니다.</div>
-                </div>
+              <div className={styles.lostItemList__type}>{article.id}</div>
+
+              <div className={styles.lostItemList__title}>
+                <div className={styles.lostItemList__reportedText}>신고에 의해 숨김 처리 되었습니다.</div>
               </div>
-              <div className={styles.list__author}>{article.author}</div>
-              <div className={styles['list__registered-at']}>{registeredDate}</div>
+
+              <div className={styles.lostItemList__author}>{article.author}</div>
+              <div className={styles.lostItemList__date}>{registeredDate}</div>
+
+              {/* 5열 그리드 유지하기 위한 div입니다. */}
+              <div className={styles.lostItemList__status} />
             </button>
           );
         }
 
         return (
-          <Link className={styles.list__link} href={detailLink} key={article.id}>
-            <div className={styles.list__id}>{article.id}</div>
-            <div className={styles.title}>
-              <div className={styles.title__container}>
-                <span className={styles.category_tag}>{article.category}</span>
-                <div className={styles['title__container-type']}>[{article.type === 'LOST' ? '분실' : '습득'}]</div>
-                <div className={styles.content_main}>{article.content}</div>
+          <Link key={article.id} className={styles.lostItemList__row} href={detailLink}>
+            <div className={styles.lostItemList__type}>{article.type === 'LOST' ? '분실물' : '습득물'}</div>
+
+            <div className={styles.lostItemList__title}>
+              <div className={styles.lostItemList__titleMeta}>
+                <span className={styles.lostItemList__badge}>{article.category}</span>
+                <div className={styles.lostItemList__place}>{article.found_place}</div>
+                <div className={styles.lostItemList__foundDate}>{article.found_date}</div>
+
                 {isNewArticle && (
                   <Image
+                    className={styles.lostItemList__newIcon}
                     src="https://static.koreatech.in/upload/7f2af097aeeca368b0a491f9e00f80ca.png"
                     alt="new"
                     width={15}
@@ -57,8 +61,10 @@ export default function LostItemList({ articles }: LostItemListProps) {
                 )}
               </div>
             </div>
-            <div className={styles.list__author}>{article.author}</div>
-            <div className={styles['list__registered-at']}>{registeredDate}</div>
+
+            <div className={styles.lostItemList__author}>{article.author}</div>
+            <div className={styles.lostItemList__date}>{registeredDate}</div>
+            <div className={styles.lostItemList__status}>찾는중</div>
           </Link>
         );
       })}
