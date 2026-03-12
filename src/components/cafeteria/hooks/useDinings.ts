daac6e308e79e78ddab78be70dfbd6b879bbfcc8
이준영 <ff1451@gmail.com>
2026-03-12 11:21:56 +0900

    refactor: 학생식당 도메인 적용

--- src/components/cafeteria/hooks/useDinings.ts
+++ src/components/cafeteria/hooks/useDinings.ts
@@ -1,10 +1,9 @@
 import { useMutation, useQueryClient, useSuspenseQuery } from '@tanstack/react-query';
-import { cancelCafeteriaDiningLike, getCafeteriaDinings, likeCafeteriaDining } from 'api/cafeteria';
+import { cafeteriaMutations } from 'api/cafeteria/mutations';
+import { cafeteriaQueries } from 'api/cafeteria/queries';
 import { Dining, OriginalDining } from 'api/dinings/entity';
 import { convertDateToSimpleString } from 'components/cafeteria/utils/time';
 import useTokenState from 'utils/hooks/state/useTokenState';
-
-const DININGS_KEY = 'DININGS_KEY';
 
 function useDinings(date: Date) {
   const convertedDate = convertDateToSimpleString(date);
@@ -12,8 +11,7 @@ function useDinings(date: Date) {
   const token = useTokenState();
 
   const { data: dinings } = useSuspenseQuery({
-    queryKey: [DININGS_KEY, convertedDate],
-    queryFn: async () => getCafeteriaDinings(convertedDate),
+    ...cafeteriaQueries.dinings(convertedDate),
     select: (data) => {
       if ('status' in data || !Array.isArray(data)) {
         return [];
@@ -24,19 +22,15 @@ function useDinings(date: Date) {
       })) as Array<Dining>;
     },
   });
+  const likeMutation = cafeteriaMutations.likeDining(queryClient, token, convertedDate);
+  const cancelLikeMutation = cafeteriaMutations.cancelLikeDining(queryClient, token, convertedDate);
 
   const likeDiningMutation = useMutation({
-    mutationFn: async (diningId: number) => likeCafeteriaDining(diningId, token),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [DININGS_KEY, convertedDate] });
-    },
+    ...likeMutation,
   });
 
   const cancelLikeDiningMutation = useMutation({
-    mutationFn: async (diningId: number) => cancelCafeteriaDiningLike(diningId, token),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [DININGS_KEY, convertedDate] });
-    },
+    ...cancelLikeMutation,
   });
 
   const likeDining = (diningId: number, isLike: boolean) => {
