4c4d5e8997ccc18317eb0d214e5147fd895f4901
이준영 <ff1451@gmail.com>
2026-06-26 16:44:45 +0900

    refactor: AB 테스트 계획 철폐에 따른 코드 제거

--- src/components/IndexComponents/HomePage/index.tsx
+++ src/components/IndexComponents/HomePage/index.tsx
@@ -1,18 +1,16 @@
 import useMediaQuery from 'utils/hooks/layout/useMediaQuery';
-import { useHomeExperimentVariant } from 'utils/zustand/homeExperiment';
 import DesktopHome from './DesktopHome';
-import MobileHome from './MobileHome';
+import MobileHomeRedesign from './MobileHomeRedesign';
 import type { HomePageProps } from './types';
 
 function HomePage(props: HomePageProps) {
   const isMobile = useMediaQuery();
-  const variant = useHomeExperimentVariant();
 
   if (!isMobile) {
     return <DesktopHome {...props} />;
   }
 
-  return <MobileHome {...props} variant={variant} />;
+  return <MobileHomeRedesign />;
 }
 
 export default HomePage;
