a5add92a8ee8fe192d01c07f77d94bfa03ff1807
jenna <jihyeonie@gmail.com>
2026-08-26 18:03:46 +0900

    feat: 팀원 모집 검색창 기본 기능 구현

--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -1,5 +1,23 @@
-function TeamListPage() {
-  return <div>팀원 모집</div>;
-}
+import { useState } from 'react';
 
-export default TeamListPage;
+import TeamSearchBar from 'components/Team/TeamListPage/components/TeamSearchBar';
+
+import styles from './TeamListPage.module.scss';
+
+export default function TeamListPage() {
+  // 검색창에 현재 입력 중인 값
+  const [searchTitle, setSearchTitle] = useState('');
+
+  // 검색 버튼이나 Enter를 눌렀을 때 실행
+  const handleSearch = () => {
+    console.log('검색어:', searchTitle);
+  };
+
+  return (
+    <main className={styles.container}>
+      <TeamSearchBar value={searchTitle} onChange={setSearchTitle} onSearch={handleSearch} />
+
+      <p>전체(20)</p>
+    </main>
+  );
+}
