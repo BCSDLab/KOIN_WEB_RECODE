6843427003b54ecddd332fc3ce499c6b73e66dc6
박성주 <psj001002@koreatech.ac.kr>
2026-09-10 11:39:39 +0900

    fix: container여부에 따른 분기 로직 간소화

--- src/utils/hooks/ui/useOutsideClick.ts
+++ src/utils/hooks/ui/useOutsideClick.ts
@@ -16,14 +16,12 @@ export const useOutsideClick = <TContainer extends HTMLElement = HTMLDivElement>
       const container = containerRef.current;
       const background = backgroundRef.current;
 
-      if (container) {
-        if (!container.contains(clickedElement)) {
-          onOutsideClick(e);
-        }
+      if (container && !container.contains(clickedElement)) {
+        onOutsideClick(e);
         return;
       }
 
-      if (background && clickedElement === background) {
+      if (!container && clickedElement === background) {
         onOutsideClick(e);
       }
     };
