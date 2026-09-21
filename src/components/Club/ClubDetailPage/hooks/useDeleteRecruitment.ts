71ae88f08d83842c0e0c7a5be7e28f7ee083c5c7
이준영 <ff1451@gmail.com>
2026-09-21 21:46:41 +0900

    refactor: 동아리 작성/수정/삭제/좋아요/알림 훅에서 죽은 useTokenState 제거

--- src/components/Club/ClubDetailPage/hooks/useDeleteRecruitment.ts
+++ src/components/Club/ClubDetailPage/hooks/useDeleteRecruitment.ts
@@ -3,17 +3,15 @@ import { useRouter } from 'next/router';
 import { isKoinError, sendClientError } from '@bcsdlab/koin';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { clubMutations } from 'api/club/mutations';
-import useTokenState from 'utils/hooks/state/useTokenState';
 import showToast from 'utils/ts/showToast';
 
 export default function useDeleteRecruitment() {
   const router = useRouter();
   const { id } = router.query as { id: string };
   const queryClient = useQueryClient();
-  const token = useTokenState();
 
   return useMutation({
-    ...clubMutations.deleteRecruitment(queryClient, token, Number(id), {
+    ...clubMutations.deleteRecruitment(queryClient, Number(id), {
       onSuccess: () => {
         showToast('success', '모집이 삭제되었습니다.');
       },
--- src/components/Club/NewClubEvent/hooks/usePostNewEvent.ts
+++ src/components/Club/NewClubEvent/hooks/usePostNewEvent.ts
@@ -4,16 +4,14 @@ import { isKoinError, sendClientError } from '@bcsdlab/koin';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { clubMutations } from 'api/club/mutations';
 import ROUTES from 'static/routes';
-import useTokenState from 'utils/hooks/state/useTokenState';
 import showToast from 'utils/ts/showToast';
 
 export default function usePostNewEvent(clubId: number | undefined) {
-  const token = useTokenState();
   const queryClient = useQueryClient();
   const router = useRouter();
 
   const { mutateAsync } = useMutation({
-    ...clubMutations.createEvent(queryClient, token, clubId!, {
+    ...clubMutations.createEvent(queryClient, clubId!, {
       onSuccess: () => {
         showToast('success', '동아리 행사가 생성되었습니다.');
         router.push(ROUTES.ClubDetail({ id: String(clubId) }));
